9e90189acff9c393ae0ede86efbaa0b5ced353b3
Alex McArther <acmcarther@gmail.com>
2018-02-12 10:01:13 -0800

    Fix runfiles test: Macos runfiles are quite different (#66)
    Apparently "data" was not a quirk of macos, but rather the directory that the data file was in originally. I've moved that part of the pathbuilding accordingly.
    
    I'm starting to suspect that the discrepancy isn't in how the runfiles dir is structure, but rather where "current_exe" points to.

--- examples/hello_runfiles/src/lib.rs
+++ examples/hello_runfiles/src/lib.rs
@@ -5,11 +5,14 @@ use std::path::PathBuf;
 pub fn get_runfiles_dir() -> io::Result<PathBuf> {
     let mut path = std::env::current_exe()?;
 
-    let mut name = path.file_name().unwrap().to_owned();
-    name.push(".runfiles");
-
-    path.pop();
-    path.push(name);
+    if cfg!(target_os = "macos") {
+      path.pop();
+    } else {
+      let mut name = path.file_name().unwrap().to_owned();
+      name.push(".runfiles");
+      path.pop();
+      path.push(name);
+    }
 
     Ok(path)
 }
@@ -27,7 +30,11 @@ mod test {
     fn test_can_read_data_from_runfiles() {
         let runfiles = get_runfiles_dir().unwrap();
 
-        let mut f = File::open(runfiles.join("examples/hello_runfiles/data/sample.txt")).unwrap();
+        let mut f = if cfg!(target_os = "macos") {
+          File::open(runfiles.join("data/sample.txt")).unwrap()
+        } else {
+          File::open(runfiles.join("examples/hello_runfiles/data/sample.txt")).unwrap()
+        };
         let mut buffer = String::new();
 
         f.read_to_string(&mut buffer).unwrap();
