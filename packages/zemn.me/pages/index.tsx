91c9e957b111f4b4633c1fdd311758499cb93703
Thomas Shadwell <zemnmez@googlemail.com>
2020-08-01 21:58:54 +0100

    after abandoning all i love, I have found some success

--- packages/zemn.me/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { bio } from '@zemn.me/bio';
-
-export const cmpt = () => <>{JSON.stringify(bio)}</>
-
-export default cmpt;
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Bio } from '@zemn.me/bio';
+
+export const cmpt = () => <>{JSON.stringify(Bio)}</>
+
+export default cmpt;
