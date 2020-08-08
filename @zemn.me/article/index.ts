40c2723b535ea30828d9e2bb8dd55925697f4a83
zemnmez <zemnmez@googlemail.com>
2020-08-08 22:14:37 +0100

    wild

--- @zemn.me/article/index.ts
+++
@@ -1,7 +0,0 @@
-import hackiCloud from './how-to-hack-icloud';
-import psychiatry from './psychiatry';
-
-export default [
-    hackiCloud,
-    psychiatry
-] as const;
--- pages/article/2020/psychiatry/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './psychiatry';
-export { default } from './psychiatry';
