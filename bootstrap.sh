b894ebaaeec4075ecf8b9f57a250842bec40998e
Daniel Wagner-Hall <dwagnerhall@apple.com>
2021-04-20 13:56:12 +0100

    Fix bootstrapping since #694 (#699)

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,11 @@ BOOTSTRAP_DIR="${SCRIPT_DIR}/crate_universe/private/bootstrap"
 
 # Go to root of rules_rust
 pushd "${SCRIPT_DIR}" &> /dev/null
-has_changes="$(git log origin/main.. -- crate_universe)"
+has_changes="$(git diff --name-only origin/main -- crate_universe bootstrap.sh)"
 popd &> /dev/null
 
 # Only bootstrap the binary if there are changes
-if [[ -n "${has_changes}" ]]; then
+if [[ -n "${has_changes}" ]] || git ls-files --others --exclude-standard | grep 'crate_universe'; then
     pushd "${BOOTSTRAP_DIR}" &> /dev/null
     bazel run //:build && bazel run //:install && bazel clean
     popd &> /dev/null
@@ -24,11 +24,7 @@ if [[ -n "${has_changes}" ]]; then
     # Generate a bazelrc file for ensuring we always use the resolver binary for the current host
     cat << EOF > "${SCRIPT_DIR}/crate_universe.bazelrc"
 common --announce_rc
-common --override_repository="rules_rust_crate_universe__aarch64-apple-darwin=${BOOTSTRAP_DIR}"
-common --override_repository="rules_rust_crate_universe__aarch64-unknown-linux-gnu=${BOOTSTRAP_DIR}"
-common --override_repository="rules_rust_crate_universe__x86_64-apple-darwin=${BOOTSTRAP_DIR}"
-common --override_repository="rules_rust_crate_universe__x86_64-pc-windows-gnu=${BOOTSTRAP_DIR}"
-common --override_repository="rules_rust_crate_universe__x86_64-unknown-linux-gnu=${BOOTSTRAP_DIR}"
+common --repo_env="RULES_RUST_CRATE_UNIVERSE_RESOLVER_URL_OVERRIDE=file://${BOOTSTRAP_DIR}/file/resolver"
 EOF
 
     cp ${SCRIPT_DIR}/crate_universe.bazelrc ${SCRIPT_DIR}/examples/crate_universe/crate_universe.bazelrc
