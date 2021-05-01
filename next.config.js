b4a915c0a5c0e1aba39d76adceda9d1da60b29f9
zemnmez <zemnmez@googlemail.com>
2021-05-01 14:46:12 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -1,14 +1,56 @@
 /**
- * Adapts plugins that just operate on an input object
- * to the function form.
+ * @see https://github.com/vercel/next.js/blob/canary/packages/next/next-server/lib/constants.ts#L1-L4
  */
-const simplePlugin = (plugin) =>
-	// prev plugin, and arbitrary args
-	(lastPlugin, ...a) =>
-		// plugin function form
-		(...pluginParams) =>
-			// pass the result of the last plugin to our plugin
-			plugin(lastPlugin(...pluginParams), ...a)
+const constants = require('next/constants')
+
+const getName = (v) => {
+	let name = v.desc || v.name
+	if (!name) name = v.toString()
+	return name
+}
+
+const guardPluginReturnsUndefined = (plugin) => {
+	const ret = (...params) => {
+		const ret = plugin(...params)
+		if (ret === undefined)
+			throw new Error(`Plugin ${getName(plugin)} returned undefined`)
+		return ret
+	}
+
+	ret.desc = getName(plugin)
+	return ret
+}
+
+const chainOne = (first, next) => {
+	const ret = (phase, { defaultConfig, ...propEtc }, ...etc) => {
+		console.info('chainOne: exec plugin', getName(first))
+		const newConfig = first(phase, { defaultConfig, ...propEtc }, ...etc)
+		console.info('chainOne: exec plugin', getName(next))
+		return next(phase, { defaultConfig: newConfig, ...propEtc }, ...etc)
+	}
+	ret.desc = `chainOne(${getName(first)}, ${getName(next)})`
+	return ret
+}
+
+const phase = (config, phase) => {
+	const ret = (realPhase, { defaultConfig, ...confProps }, ...etc) => {
+		if (phase !== realPhase) return defaultConfig
+		return config(realPhase, { defaultConfig, ...confProps }, ...etc)
+	}
+
+	ret.desc = `phase(${getName(config)}, ${phase})`
+	return ret
+}
+
+const chain = (...plugins) => {
+	let config
+	for (let plugin of plugins) {
+		plugin = guardPluginReturnsUndefined(plugin)
+		console.log('chain: installing plugin', getName(plugin))
+		config = config !== undefined ? chainOne(config, plugin) : plugin
+	}
+	return config
+}
 
 const ramp = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
 class BaseConverter {
@@ -41,73 +83,79 @@ class BaseConverter {
 	}
 }
 
-const cssMinifierPlugin = simplePlugin((config) => ({
-	...config,
-	webpack(wpcfg, ...a) {
-		wpcfg.module.rules[1].oneOf.forEach((moduleLoader) => {
-			Array.isArray(moduleLoader.use) &&
-				moduleLoader.use.forEach((l) => {
-					if (l.loader.includes('css-loader')) {
-						l.options = {
-							...l.options,
-							modules: {
-								...l.options.modules,
-								...{ getLocalIdent: uniqueClass },
-								//{}
-							},
+function cssMinifierPlugin(phase, { defaultConfig: config }) {
+	return {
+		...config,
+		webpack(wpcfg, ...a) {
+			wpcfg.module.rules[1].oneOf.forEach((moduleLoader) => {
+				Array.isArray(moduleLoader.use) &&
+					moduleLoader.use.forEach((l) => {
+						if (l.loader.includes('css-loader')) {
+							l.options = {
+								...l.options,
+								modules: {
+									...l.options.modules,
+									...{ getLocalIdent: uniqueClass },
+									//{}
+								},
+							}
 						}
-					}
-				})
-		})
+					})
+			})
 
-		wpcfg.node = { fs: 'empty' }
-		if (config.webpack) wpcfg = config.webpack(wpcfg, ...a)
-		return wpcfg
-	},
-}))
+			wpcfg.node = { fs: 'empty' }
+			if (config.webpack) wpcfg = config.webpack(wpcfg, ...a)
+			return wpcfg
+		},
+	}
+}
 
 const ClosurePlugin = require('closure-webpack-plugin')
 
-const closureCompilerPlugin = simplePlugin((config) => ({
-	...config,
-	webpack(wpcfg, ...a) {
-		wpcfg = {
-			...wpcfg,
-			optimization: {
-				...wpcfg.optimization,
-				minimizer: [
-					...wpcfg.optimization.minimizer,
-					new ClosurePlugin({ mode: 'STANDARD' }, {}),
-				],
-			},
-		}
-		if (config.webpack) wpcfg = config.webpack(wpcfg, ...a)
-		return wpcfg
-	},
-}))
+function closureCompilerPlugin(phase, { defaultConfig: config }) {
+	return {
+		...config,
+		webpack(wpcfg, ...a) {
+			wpcfg = {
+				...wpcfg,
+				optimization: {
+					...wpcfg.optimization,
+					minimizer: [
+						...wpcfg.optimization.minimizer,
+						new ClosurePlugin({ mode: 'STANDARD' }, {}),
+					],
+				},
+			}
+			if (config.webpack) wpcfg = config.webpack(wpcfg, ...a)
+			return wpcfg
+		},
+	}
+}
 
-const xdmPlugin = simplePlugin((config) => ({
-	...config,
-	pageExtensions: [...(config.pageExtensions || []), 'mdx'],
-	webpack(wpcfg, options, ...a) {
-		wpcfg = {
-			...wpcfg,
-			module: {
-				...wpcfg.module,
-				rules: [
-					...wpcfg.module.rules,
-					{
-						test: /\.mdx$/,
-						use: [{ loader: 'xdm/webpack.cjs', options: {} }],
-					},
-				],
-			},
-		}
+function xdmPlugin(phase, { defaultConfig: config }) {
+	return {
+		...config,
+		pageExtensions: [...(config.pageExtensions || []), 'mdx'],
+		webpack(wpcfg, options, ...a) {
+			wpcfg = {
+				...wpcfg,
+				module: {
+					...wpcfg.module,
+					rules: [
+						...wpcfg.module.rules,
+						{
+							test: /\.mdx$/,
+							use: [{ loader: 'xdm/webpack.cjs', options: {} }],
+						},
+					],
+				},
+			}
 
-		if (config.webpack) wpcfg = config.webpack(wpcfg, options, ...a)
-		return wpcfg
-	},
-}))
+			if (config.webpack) wpcfg = config.webpack(wpcfg, options, ...a)
+			return wpcfg
+		},
+	}
+}
 
 const conv = new BaseConverter(ramp)
 
@@ -122,36 +170,44 @@ const uniqueClass = (() => {
 	}
 })()
 
-const identityPlugin = simplePlugin((a) => a)
+function baseConfig(phase, { defaultConfig: config }) {
+	return {
+		...config,
+		generateBuildId: () =>
+			require('next-build-id')({
+				dir: __dirname,
+				describe: true,
+			}),
+		webpack: (wpcfg, options, ...a) => {
+			const { isServer } = options
+			// Fixes npm packages that depend on `fs` module
+			if (!isServer) {
+				wpcfg.node = {
+					fs: 'empty',
+				}
+			}
 
-const productionOnly = (plugin) => {
-	if (process.env.NODE_ENV !== 'production') return identityPlugin
-	return plugin
+			if (config.webpack) wpcfg = config.webpack(config, options, ...a)
+			return wpcfg
+		},
+	}
 }
 
-const baseConfig = simplePlugin((config) => ({
-	...config,
-	generateBuildId: () =>
-		require('next-build-id')({
-			dir: __dirname,
-			describe: true,
-		}),
-	webpack: (wpcfg, options, ...a) => {
-		const { isServer } = options
-		// Fixes npm packages that depend on `fs` module
-		if (!isServer) {
-			wpcfg.node = {
-				fs: 'empty',
-			}
-		}
+/*const printConfigSetup = (
+	phase,
+	{ defaultConfig: config, ...propEtc },
+	...paramEtc
+) => {
+	console.info('phase', phase)
+	console.info('config', config)
+	console.info('propEtc', propEtc)
+	console.info('paramEtc', paramEtc)
+	return config
+}*/
 
-		if (wpcfg.webpack) wpcfg = wpcfg.webpack(config, options, ...a)
-		return config
-	},
-}))
-
-module.exports = (phase, { defaultConfig }) => defaultConfig
-module.exports = baseConfig(module.exports)
-module.exports = productionOnly(cssMinifierPlugin)(module.exports)
-module.exports = productionOnly(closureCompilerPlugin)(module.exports)
-module.exports = xdmPlugin(module.exports)
+module.exports = chain(
+	baseConfig,
+	phase(closureCompilerPlugin, constants.PHASE_PRODUCTION_BUILD),
+	phase(cssMinifierPlugin, constants.PHASE_PRODUCTION_BUILD),
+	xdmPlugin,
+)
