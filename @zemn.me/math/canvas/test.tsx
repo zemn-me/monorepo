f3b1164878d29ab22e173226fae6531050eba79d
Thomas Neil James Shadwell <tshadwell@google.com>
2021-05-19 22:45:18 +0100

    add smoke test for 3d rendering, and remove remark-description-list as a dep (#16)

---
+++ @zemn.me/math/canvas/test.tsx
@@ -0,0 +1,29 @@
+import { unmountComponentAtNode } from 'react-dom'
+import * as Shape from '../shape'
+import { Canvas } from './element'
+import { act } from 'react-dom/test-utils'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+let container: HTMLDivElement | null = null
+
+beforeEach(() => {
+	container = document.createElement('div')
+	document.body.appendChild(container)
+})
+
+afterEach(() => {
+	unmountComponentAtNode(container!)
+	container?.remove()
+	container = null
+})
+
+describe('react.Canvas', () => {
+	it('should append an svg element', () => {
+		act(() => {
+			ReactDOM.render(<Canvas draw={new Shape.Square(1)} />, container)
+		})
+
+		expect(container?.querySelector('svg')).not.toBeUndefined()
+	})
+})
