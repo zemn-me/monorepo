f7334224025cfaed059c8d4a6dd0c1ce1d9d8483
zemnmez <zemnmez@gmail.com>
2021-08-21 18:41:49 +0000

    absolute typescript imports

---
+++ typescript/import_test/a.ts
@@ -0,0 +1,5 @@
+import * as b  from "//typescript/import_test/b";
+
+const copy: string = b.MyString;
+
+export { copy as MyString };
---
+++ typescript/import_test/import_test.ts
@@ -0,0 +1,6 @@
+import * as a from "//typescript/import_test/a";
+
+test('import', () => {
+    expect(a.MyString).toEqual("Hello world!");
+});
+
