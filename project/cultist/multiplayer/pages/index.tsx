c39213bfd61847a7c7c9c93b950d544ca673380e
Thomas Neil James Shadwell <tshadwell@google.com>
2022-01-04 18:05:33 -0800

    enable Strict Mode for cultist multiplayer. other minor fixes (#60)

--- project/cultist/multiplayer/pages/index.tsx
+++ project/cultist/multiplayer/pages/index.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import exampleSave from 'project/cultist/example/savestate';
 
-// local imports (not NPM) seem to be broken...
-
 import * as State from 'project/cultist/state';
 import * as Save from 'project/cultist/save';
 import * as Board from 'project/cultist/react/board';
-import core_en from 'project/cultist/gen/core_en.json';
-
-console.log(core_en);
 
 const Home = (): React.ReactElement => (
-	<>
+	<React.StrictMode>
 		<Board.Board
 			state={State.deserialize.state(
 				Save.load(JSON.stringify(exampleSave))
 			)}
 		/>
-	</>
+	</React.StrictMode>
 );
 
 export default Home;
--- project/cultist/react/board.tsx
+++ project/cultist/react/board.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import State from 'project/cultist/state';
+import * as State from 'project/cultist/state';
 
 export interface BoardProps {
 	state: State.State;
@@ -8,14 +8,68 @@ export interface BoardProps {
 export const Board: React.FC<Readonly<BoardProps>> = ({ state }) => {
 	return (
 		<>
+			{state.characterDetails ? (
+				<figure>
+					<header>Character Details</header>
+					<dl>
+						<dt>Name</dt>
+						<dd>{state.characterDetails.toJSON().name}</dd>
+
+						<dt>Profession</dt>
+						<dd>{state.characterDetails.profession}</dd>
+					</dl>
+				</figure>
+			) : null}
 			{[...(state.elementStacks?.entries() ?? [])].map(
 				([key, elementInstance]) => (
 					<Card key={key} instance={elementInstance} />
 				)
 			)}
+
+			{[...(state.decks?.entries() ?? [])].map(([name, deck]) => (
+				<Deck key={name} name={name} deck={deck} />
+			))}
 		</>
 	);
 };
+
+export interface DeckProps {
+	name: string;
+	deck: State.Deck;
+}
+
+export const Deck: React.FC<Readonly<DeckProps>> = ({ name, deck }) => (
+	<figure>
+		<figcaption>Deck: {name}</figcaption>
+
+		{(() => {
+			console.log(deck);
+			return null;
+		})()}
+
+		{(deck.eliminatedCards?.size ?? 0) > 0 ? (
+			<figure>
+				<figcaption>Eliminated Cards</figcaption>
+				<ol>
+					{deck.eliminatedCards?.map(name => (
+						<li key={name}>{name}</li>
+					))}
+				</ol>
+			</figure>
+		) : null}
+
+		{(deck.cards?.size ?? 0) > 0 ? (
+			<figure>
+				<figcaption>Cards</figcaption>
+				<ol>
+					{deck.cards?.map(name => (
+						<li key={name}>{name}</li>
+					))}
+				</ol>
+			</figure>
+		) : null}
+	</figure>
+);
 
 export interface CardProps {
 	instance: State.ElementInstance;
