21adebeb56b170b7a7c19eaa104002956e184388
Thomas Neil James Shadwell <tshadwell@google.com>
2022-01-07 10:34:38 -0800

    table display (#61)

--- project/cultist/multiplayer/pages/index.tsx
+++ project/cultist/multiplayer/pages/index.tsx
@@ -3,11 +3,11 @@ import exampleSave from 'project/cultist/example/savestate';
 
 import * as State from 'project/cultist/state';
 import * as Save from 'project/cultist/save';
-import * as Board from 'project/cultist/react/board';
+import * as Board from 'project/cultist/react/table';
 
 const Home = (): React.ReactElement => (
 	<React.StrictMode>
-		<Board.Board
+		<Board.Table
 			state={State.deserialize.state(
 				Save.load(JSON.stringify(exampleSave))
 			)}
