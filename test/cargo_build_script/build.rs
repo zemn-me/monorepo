97fd3295401021e1afd20575b375e36c47f27b67
Damien DeVille <damien@ddeville.me>
2022-03-10 01:29:30 -0800

    Populate CFLAGS and CXXFLAGS when invoking build script. (#1081)
    Downstream tools (in particular cc-rs) depend on them being set.
    
    The particular use case that I noticed was broken was the macos
    deployment target (specified via the --macos_minimun_os Bazel option)
    wasn't being applied on binaries being built via cc-rs in a build.rs
    script. This was causing all sort of bugs such as symbols not being
    weakly linked since it was using the SDK version (the default) for the
    deployment target and bazel-built Rust binaries wouldn't load on older
    OS versions due to failures to resolve symbols at runtime, even though
    the deployment target was correctly set in Bazel.

--- test/cargo_build_script/build.rs
+++ test/cargo_build_script/build.rs
@@ -28,12 +28,12 @@ fn main() {
         std::env::var("TOOL").unwrap()
     );
 
-    // Assert that the CC and CXX env vars existed and were executable.
+    // Assert that the CC, CXX and LD env vars existed and were executable.
     // We don't assert what happens when they're executed (in particular, we don't check for a
     // non-zero exit code), but this asserts that it's an existing file which is executable.
     //
     // Unfortunately we need to shlex the path, because we add a `--sysroot=...` arg to the env var.
-    for env_var in &["CC", "CXX"] {
+    for env_var in &["CC", "CXX", "LD"] {
         let v = std::env::var(env_var)
             .unwrap_or_else(|err| panic!("Error getting {}: {}", env_var, err));
         let (path, args) = if let Some(index) = v.find("--sysroot") {
@@ -46,5 +46,10 @@ fn main() {
             .args(args.into_iter())
             .status()
             .unwrap();
+    }
+
+    // Assert that some env variables are set.
+    for env_var in &["CFLAGS", "CXXFLAGS", "LDFLAGS"] {
+        assert!(std::env::var(env_var).is_ok());
     }
 }
