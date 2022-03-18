68ca1e9f717dad97b06a82e090da5fa5a38d2dde
zemnmez <zemnmez@googlemail.com>
2022-03-18 13:31:43 -0700

    Fix formatting

--- project/zemn.me/root.tsx
+++ project/zemn.me/root.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import style from './style.module.css';
 
-export const Root: React.FC = () => <div className={style.main}>
-    Hello, world!
-</div>
+export const Root: React.FC = () => (
+	<div className={style.main}>Hello, world!</div>
+);
 
 export default Root;
