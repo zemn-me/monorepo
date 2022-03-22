19ed431be59a5eb51d6ae60dacd70c5d6bcdd8c6
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-03-22 00:00:18 -0700

    strict mode (#92)
    * Add a cute little bar with a logo

--- project/zemn.me/root.tsx
+++ project/zemn.me/root.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import Pages from 'project/zemn.me/pages';
+import NavBar from 'project/zemn.me/elements/NavBar';
+
+// side-effects only.
+import './root.module.css';
 
 export const Root: React.FC = () => (
-	<BrowserRouter>
-		<Routes>{Pages}</Routes>
-	</BrowserRouter>
+	<>
+		<BrowserRouter>
+			<NavBar />
+			<Routes>{Pages}</Routes>
+		</BrowserRouter>
+	</>
 );
 
 export default Root;
