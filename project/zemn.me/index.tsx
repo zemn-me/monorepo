f6b9c5ec6995d97f602e238e28c30413c9936e03
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-03-21 23:26:41 -0700

    zemn.me: strict mode and add deps that I am 99% sure should have already been present. (#91)

--- project/zemn.me/index.tsx
+++ project/zemn.me/index.tsx
@@ -2,4 +2,9 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Root from './root';
 
-ReactDOM.render(<Root />, document.querySelector('main'));
+ReactDOM.render(
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	document.querySelector('main')
+);
