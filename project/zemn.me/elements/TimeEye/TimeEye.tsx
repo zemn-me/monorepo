9af51708dffd86c10bca711859e579b538cec8fe
zemnmez <zemnmez@googlemail.com>
2022-03-22 10:23:21 -0700

    shitty title page code

--- project/zemn.me/elements/TimeEye/TimeEye.tsx
+++ project/zemn.me/elements/TimeEye/TimeEye.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from 'ts/react/element/time/time.module.css';
+import style from './TimeEye.module.css';
 import classNames from 'classnames';
 
 export const TimeEye: React.FC<JSX.IntrinsicElements['svg']> = ({
@@ -8,18 +8,15 @@ export const TimeEye: React.FC<JSX.IntrinsicElements['svg']> = ({
 }) => (
 	<svg
 		{...props}
-		className={classNames(className, style.sadIcon)}
-		xmlns="http://www.w3.org/2000/svg"
+		className={classNames(className, style.timeEye)}
 		viewBox="0 0 17.78 7.81"
 	>
 		<g transform="translate(-13.03 -62.53)">
 			<path
-				className={style.frustum}
 				strokeWidth=".26"
 				d="M16.73 62.66l-3.47 6.02h17.32l-3.47-6.02z"
 			/>
 			<circle
-				className={style.iris}
 				cx="21.92"
 				cy="65.47"
 				r="1.61"
@@ -27,7 +24,6 @@ export const TimeEye: React.FC<JSX.IntrinsicElements['svg']> = ({
 				strokeWidth=".16"
 			/>
 			<ellipse
-				className={style.sclera}
 				cx="21.92"
 				cy="65.47"
 				fill="none"
@@ -36,17 +32,10 @@ export const TimeEye: React.FC<JSX.IntrinsicElements['svg']> = ({
 				ry="1.58"
 			/>
 			<path
-				className={style.tear}
 				strokeWidth=".16"
 				d="M23.53 68.65a1.61 1.61 0 0 1-3.22 0c0-.9.72-1.2 1.61-1.62.9.42 1.61.73 1.61 1.62z"
 			/>
-			<circle
-				className={style.pupil}
-				cx="21.92"
-				cy="65.47"
-				r=".54"
-				strokeWidth=".08"
-			/>
+			<circle cx="21.92" cy="65.47" r=".54" strokeWidth=".08" />
 		</g>
 	</svg>
 );
