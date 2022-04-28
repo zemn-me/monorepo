zemn-me/monorepo

commit 841fc6fb82141576d91aecb1d3f2656d58b0ab71

Enable xz archives (#1284)
* Expand the universe of known shas to include the .xz archive shas.

I have explicitly added keys for the files with the extension explicitly
on the file key. The point of this is to (in a future change) allow
tar.xz archives to be used instead of tar.gz archives.

This change keeps compatiblity with the old keys that assume the tar.gz
archives. I don't know if this is something that we should drop in the future
or just not worry about.

* Enable xz archive support.

XZ archives are about half the size of the gz equivalents. This
improves the cold repo bootstrap case a fair amount.

Co-authored-by: Daniel Wagner-Hall <dwagnerhall@apple.com>

rust/private/repository_utils.bzl
@@ -46,7 +46,7 @@ filegroup(
 """
 
 def BUILD_for_compiler(target_triple):
-    """Emits a BUILD file the compiler `.tar.gz`.
+    """Emits a BUILD file the compiler archive.
 
     Args:
         target_triple (str): The triple of the target platform
@@ -72,7 +72,7 @@ filegroup(
 )"""
 
 def BUILD_for_cargo(target_triple):
-    """Emits a BUILD file the cargo `.tar.gz`.
+    """Emits a BUILD file the cargo archive.
 
     Args:
         target_triple (str): The triple of the target platform
@@ -102,7 +102,7 @@ sh_binary(
 """
 
 def BUILD_for_rustfmt(target_triple):
-    """Emits a BUILD file the rustfmt `.tar.gz`.
+    """Emits a BUILD file the rustfmt archive.
 
     Args:
         target_triple (str): The triple of the target platform
@@ -126,7 +126,7 @@ filegroup(
 """
 
 def BUILD_for_clippy(target_triple):
-    """Emits a BUILD file the clippy `.tar.gz`.
+    """Emits a BUILD file the clippy archive.
 
     Args:
         target_triple (str): The triple of the target platform
@@ -164,7 +164,7 @@ alias(
 """
 
 def BUILD_for_stdlib(target_triple):
-    """Emits a BUILD file the stdlib `.tar.gz`.
+    """Emits a BUILD file the stdlib archive.
 
     Args:
         target_triple (str): The triple of the target platform
@@ -344,15 +344,14 @@ def load_rust_src(ctx):
         ctx (ctx): A repository_ctx.
     """
     tool_suburl = produce_tool_suburl("rust-src", None, ctx.attr.version, ctx.attr.iso_date)
-    static_rust = ctx.os.environ.get("STATIC_RUST_URL", "https://static.rust-lang.org")
-    url = "{}/dist/{}.tar.gz".format(static_rust, tool_suburl)
+    url = ctx.attr.urls[0].format(tool_suburl)
 
     tool_path = produce_tool_path("rust-src", None, ctx.attr.version)
-    archive_path = tool_path + ".tar.gz"
+    archive_path = tool_path + _get_tool_extension(ctx)
     ctx.download(
         url,
         output = archive_path,
-        sha256 = ctx.attr.sha256s.get(tool_suburl) or FILE_KEY_TO_SHA.get(tool_suburl) or "",
+        sha256 = ctx.attr.sha256s.get(archive_path) or FILE_KEY_TO_SHA.get(archive_path) or "",
         auth = _make_auth_dict(ctx, [url]),
     )
     ctx.extract(
@@ -551,22 +550,19 @@ def load_arbitrary_tool(ctx, tool_name, tool_subdirectories, version, iso_date, 
     tool_suburl = produce_tool_suburl(tool_name, target_triple, version, iso_date)
     urls = []
 
-    static_rust_url_from_env = ctx.os.environ.get("STATIC_RUST_URL")
-    if static_rust_url_from_env:
-        urls.append("{}/dist/{}.tar.gz".format(static_rust_url_from_env, tool_suburl))
-
     for url in getattr(ctx.attr, "urls", DEFAULT_STATIC_RUST_URL_TEMPLATES):
         new_url = url.format(tool_suburl)
         if new_url not in urls:
             urls.append(new_url)
 
     tool_path = produce_tool_path(tool_name, target_triple, version)
-    archive_path = "{}.tar.gz".format(tool_path)
+
+    archive_path = tool_path + _get_tool_extension(ctx)
     ctx.download(
         urls,
         output = archive_path,
-        sha256 = getattr(ctx.attr, "sha256s", dict()).get(tool_suburl) or
-                 FILE_KEY_TO_SHA.get(tool_suburl) or
+        sha256 = getattr(ctx.attr, "sha256s", dict()).get(archive_path) or
+                 FILE_KEY_TO_SHA.get(archive_path) or
                  sha256,
         auth = _make_auth_dict(ctx, urls),
     )
@@ -574,7 +570,7 @@ def load_arbitrary_tool(ctx, tool_name, tool_subdirectories, version, iso_date, 
         ctx.extract(
             archive_path,
             output = "",
-            stripPrefix = "{}/{}".format(tool_path, subdirectory),
+            stripPrefix = "{}/{}".format(tool_suburl, subdirectory),
         )
 
 def _make_auth_dict(ctx, urls):
@@ -585,3 +581,12 @@ def _make_auth_dict(ctx, urls):
     for url in urls:
         ret[url] = auth
     return ret
+
+def _get_tool_extension(ctx):
+    urls = getattr(ctx.attr, "urls", DEFAULT_STATIC_RUST_URL_TEMPLATES)
+    if urls[0][-7:] == ".tar.gz":
+        return ".tar.gz"
+    elif urls[0][-7:] == ".tar.xz":
+        return ".tar.xz"
+    else:
+        return ""

util/fetch_shas.sh
@@ -18,6 +18,11 @@ VERSIONS="$(cat "${BUILD_WORKSPACE_DIRECTORY}/util/fetch_shas_VERSIONS.txt")"
 BETA_ISO_DATES="$(cat "${BUILD_WORKSPACE_DIRECTORY}/util/fetch_shas_BETA_ISO_DATES.txt")"
 NIGHTLY_ISO_DATES="$(cat "${BUILD_WORKSPACE_DIRECTORY}/util/fetch_shas_NIGHTLY_ISO_DATES.txt")"
 
+EXTENSIONS=(
+   tar.gz
+   tar.xz
+)
+
 enumerate_keys() {
   for TOOL in $TOOLS
   do
@@ -50,11 +55,42 @@ enumerate_keys() {
 }
 
 emit_bzl_file_contents() {
-  echo "$@" \
-    | parallel --trim lr -d ' ' --will-cite 'printf "%s %s\n", {}, $(curl --fail https://static.rust-lang.org/dist/{}.tar.gz.sha256 | cut -f1 -d" ")' \
-    | sed "s/,//g" \
-    | grep -v " $" \
-    > /tmp/reload_shas_shalist.txt
+  if which parallel > /dev/null; then
+    for ext in "${EXTENSIONS[@]}"; do
+    echo "$@" \
+      | parallel --trim lr -d ' ' --will-cite 'printf "%s %s\n", {}, $(curl --fail https://static.rust-lang.org/dist/{}.'"${ext}"'.sha256 | cut -f1 -d" ")' \
+      | sed "s/,//g" \
+      | grep -v " $" \
+      > "${TMPDIR}"/shas.txt
+    done
+  else
+    mkdir "$TMPDIR"/outs
+
+    echo "--parallel" >> "${TMPDIR}"/curl_config
+    echo "--fail" >> "${TMPDIR}"/curl_config
+    echo "--silent" >> "${TMPDIR}"/curl_config
+    echo "--create-dirs" >> "${TMPDIR}"/curl_config
+    for key in "$@"; do
+      for ext in "${EXTENSIONS[@]}"; do
+        echo "--output ${TMPDIR}/outs/${key}.${ext}" >> "${TMPDIR}"/curl_config
+        echo "--url https://static.rust-lang.org/dist/${key}.${ext}.sha256" >> "${TMPDIR}"/curl_config
+      done
+    done
+    curl --config "${TMPDIR}"/curl_config
+
+    pushd "$TMPDIR"/outs >/dev/null
+        find . -type f -print | \
+        awk '{
+            file_key=substr($1, 3);
+            getline <$1;
+            printf("%s %s\n", file_key, $1);
+            if (match(file_key, /\.tar\.gz$/)) {
+                printf("%s %s\n", substr(file_key, 1, length(file_key)-7), $1);
+            }
+        }' \
+        > "${TMPDIR}"/shas.txt
+    popd >/dev/null
+  fi
 
   echo "\"\"\"A module containing a mapping of Rust tools to checksums"
   echo ""
@@ -62,9 +98,10 @@ emit_bzl_file_contents() {
   echo "\"\"\""
   echo ""
   echo "FILE_KEY_TO_SHA = {"
-  cat /tmp/reload_shas_shalist.txt | sed '/^[[:space:]]*$/d' | sort | awk '{print "    \"" $1 "\": \"" $2 "\","}'
+  cat "${TMPDIR}"/shas.txt | sed '/^[[:space:]]*$/d' | sort | awk '{print "    \"" $1 "\": \"" $2 "\","}'
   echo "}"
-  rm /tmp/reload_shas_shalist.txt
 }
 
+export TMPDIR="$(mktemp -d -t bazel_reload_shas_shalists)"
 echo "$(emit_bzl_file_contents $(enumerate_keys))" > "${BUILD_WORKSPACE_DIRECTORY}/rust/known_shas.bzl"
+rm -rf "${TMPDIR}"

commit 0175a1b7aaada0ea34c984c30c245da2a56e8d6c

fix for using a nightly channel after https://github.com/bazelbuild/rules_rust/commit/841fc6fb82141576d91aecb1d3f2656d58b0ab71 (#1302)
The commit https://github.com/bazelbuild/rules_rust/commit/841fc6fb82141576d91aecb1d3f2656d58b0ab71 for https://github.com/bazelbuild/rules_rust/pull/1284 broke the ability to use rust_register_toolchains with a nightly channel, see https://github.com/bazelbuild/rules_rust/commit/841fc6fb82141576d91aecb1d3f2656d58b0ab71#commitcomment-72310891.
I tracked the issue down to this line which was updated there. While using a dated nightly, these variables look like:
* tool_path:  `rust-nightly-x86_64-unknown-linux-gnu`
* tool_suburl:  `2022-04-06/rust-nightly-x86_64-unknown-linux-gnu`

Using tool_suburl causes the extract to fail like in the comment above:
`Prefix "2022-04-06/rust-nightly-x86_64-unknown-linux-gnu/rustc" was given, but not found in the archive. Here are possible prefixes for this archive: "rust-nightly-x86_64-unknown-linux-gnu".`

Switching back to the old behaviour fixes the glitch. I'm not sure if this breaks novel use cases introduced by https://github.com/bazelbuild/rules_rust/commit/841fc6fb82141576d91aecb1d3f2656d58b0ab71; @wt, could you please take a look.

On a sidenote, this is an instance where having some some build bot CI that uses nightly would have noticed this.

rust/private/repository_utils.bzl
@@ -570,7 +570,7 @@ def load_arbitrary_tool(ctx, tool_name, tool_subdirectories, version, iso_date, 
         ctx.extract(
             archive_path,
             output = "",
-            stripPrefix = "{}/{}".format(tool_suburl, subdirectory),
+            stripPrefix = "{}/{}".format(tool_path, subdirectory),
         )
 
 def _make_auth_dict(ctx, urls):