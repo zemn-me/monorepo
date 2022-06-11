9b83dcd63042f67ed97bba05976da6ca20751b68
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-06-10 19:53:05 -0600

    stoopid again

--- .github/scripts/fix_any_failing_fixable_tests.sh
+++ .github/scripts/fix_any_failing_fixable_tests.sh
@@ -4,7 +4,7 @@ BAZEL_CMD="${BAZEL_CMD:-yarn -s run bazel}"
 FIXER_TARGETS=$($BAZEL_CMD test \
     --test_summary=terse \
     --test_tag_filters=fixable \
-    $@ | \
+    $@ //... | \
     awk '{print $1}' | \
     sed 's/$/.fix/g')
 
