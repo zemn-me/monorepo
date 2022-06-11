acfb0cc2b5502dff663b865b3c72d38962c7b623
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-06-10 19:38:03 -0600

    im sorry

--- .github/scripts/fix_any_failing_fixable_tests.sh
+++ .github/scripts/fix_any_failing_fixable_tests.sh
@@ -3,7 +3,7 @@ BAZEL_CMD="${BAZEL_CMD:-yarn -s run bazel}"
 
 FIXER_TARGETS=$($BAZEL_CMD test \
     --test_summary=terse \
-    --test_tag_fitlers=fixable \
+    --test_tag_filters=fixable \
     $@ | \
     awk '{print $1}' | \
     sed 's/$/.fix/g')
