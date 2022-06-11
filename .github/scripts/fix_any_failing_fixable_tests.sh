a1d16c0f04816e6b2073df65b1515f056f19bfc6
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-06-10 19:20:11 -0500

    Update fix_any_failing_fixable_tests.sh

--- .github/scripts/fix_any_failing_fixable_tests.sh
+++ .github/scripts/fix_any_failing_fixable_tests.sh
@@ -2,16 +2,16 @@
 BAZEL_CMD="${BAZEL_CMD:-yarn -s run bazel}"
 
 FIXER_TARGETS=$($BAZEL_CMD test \
-    # Only print failing targets
+    # Only print failing targets \
     --test_summary=terse \
-    # Only test fixable targets
+    # Only test fixable targets \
     --test_tag_filers=fixable \
-    # Necessary for overriding filters on
-    # versioned branch
+    # Necessary for overriding filters on \
+    # versioned branch \
     $@ | \
-    # Get list of failing targets
+    # Get list of failing targets \
     awk '{print $1}' | \
-    # append '.fix' to fixable targets
+    # append '.fix' to fixable targets \
     sed 's/$/.fix/g')
 
 # Run all fixers
