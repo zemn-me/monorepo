686b2551a50b642742eda877e070fbfa0ac0ec39
Thomas Shadwell <thomas.shadwell@googlemail.com>
2022-07-08 12:28:34 -0700

    Merge in do sync.
    Also allow interfaces that extend a single type. This is NOT
    equivalent to that interface as the linter would suggest --
    alias types do not exist at type analysis time, and therefore
    cannot be self-referential, but interfaces can.

--- ts/do-sync/index.ts
+++ ts/do-sync/index.ts
@@ -1,2 +1,6 @@
 export * from './doSync';
+<<<<<<< HEAD
 export { default } from './doSync'
+=======
+export { default } from './doSync';
+>>>>>>> ab18ad9 (Merge in do sync.)
---
+++ ts/do-sync/testing/doSync.test.ts
@@ -0,0 +1,70 @@
+import { doSync, JSONObject } from 'ts/do-sync';
+import sharpT from 'sharp';
+
+const pixel =
+	'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==';
+const resized =
+	'/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAACAAIDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAABQf/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCDgB1If//Z';
+
+const fn = async (a: string, b: number, c: string) => `${a}${b}${c}`;
+
+interface resizeOpts extends sharpT.JpegOptions, JSONObject {
+	width: number;
+	height: number;
+}
+
+interface resizeRet extends JSONObject {
+	width: number;
+	height: number;
+	blob: string;
+}
+
+const resizeAsync = doSync(
+	async (
+		target: string,
+		{ width, height, ...jpegOpions }: resizeOpts
+	): Promise<resizeRet> => {
+		const sharp = require('sharp') as typeof sharpT;
+		const blob = (
+			await sharp(Buffer.from(target, 'base64'))
+				.resize(width, height)
+				.jpeg(jpegOpions)
+				.toBuffer()
+		).toString('base64');
+		return { blob, width, height };
+	}
+);
+
+describe('AsyncFn', () => {
+	test('resizeAsync', () => {
+		expect(
+			resizeAsync(pixel, {
+				width: 2,
+				height: 2,
+			})
+		).toEqual({
+			blob: resized,
+			width: 2,
+			height: 2,
+		});
+	});
+});
+
+describe('doSync', () => {
+	test('basic', () => {
+		expect(doSync(fn)('ok', 1, 'ok'));
+	});
+
+	test('error handling', () => {
+		expect(() =>
+			doSync(() => {
+				throw 'whoopsie';
+			})()
+		).toThrow();
+		expect(() =>
+			doSync(() => {
+				throw 'whoopsie';
+			})()
+		).toThrow('whoopsie');
+	});
+});
