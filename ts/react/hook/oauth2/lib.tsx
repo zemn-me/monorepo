390248cbc92e89798d0e8cd0b111975a7e98a9f3
zemnmez <zemnmez@googlemail.com>
2022-07-24 13:35:26 -0700

    react-oauth2-hook: a bit more refactoring

--- ts/react/hook/oauth2/lib.tsx
+++ ts/react/hook/oauth2/lib.tsx
@@ -44,8 +44,13 @@ export interface Options {
 interface Target {
 	authorizeUrl: string,
 	scope: string[],
-	clientID: string[]
+	clientID: string
 }
+
+const 
+	tokenScopingKey = "oauth-token",
+	stateScopingKey = "oauth-state";
+
 
 function targetScopingKey(t: Target): string {
 	return JSON.stringify(
@@ -59,90 +64,52 @@ function targetScopingKey(t: Target): string {
 	)
 }
 
-/**
- * useOAuth2Token is a React hook providing an OAuth2 implicit grant token.
- *
- * When useToken is called, it will attempt to retrieve an existing
- * token by the criteria of `{ authorizeUrl, scopes, clientID }`.
- * If a token by these specifications does not exist, the first
- * item in the returned array will be `undefined`.
- *
- * If the user wishes to retrieve a new token, they can call `getToken()`,
- * a function returned by the second parameter. When called, the function
- * will open a window for the user to confirm the OAuth grant, and
- * pass it back as expected via the hook.
- *
- * The OAuth token must be passed to a static endpoint. As
- * such, the `callbackUrl` must be passed with this endpoint.
- * The `callbackUrl` should render the [[OAuthCallback]] component,
- * which will securely verify the token and pass it back,
- * before closing the window.
- *
- * All instances of this hook requesting the same token and scopes
- * from the same place are synchronised. In concrete terms,
- * if you have many components waiting for a Facebook OAuth token
- * to make a call, they will all immediately update when any component
- * gets a token.
- *
- * Finally, in advanced cases the user can manually overwrite any
- * stored token by capturing and calling the third item in
- * the reponse array with the new value.
- * 
- * @example
- *const SpotifyTracks = () => {
- * const [token, getToken] = useOAuth2Token({
- *     authorizeUrl: "https://accounts.spotify.com/authorize",
- *     scope: ["user-library-read"],
- *     clientID: "abcdefg",
- *     redirectUri: document.location.origin + "/callback"
- * })
- *
- * const [response, setResponse] = React.useState()
- * const [error, setError] = React.useState()
- *
- * // when we get a token, query spotify
- * React.useEffect(() => {
- *     if (token == undefined) {return}
- *     fetch('https://api.spotify.com/v1/me/tracks', {
- *         headers: {
- *             Authorization: `Bearer ${token}`
- *         }
- *     }).then(
- *         json => response.json()
- *     ).then(
- *         data => setResponse(data)
- *     ).catch(
- *         error => setError(error)
- *     )
- * }, [token])
- *
- * if (!token || error) return <div onClick={getToken}> login with Spotify </div>
- *
- *return <div>
- * Your saved tracks on Spotify: {JSON.stringify(response)}
- *</div>
- *}
- */
-export function useOAuth2Token(options: Options): [token: OAuthToken | undefined, getToken: getToken, setToken: setToken ]
+function joinScopingKeys(...keys: string[]): string {
+	return keys.join("-");
+}
+
+function tokenKey(storageKey: string, t: Target) {
+	return joinScopingKeys(
+		storageKey,
+		tokenScopingKey,
+		targetScopingKey(t)
+	)
+}
+
+
+function stateKey(storageKey: string, t: Target) {
+	return joinScopingKeys(
+		storageKey,
+		stateScopingKey,
+		targetScopingKey(t)
+	)
+}
+
+function stateToken(t: Target) {
+	return joinScopingKeys(
+		cryptoRandomString(),
+		targetScopingKey(t)
+	)
+
+}
 
 export function useOAuth2Token({
 	authorizeUrl,
 	scope = [],
 	redirectUri,
 	clientID,
-	storageKey: string
-}: Options): [OAuthToken | undefined, getToken, setToken] {
-	const target = {
-		authorizeUrl,
-		scope,
-		clientID,
-	};
+	storageKey
+}: Options): [token: OAuthToken | undefined, getAuthorizeUrl: () => string, setToken: (tok: string) => void] {
 
-	const [token, setToken] = useStorage<string>(
-		storagePrefix + '-' + JSON.stringify(target)
-	);
+	const [token, setToken] = useStorageState<string>(
+		localStorage, tokenKey(storageKey, {authorizeUrl, scope, clientID} ));
 
-	let [state, setState] = useStorage<string>(oauthStateName);
+	let [stateToken, setStateToken] = useStorageState<string>(localStorage,
+		stateKey(storageKey, { authorizeUrl, scope, clientID}));
+
+	const getAuthorizeUrl = () => {
+
+	}
 
 	const getToken = () => {
 		setState(
