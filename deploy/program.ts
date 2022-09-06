3be4badf480d0193178c39872a21a5945da99149
zemnmez <zemnmez@googlemail.com>
2022-09-06 11:24:22 -0700

    fixes

--- deploy/program.ts
+++ deploy/program.ts
@@ -269,7 +269,9 @@ export const releaseNotes =
 			for (const [, data] of pulumi_deploys) {
 				const d = data;
 				if (!d) {
-					paragraphs.push(`A Pulumi deploy may have happened. It didn't give us any information, though.`);
+					paragraphs.push(
+						`A Pulumi deploy may have happened. It didn't give us any information, though.`
+					);
 					continue;
 				}
 				const items = [d.stdout, d.stderr].filter(isDefined);
