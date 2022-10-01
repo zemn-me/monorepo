777b7f837c88bc765aec4eeddaa92d57e90eecb4
zemnmez <zemnmez@gmail.com>
2022-10-01 11:18:39 +0000

    it works!!!!!

--- ts/next.js/testing/example/next_entry.ts
+++ ts/next.js/testing/example/next_entry.ts
@@ -1,45 +1,35 @@
 import { runfiles } from '@bazel/runfiles';
 import { spawnSync } from 'child_process';
 import * as fs from 'fs';
-import { promisify } from 'util';
 
 
 
-const { spawn } = require('child_process')
-//const entry = 'node_modules/next/dist/bin/next';
 
-
-
-//const next = runfiles.resolve('npm/next/bin/next.sh');
-const files = runfiles.resolve('monorepo/ts/next.js/testing/example');
 const entry = require.resolve(`next/dist/bin/next`);
 const build_target = runfiles.resolveWorkspaceRelative('ts/next.js/testing/example');
-console.log('entry', entry, 'build_target', build_target);
 
-//throw new Error(`next, ${next} files ${files} cwd ${process.cwd()}, bin ${process.env.BAZEL_BINDIR}`);
-//throw new Error(spawnSync('ls', ['../../../../../external']).output.toString())
 
 
 const args = process.argv
-	.slice(2)
-	.concat(['build', '--debug', build_target ]);
+	.slice(2);
 
 const spawnOptions = {
 	shell: process.env.SHELL,
 	stdio: 'inherit',
 };
 
-const res = spawnSync(entry, args, spawnOptions as any);
-if (res.status === null) {
+const res1 = spawnSync(entry, args.concat(['build', build_target]), spawnOptions as any);
+if (res1.status === null) {
 	// Process can fail with a null exit-code (e.g. OOM), handle appropriately
-	throw new Error(`Process terminated unexpectedly: ${res.signal}`);
+	throw new Error(`Process terminated unexpectedly: ${res1.signal}`);
+}
+const res2 = spawnSync(entry, args.concat(['export', build_target]), spawnOptions as any);
+if (res2.status === null) {
+	// Process can fail with a null exit-code (e.g. OOM), handle appropriately
+	throw new Error(`Process terminated unexpectedly: ${res2.signal}`);
 }
 
-
-//fs.renameSync(build_target + '/build', './ts/next.js/example/build')
-console.error(spawnSync('ls',[ build_target], { stdio: 'inherit' }).output.toString());
-console.error('cwd', process.cwd());
-fs.renameSync(build_target + '/build', process.env.OUT!);
+fs.renameSync(build_target + '/out', process.env.OUT!);
 
 
-process.exit(res.status);
+process.exit(res1.status || res2.status);
