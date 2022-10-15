7712d759ce6c98662ad049385cdfb6c83fe5fe6e
zemnmez <zemnmez@googlemail.com>
2022-10-15 08:32:08 -0700

    make it build!

--- ts/next.js/index.tsx
+++ ts/next.js/index.tsx
@@ -4,11 +4,11 @@ export * as config from 'monorepo/ts/next.js/next.config';
 
 export function HeaderTags() {
 	return (
-        <Head>
-		<meta
-			content="default-src 'self'"
-			httpEquiv="Content-Security-Policy"
-		/>
-        </Head>
+		<Head>
+			<meta
+				content="default-src 'self'"
+				httpEquiv="Content-Security-Policy"
+			/>
+		</Head>
 	);
 }
