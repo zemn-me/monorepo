81baeb25ae12c2d0aecb7f035eb47814c111d624
zemnmez <zemnmez@googlemail.com>
2022-10-16 09:36:26 -0700

    automatic fixes

--- git/head.ts
+++ git/head.ts
@@ -2,9 +2,10 @@ import HEAD from 'monorepo/git/head_ref';
 
 /**
  * The HEAD ref of this git monorepo.
- * 
+ *
  * This is a generated file, and typescript may not be aware of it.
  */
 export const ref = HEAD;
 
-export const link = `https://github.com/Zemnmez/monorepo/commit/${ref}` as const;
+export const link =
+	`https://github.com/Zemnmez/monorepo/commit/${ref}` as const;
--- ts/pulumi/dog/pleaseintroducemetoyour/public/static/pages/index.tsx
+++ ts/pulumi/dog/pleaseintroducemetoyour/public/static/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import * as git from 'monorepo/git';
+import Head from 'next/head';
 
 export default function Main() {
 	return (
