5cfbe7fbd390018890799381049634669b995524
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2022-11-23 19:16:23 +0000

    remove naked 'class' identifier

--- rs/css/module/testing/basic.output.golden.ts
+++ rs/css/module/testing/basic.output.golden.ts
@@ -1,13 +1,14 @@
 import "./basic.output.compiled.css";
-export const class = 'class__rs/css/module/testing/basic.module.css';
+export const base64 = 'base64__rs/css/module/testing/basic.module.css';
 export const classDuplicateUrl = 'class-duplicate-url__rs/css/module/testing/basic.module.css';
+export const class = 'class__rs/css/module/testing/basic.module.css';
 export const on = 'on__rs/css/module/testing/basic.module.css';
 export const top = 'top__rs/css/module/testing/basic.module.css';
 export const grid = 'grid__rs/css/module/testing/basic.module.css';
 export const middle = 'middle__rs/css/module/testing/basic.module.css';
 export const myStyle = 'myStyle__rs/css/module/testing/basic.module.css';
-export const test = 'test__rs/css/module/testing/basic.module.css';
+export const someClass = 'some-class__rs/css/module/testing/basic.module.css';
 export const bottom = 'bottom__rs/css/module/testing/basic.module.css';
+export const test = 'test__rs/css/module/testing/basic.module.css';
 export const light = 'light__rs/css/module/testing/basic.module.css';
-export const base64 = 'base64__rs/css/module/testing/basic.module.css';
 export const bulb = 'bulb__rs/css/module/testing/basic.module.css';
