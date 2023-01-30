2a37430d02c211c82f059cd24e87a0fd7341d9e1
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-01-30 14:43:10 -0800

    Automatically bump major version on all new releases.
    This is necessary such that the new versioning code (which is based on VERSION/*) doesn't
    immediately refuse to push new versions (new versions will only be pushed if they don't already
    exist).

--- bzl/versioning/bump.py
+++ bzl/versioning/bump.py
@@ -4,11 +4,11 @@ import subprocess
 import shutil
 
 parser = argparse.ArgumentParser(description="Performs the action of a version bump.")
-parser.add_argument('--to_bump_in', help="The version file to bump, as a root-relative path.", type=str)
-parser.add_argument('--to_bump_out', help="The version file to bump, as a root-relative path.", type=str)
-parser.add_argument('--lockfile_build_label', help="A label that points to the generated (new) version lockfile.", type=str)
-parser.add_argument('--lockfile_build_rootpath', help="The path from the repo root that the lockfile is generated into", type=str)
-parser.add_argument('--lockfile_out_rootpath', help="The location to place the newly minted version lockfile at.", type=str)
+parser.add_argument('--to_bump_in', required=True, help="The version file to bump, as a root-relative path.", type=str)
+parser.add_argument('--to_bump_out', required=True, help="The version file to bump, as a root-relative path.", type=str)
+parser.add_argument('--lockfile_build_label', required=False, help="A label that points to the generated (new) version lockfile.", type=str)
+parser.add_argument('--lockfile_build_rootpath', required=False help="The path from the repo root that the lockfile is generated into", type=str)
+parser.add_argument('--lockfile_out_rootpath', required=False, help="The location to place the newly minted version lockfile at.", type=str)
 
 # This happens directly on the real workspace -- also, needs to be
 # run from bazel to have this set.
@@ -26,10 +26,11 @@ with open(args.to_bump_out, mode='w', encoding='utf-8') as f:
     f.write(str(number+1))
 
 # Once the version has been bumped, generate the new version bump file.
-subprocess.run(["bazelisk", "build", args.lockfile_build_label])
+if args.lockfile_build_label != None and args.lockfile_build_rootpath != None and args.lockfile_out_rootpath != None:
+    subprocess.run(["bazelisk", "build", args.lockfile_build_label])
 
-# Copy the newly created lockfile across
-shutil.copyfile(
-    os.path.join("dist", "bin", args.lockfile_build_rootpath), 
-    args.lockfile_out_rootpath
-)
+    # Copy the newly created lockfile across
+    shutil.copyfile(
+        os.path.join("dist", "bin", args.lockfile_build_rootpath), 
+        args.lockfile_out_rootpath
+    )
