43ca1ca340ab95bbffe5a1a63f32162e24033a4c
zemnmez <zemnmez@googlemail.com>
2023-03-04 15:19:39 -0800

    fiddle with the rust main a little

--- project/airlock-loadout/src/main.rs
+++ project/airlock-loadout/src/main.rs
@@ -1,19 +1,8 @@
-{% unless std %}#![no_std]
-#![no_main]
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-{% endunless %}use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-
-{% unless std %}use log::*;
-
-#[no_mangle]
-{% endunless %}fn main() {
+fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
-{% if std %}
-    println!("Hello, world!");{% else %}
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    info!("Hello, world!");{% endif %}
+    println!("Hello, world!");
 }
