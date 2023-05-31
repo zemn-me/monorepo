d9ab2e6879ec9f5dbae44cd85a434fde36f1013c
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-05-31 17:36:40 +0900

    Fix asset paths for pulumi.

--- ts/pulumi/im/shadwell/thomas/public/index.ts
+++ ts/pulumi/im/shadwell/thomas/public/index.ts
@@ -1,12 +1,9 @@
 import * as staticwebsite from '@pulumi/aws-static-website';
-import * as asset from '@pulumi/pulumi/asset';
 import * as cert from 'ts/pulumi/im/shadwell/cert';
 
 export const site = new staticwebsite.Website('thomas.shadwell.im', {
 	withCDN: true,
-	indexHTML: new asset.FileAsset(
-		'ts/pulumi/im/shadwell/thomas/public/index.html'
-	).path,
+	indexHTML: 'index.html',
 	sitePath: 'ts/pulumi/im/shadwell/thomas/public',
 	targetDomain: 'thomas.shadwell.im',
 	certificateARN: cert.arn,
