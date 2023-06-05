d64766709b26615f18c8a097c43c1fc6c84548ef
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-05 15:18:01 +0900

    Introduce my own pulumi Website component. I didn't want to have to do this, but have had to becuase of https://github.com/pulumi/pulumi-aws-static-website/issues/31.
    It's clear that doing this will be useful anyway, so it doesn't seem like a terrible idea. In particular, I can get it to also provision SSL certificates.
    
    Good references for this is:
    
    https://github.com/pulumi/pulumi-aws-static-website/blob/main/sdk/nodejs/website.ts
    
    and
    
    https://www.pulumi.com/learn/abstraction-encapsulation/component-resources/

--- ts/pulumi/dog/pleaseintroducemetoyour/index.ts
+++ ts/pulumi/dog/pleaseintroducemetoyour/index.ts
@@ -1,3 +1,2 @@
-export * from 'ts/pulumi/dog/pleaseintroducemetoyour/cert';
 export * as Public from 'ts/pulumi/dog/pleaseintroducemetoyour/public';
 export * from 'ts/pulumi/dog/pleaseintroducemetoyour/zone';
