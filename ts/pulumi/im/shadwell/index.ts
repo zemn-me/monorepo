bffdb776c010ca04efaa49ad34e27050efc88caf
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-05 15:39:34 +0900

    Introduce my own pulumi Website component. I didn't want to have to do this, but have had to becuase of https://github.com/pulumi/pulumi-aws-static-website/issues/31.
    It's clear that doing this will be useful anyway, so it doesn't seem like a terrible idea. In particular, I can get it to also provision SSL certificates.
    
    Good references for this is:
    
    https://github.com/pulumi/pulumi-aws-static-website/blob/main/sdk/nodejs/website.ts
    
    and
    
    https://www.pulumi.com/learn/abstraction-encapsulation/component-resources/

--- ts/pulumi/im/shadwell/index.ts
+++ ts/pulumi/im/shadwell/index.ts
@@ -1,3 +1,2 @@
-export * from 'ts/pulumi/im/shadwell/cert';
 export * from 'ts/pulumi/im/shadwell/thomas';
 export * from 'ts/pulumi/im/shadwell/zone';
--- ts/pulumi/me/zemn/staging/index.ts
+++ ts/pulumi/me/zemn/staging/index.ts
@@ -1,56 +1,10 @@
-import * as aws from '@pulumi/aws';
-import * as staticwebsite from '@pulumi/aws-static-website';
-import * as pulumi from '@pulumi/pulumi';
+import Website from 'ts/pulumi/lib/website';
 import * as zone from 'ts/pulumi/me/zemn/zone';
 
-const domainName = pulumi.interpolate`staging.${zone.zone.name}`;
-
-export const cert = new aws.acm.Certificate('staging.zemn.me_cert', {
-	domainName: domainName,
-	validationMethod: 'DNS',
-});
-
-const second = 1;
-const minute = 60 * second;
-
-// this code seems stupid but you can't iterate over a list
-// in pulumi outputs so it's necessary.
-
-// NB: number of validations is the number of domains https://github.com/pulumi/pulumi/issues/5736#issuecomment-725767836
-
-const record = (
-	name: string,
-	validation: pulumi.Output<aws.types.output.acm.CertificateDomainValidationOption>
-) =>
-	new aws.route53.Record(name, {
-		name: validation.resourceRecordName,
-		records: [validation.resourceRecordValue],
-		type: validation.resourceRecordType,
-		zoneId: zone.zone.zoneId,
-		ttl: 1 * minute, // because these really don't need to be cached
-	});
-
-export const validation0 = record(
-	'zemn_me_staging_cert_validation_0',
-	cert.domainValidationOptions[0]
-);
-
-export const validation = new aws.acm.CertificateValidation(
-	'zemn_me_staging_cert-validation',
-	{
-		certificateArn: cert.arn,
-		validationRecordFqdns: [validation0.fqdn],
-	}
-);
-
-// should use this one because it waits for the certificate to actualy be validated
-export const arn = validation.certificateArn;
-
-export const site = new staticwebsite.Website('staging.zemn.me', {
-	withCDN: true,
-	indexHTML: 'index.html',
-	error404: '404.html',
-	sitePath: 'project/zemn.me/next/out',
-	targetDomain: 'staging.zemn.me',
-	certificateARN: arn,
+export const site = new Website('staging.zemn.me', {
+	index: 'project/zemn.me/next/out/index.html',
+	notFound: 'project/zemn.me/next/out/404.html',
+	directory: 'project/zemn.me/next/out',
+	zone: zone.zone,
+	subDomain: 'staging',
 });
