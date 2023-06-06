da60f85773b45b54acfba300e7b21e7a705b8d05
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-06 17:58:11 +0900

    Timeline for zemn.me

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import * as bio from 'project/zemn.me/bio';
 import { TimeEye } from 'project/zemn.me/elements/TimeEye';
+import Timeline from 'project/zemn.me/next/pages/timeline';
 import * as lang from 'ts/react/lang';
 
 export default function Main() {
@@ -24,17 +25,19 @@ export default function Main() {
 			<main>
 				<section>
 					<header>
-						<TimeEye />
 						<h2 lang={lang.get(bio.Bio.who.fullName)}>
 							{lang.text(bio.Bio.who.fullName)}
 						</h2>
+						<TimeEye />
+						<p>I am a recognised international expert on computer security, with specialisms in web security, security program construction, and automated security analysis.</p>
+						<p>I am interested in using my expertise as a consultant to the law industry. For business, email me at <a href="mailto:thomas@shadwell.im">thomas@shadwell.im</a>.</p>
 					</header>
 					{bio.Bio.links !== undefined ? (
 						<nav>
 							{bio.Bio.links.map(([text, url]) => (
 								<a
 									href={url.toString()}
-									key={lang.get(text)}
+									key={url.toString()}
 									lang={lang.get(text)}
 								>
 									{lang.text(text)}
@@ -43,7 +46,10 @@ export default function Main() {
 						</nav>
 					) : null}
 				</section>
-				<TimeEye />
+				<section>
+					{ /* I should do one of those fancy dotted-line header breaks here from zemn.me */}
+					<Timeline />
+				</section>
 			</main>
 		</>
 	);
