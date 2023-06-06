f16e53b3cb7766f6f211edc52e4a9b34fdda1ae4
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-06 11:59:31 +0900

    Sort timeline for zemn.me

---
+++ project/zemn.me/next/pages/timeline.tsx
@@ -0,0 +1,48 @@
+import Immutable from 'immutable';
+import * as Bio from 'project/zemn.me/bio';
+
+/**
+ * Given a map that contains a set, set a single item, expanding or creating
+ * the set if needed.
+ * @param v the map
+ * @param key the key in the map
+ * @param value the item to set
+ * @returns the same map that was put in
+ */
+function setManyMap<K, V>(
+	v: Immutable.Map<K, Immutable.Set<V>>,
+	key: K,
+	value: V
+): Immutable.Map<K, Immutable.Set<V>> {
+	return v.set(key, v.get(key, Immutable.Set<V>()).add(value));
+}
+
+function groupBy<T, Q>(
+	i: Iterable<T>,
+	select: (v: T) => Q
+): Immutable.Map<Q, Immutable.Set<T>> {
+	let m = Immutable.Map<Q, Immutable.Set<T>>();
+	for (const v of i) m = setManyMap(m, select(v), v);
+
+	return m;
+}
+
+const byYearByMonth = groupBy(Bio.Bio.timeline, t => t.date.getFullYear())
+	.map(v =>
+		groupBy(v, e => e.date.getMonth())
+			.toOrderedMap()
+			.sortBy(
+				(_, k) => k,
+				(a, b) => a - b
+			)
+	)
+	.toOrderedMap()
+	.sortBy(
+		(_, k) => k,
+		(a, b) => a - b
+	);
+
+export default function Timeline() {
+	console.log(byYearByMonth.toJS());
+	return JSON.stringify(byYearByMonth.toJS());
+}
