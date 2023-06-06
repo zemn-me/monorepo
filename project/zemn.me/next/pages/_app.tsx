7c0992072271e7e7016a8c64945805777d104c9c
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-06 17:33:19 +0900

    Timeline for zemn.me

--- project/zemn.me/next/pages/_app.tsx
+++ project/zemn.me/next/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { HeaderTags } from 'ts/next.js';
+import 'project/zemn.me/next/pages/base.css';
 
 export function App({ Component, pageProps }: AppProps) {
 	return (
@@ -11,6 +12,7 @@ export function App({ Component, pageProps }: AppProps) {
 				<meta content="@zemnmez" name="twitter:site" />
 				<meta content="@zemnnmez" name="twitter:creator" />
 				<meta content="zemnmez" name="author" />
+				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
 			</Head>
 		</>
 	);
