c2b065efcf36f3f422fcafd94075054b3292e364
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-17 01:01:50 +0800

    Project zemn.me: continue work on site.

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
+import Header from 'project/zemn.me/next/components/header';
 import * as bio from 'project/zemn.me/bio';
-import { TimeEye } from 'project/zemn.me/elements/TimeEye';
-import Timeline from 'project/zemn.me/next/pages/timeline';
+import Timeline from 'project/zemn.me/next/components/timeline';
 import * as lang from 'ts/react/lang';
 import * as kenwood from 'project/zemn.me/next/assets/kenwood';
 import style from 'project/zemn.me/next/pages/index.module.css';
@@ -23,33 +23,10 @@ export default function Main() {
 				<kenwood.VideoSources/>
 			</video>
 			<header className={style.banner}>
-				<h1 lang={lang.get(bio.Bio.who.handle)}>
-					{lang.text(bio.Bio.who.handle)}
-				</h1>
 			</header>
 				<section className={style.content}>
-					<header>
-						<h2 lang={lang.get(bio.Bio.who.fullName)}>
-							{lang.text(bio.Bio.who.fullName)}
-						</h2>
-						<TimeEye />
-						<p>I am an internationally recognised international expert on computer security, with specialisms in web security, security program construction, and automated security analysis.</p>
-						<p>I am interested in consulting on legal cases. For business, email me at <a href="mailto:thomas@shadwell.im">thomas@shadwell.im</a>.</p>
-					</header>
-					{bio.Bio.links !== undefined ? (
-						<nav>
-							{bio.Bio.links.map(([text, url]) => (
-								<a
-									href={url.toString()}
-									key={url.toString()}
-									lang={lang.get(text)}
-								>
-									{lang.text(text)}
-								</a>
-							))}
-						</nav>
-					) : null}
-					<section>
+					<Header className={style.header}/>
+					<section className={style.timeline}>
 						<Timeline />
 					</section>
 				</section>
