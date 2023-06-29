3504d2efc3e0017b5f0a4e81a6c061c0a69a62bb
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-29 15:59:02 +0100

    Abstract the line divider pattern to LineDivider.

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -5,6 +5,7 @@ import Timeline from 'project/zemn.me/next/components/timeline';
 import * as lang from 'ts/react/lang';
 import * as kenwood from 'project/zemn.me/next/assets/kenwood';
 import style from 'project/zemn.me/next/pages/index.module.css';
+import ZemnmezLogo from 'project/zemn.me/elements/ZemnmezLogo/ZemnmezLogo';
 
 export default function Main() {
 	return (
@@ -29,6 +30,9 @@ export default function Main() {
 					<section className={style.timeline}>
 						<Timeline />
 					</section>
+					<footer>
+						<ZemnmezLogo/>
+					</footer>
 				</section>
 			</main>
 	);
