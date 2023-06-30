6eea5537ef6891cde33f0eb4c45f0a574b7ea407
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-06-30 16:16:09 +0100

    project/zemn.me: layout of main page

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -2,12 +2,13 @@ import Head from 'next/head';
 import * as bio from 'project/zemn.me/bio';
 import { TimeEye } from 'project/zemn.me/elements/TimeEye';
 import * as kenwood from 'project/zemn.me/next/assets/kenwood';
+import style from 'project/zemn.me/next/pages/index.module.css';
 import Timeline from 'project/zemn.me/next/pages/timeline';
 import * as lang from 'ts/react/lang';
 
 export default function Main() {
 	return (
-		<>
+		<main className={style.main}>
 			<Head>
 				<title lang={lang.get(bio.Bio.who.handle)}>
 					{lang.text(bio.Bio.who.handle)}
@@ -18,60 +19,59 @@ export default function Main() {
 				/>
 			</Head>
 
-			<header>
-				<video
-					autoPlay
-					loop
-					muted
-					playsInline
-					poster={kenwood.poster.src}
-				>
-					<kenwood.VideoSources />
-				</video>
+			<video
+				autoPlay
+				className={style.headerBgv}
+				loop
+				muted
+				playsInline
+				poster={kenwood.poster.src}
+			>
+				<kenwood.VideoSources />
+			</video>
+			<header className={style.banner}>
 				<h1 lang={lang.get(bio.Bio.who.handle)}>
 					{lang.text(bio.Bio.who.handle)}
 				</h1>
 			</header>
-			<main>
-				<section>
-					<header>
-						<h2 lang={lang.get(bio.Bio.who.fullName)}>
-							{lang.text(bio.Bio.who.fullName)}
-						</h2>
-						<TimeEye />
-						<p>
-							I am an internationally recognised international
-							expert on computer security, with specialisms in web
-							security, security program construction, and
-							automated security analysis.
-						</p>
-						<p>
-							I am interested in consulting on legal cases. For
-							business, email me at{' '}
-							<a href="mailto:thomas@shadwell.im">
-								thomas@shadwell.im
+			<section className={style.content}>
+				<header>
+					<h2 lang={lang.get(bio.Bio.who.fullName)}>
+						{lang.text(bio.Bio.who.fullName)}
+					</h2>
+					<TimeEye />
+					<p>
+						I am an internationally recognised international expert
+						on computer security, with specialisms in web security,
+						security program construction, and automated security
+						analysis.
+					</p>
+					<p>
+						I am interested in consulting on legal cases. For
+						business, email me at{' '}
+						<a href="mailto:thomas@shadwell.im">
+							thomas@shadwell.im
+						</a>
+						.
+					</p>
+				</header>
+				{bio.Bio.links !== undefined ? (
+					<nav>
+						{bio.Bio.links.map(([text, url]) => (
+							<a
+								href={url.toString()}
+								key={url.toString()}
+								lang={lang.get(text)}
+							>
+								{lang.text(text)}
 							</a>
-							.
-						</p>
-					</header>
-					{bio.Bio.links !== undefined ? (
-						<nav>
-							{bio.Bio.links.map(([text, url]) => (
-								<a
-									href={url.toString()}
-									key={url.toString()}
-									lang={lang.get(text)}
-								>
-									{lang.text(text)}
-								</a>
-							))}
-						</nav>
-					) : null}
-				</section>
+						))}
+					</nav>
+				) : null}
 				<section>
 					<Timeline />
 				</section>
-			</main>
-		</>
+			</section>
+		</main>
 	);
 }
