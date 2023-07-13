0a302fbf9c7a73d830b91be5e6d29f0104776403
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-07-13 19:11:48 +0000

    Allow the fix_all script to take a $0 pattern argument.

--- bzl/fix_api/fix_all.sh
+++ bzl/fix_api/fix_all.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
+PATTERN="${1:-//...}"
+
 if [ ! -z  "$BUILD_WORKING_DIRECTORY" ]; then
     pushd $BUILD_WORKING_DIRECTORY
 fi
 
-bazel query //... | \
+bazel query $PATTERN | \
 grep '.fix$' | \
 sed 's/\.fix$//g' | \
 xargs bazel test --keep_going | \
