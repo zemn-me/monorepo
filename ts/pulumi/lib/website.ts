e29eb6b1d0c49e49f6e87e6b1863714fbf9d34fa
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-03 13:12:06 -0700

    Back out "Implement noIndex for staging builds."
    For reasons currently unknown -- if a test returns a neutral status then automerge still merges!
    
    As a result, the pulumi code here fails and yet the merge still happened.
    
    Original commit changeset: c37300567dc9

--- ts/pulumi/lib/website.ts
+++ ts/pulumi/lib/website.ts
@@ -70,11 +70,6 @@ export interface Args {
 	 * The 404 document to serve.
 	 */
 	notFound?: string;
-
-	/**
-	 * Prevent search engines from indexing.
-	 */
-	noIndex: boolean;
 }
 
 /**
@@ -220,28 +215,6 @@ export class Website extends pulumi.ComponentResource {
 			{ parent: this }
 		);
 
-		// response headers policy (http headers)
-
-		const responseHeadersPolicy = new aws.cloudfront.ResponseHeadersPolicy(
-			`${name}_response_headers`,
-			{
-				comment: 'used for noindex on our cloudfront distributions',
-				customHeadersConfig: {
-					items: [
-						...(args.noIndex
-							? [
-									{
-										header: 'X-Robots-Tag',
-										value: 'noindex',
-										override: false,
-									},
-							  ]
-							: []),
-					],
-				},
-			}
-		);
-
 		// create the cloudfront
 
 		const distribution = new aws.cloudfront.Distribution(
@@ -278,7 +251,6 @@ export class Website extends pulumi.ComponentResource {
 					  }
 					: {}),
 				defaultCacheBehavior: {
-					responseHeadersPolicyId: responseHeadersPolicy.id,
 					// i dont think we use most of these but it's probably not
 					// important
 					allowedMethods: [
