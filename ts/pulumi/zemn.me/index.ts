87237666eb8cdca2704e035d330c228fb34c7c47
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-02 20:30:08 -0700

    Implement the new submit workflow.

--- ts/pulumi/zemn.me/index.ts
+++ ts/pulumi/zemn.me/index.ts
@@ -1,28 +1,35 @@
 import * as Pulumi from '@pulumi/pulumi';
 import Website from 'ts/pulumi/lib/website';
-import * as zone from 'ts/pulumi/me/zemn/zone';
 
-
-export interface Args { }
+export interface Args {
+	zoneId: Pulumi.Input<string>;
+	domain: string;
+}
 
 export class Component extends Pulumi.ComponentResource {
-    site: Website;
-    constructor(
-        name: string,
-        args: Args,
-        opts?: Pulumi.ComponentResourceOptions
-    ) {
-        super("ts::pulumi::shadwell.im", name, args, opts);
+	site: Website;
+	constructor(
+		name: string,
+		args: Args,
+		opts?: Pulumi.ComponentResourceOptions
+	) {
+		super('ts:pulumi:shadwell.im', name, args, opts);
 
-        this.site = new Website('staging.zemn.me', {
-            index: 'project/zemn.me/next/out/index.html',
-            notFound: 'project/zemn.me/next/out/404.html',
-            directory: 'project/zemn.me/next/out',
-            zone: zone.zone,
-            subDomain: 'staging',
-        }, { parent: this });
+		this.site = new Website(
+			'staging.zemn.me',
+			{
+				index: 'project/zemn.me/next/out/index.html',
+				notFound: 'project/zemn.me/next/out/404.html',
+				directory: 'project/zemn.me/next/out',
+				zoneId: args.zoneId,
+				// this may look weird, but I don't want to replace
+				// what's already there until it's ready; so this will double stage
+				// to staging.staging.zemn.me.
+				domain: ['staging', args.domain].join('.'),
+			},
+			{ parent: this }
+		);
 
-        super.registerOutputs({ site: this.site });
-    }
-
+		super.registerOutputs({ site: this.site });
+	}
 }
