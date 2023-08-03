ab65c3825cf51e966d2ddf07ae2af5c095015fe3
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-02 20:39:53 -0700

    Implement the new submit workflow.

--- ts/pulumi/index.ts
+++ ts/pulumi/index.ts
@@ -1,94 +1,80 @@
+import * as aws from '@pulumi/aws';
 import * as Pulumi from '@pulumi/pulumi';
 import * as PleaseIntroduceMeToYourDog from 'ts/pulumi/pleaseintroducemetoyour.dog';
-import * as ZemnMe from 'ts/pulumi/zemn.me';
 import * as ShadwellIm from 'ts/pulumi/shadwell.im';
-import * as aws from '@pulumi/aws';
-
-interface StagableZoneArgs {
-    staging: boolean,
-    /**
-     * The name for the hosted zone.
-     */
-    name: string
-}
-
-/**
- * A staging-aware Zone. If the 'staging' arg is true,
- * the constructed zone is `staging.${name}`.
- */
-class StagableZone extends Pulumi.ComponentResource {
-    zone: aws.route53.Zone;
-    constructor(
-        name: string,
-        args: StagableZoneArgs,
-        opts?: Pulumi.ComponentResourceOptions
-    ) {
-        super("ts::pulumi::StagableZone", name, args, opts);
-
-        this.zone = new aws.route53.Zone(`${name}_zone`,{
-            name:
-                args.staging
-                    ? `staging.${args.name}`
-                    : args.name
-        }, { parent: this })
-
-        super.registerOutputs({
-            zone: this.zone
-        })
-
-    }
-}
-
+import * as ZemnMe from 'ts/pulumi/zemn.me';
 
 export interface Args {
-    staging: boolean
+	staging: boolean;
 }
 
 /**
  * The Pulumi infrastructure.
  */
 export class Component extends Pulumi.ComponentResource {
-    pleaseIntroduceMeToYourDog: PleaseIntroduceMeToYourDog.Component;
-    zemnMe: ZemnMe.Component;
-    shadwellIm: ShadwellIm.Component;
-    constructor(
-        name: string,
-        args: Args,
-        opts?: Pulumi.ComponentResourceOptions
-    ) {
-        super("ts::pulumi", name, args, opts);
+	pleaseIntroduceMeToYourDog: PleaseIntroduceMeToYourDog.Component;
+	zemnMe: ZemnMe.Component;
+	shadwellIm: ShadwellIm.Component;
+	constructor(
+		name: string,
+		args: Args,
+		opts?: Pulumi.ComponentResourceOptions
+	) {
+		super('ts:pulumi:Component', name, args, opts);
 
-        const pleaseIntroduceMeToYourDogZone = new StagableZone(`${name}_pleaseintroducemetoyour.dog_zone`, {
-            name: 'pleaseintroducemetoyour.dog',
-            staging: args.staging
-        }, { parent: this });
+		// i think pulumi kinda fucks up a little here, because you can totally
+		// register hosted zones with duplicate names (and I have committed this crime)
+		const zone = {
+			im: {
+				shadwell: aws.route53.getZone({
+					zoneId: 'Z0819383I2XV8V6UY6NO',
+				}),
+			},
+			me: {
+				zemn: aws.route53.getZone({ zoneId: 'ZG99IJ8QHXTSI' }),
+			},
+			dog: {
+				pleaseintroducemetoyour: aws.route53.getZone({
+					zoneId: 'Z0627984320AKGYW3FTZ5',
+				}),
+			},
+		};
 
-        this.pleaseIntroduceMeToYourDog = new PleaseIntroduceMeToYourDog.Component(`${name}_pleaseintroducemetoyour.dog`, {
-            zone: pleaseIntroduceMeToYourDogZone.zone
-        }, { parent: this });
+		const stage = (s: string) =>
+			[...(args.staging ? ['staging'] : []), s].join('.');
 
-        const zemnMeZone = new StagableZone(`${name}_zemn.me_zone`, {
-            name: 'zemn.me',
-            staging: args.staging
-        }, { parent: this });
+		this.pleaseIntroduceMeToYourDog =
+			new PleaseIntroduceMeToYourDog.Component(
+				`${name}_pleaseintroducemetoyour.dog`,
+				{
+					zoneId: Pulumi.output(
+						zone.dog.pleaseintroducemetoyour.then(z => z.id)
+					),
+					domain: stage('pleaseintroducemetoyour.dog'),
+				},
+				{ parent: this }
+			);
 
-        this.zemnMe = new ZemnMe.Component(`${name}_zemn.me`, {
-            zone: zemnMeZone.zone
-        }, { parent: this });
+		this.zemnMe = new ZemnMe.Component(
+			`${name}_zemn.me`,
+			{
+				zoneId: Pulumi.output(zone.me.zemn.then(z => z.id)),
+				domain: stage('zemn.me'),
+			},
+			{ parent: this }
+		);
 
-        const shadwellImZone = new StagableZone(`${name}_shadwell.im_zone`, {
-            name: 'shadwell.im',
-            staging: args.staging
-        }, { parent: this });
+		this.shadwellIm = new ShadwellIm.Component(
+			`${name}_shadwell.im`,
+			{
+				zoneId: Pulumi.output(zone.im.shadwell.then(z => z.id)),
+				domain: stage('shadwell.im'),
+			},
+			{ parent: this }
+		);
 
-        this.shadwellIm = new ShadwellIm.Component(`${name}_shadwell.im`, {
-            zone: shadwellImZone.zone,
-        }, { parent: this });
-
-        super.registerOutputs({
-            pleaseIntroduceMeToYourDog: this.pleaseIntroduceMeToYourDog,
-        });
-    }
-
+		super.registerOutputs({
+			pleaseIntroduceMeToYourDog: this.pleaseIntroduceMeToYourDog,
+		});
+	}
 }
-
