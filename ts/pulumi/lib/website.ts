f212b4bfad07b4821ed73923e5fd2e312e45ae6c
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-03 14:26:06 -0700

    Roll forward "Implement noIndex for staging builds."
    Testing weirdness means neutral tests get merged. I am unsure why.
    
    Original commit changeset: e9426a254f68

--- ts/pulumi/lib/website.ts
+++ ts/pulumi/lib/website.ts
@@ -70,6 +70,11 @@ export interface Args {
 	 * The 404 document to serve.
 	 */
 	notFound?: string;
+
+	/**
+	 * Prevent search engines from indexing.
+	 */
+	noIndex: boolean;
 }
 
 /**
@@ -215,6 +220,27 @@ export class Website extends pulumi.ComponentResource {
 			{ parent: this }
 		);
 
+		// response headers policy (http headers)
+
+		const responseHeadersPolicy = new aws.cloudfront.ResponseHeadersPolicy(
+			`${name}_response_headers`.replaceAll(".", "-"),
+			{
+				customHeadersConfig: {
+					items: [
+						...(args.noIndex
+							? [
+									{
+										header: 'x-robots-tag',
+										value: 'noindex',
+										override: false,
+									},
+							  ]
+							: []),
+					],
+				},
+			}
+		);
+
 		// create the cloudfront
 
 		const distribution = new aws.cloudfront.Distribution(
@@ -251,6 +277,7 @@ export class Website extends pulumi.ComponentResource {
 					  }
 					: {}),
 				defaultCacheBehavior: {
+					responseHeadersPolicyId: responseHeadersPolicy.id,
 					// i dont think we use most of these but it's probably not
 					// important
 					allowedMethods: [
