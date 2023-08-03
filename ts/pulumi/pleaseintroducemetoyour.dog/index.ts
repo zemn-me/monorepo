e9426a254f68807a58ab7f1c2f2e3e078d84a4d7
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-03 13:12:06 -0700

    Back out "Implement noIndex for staging builds."
    For reasons currently unknown -- if a test returns a neutral status then automerge still merges!
    
    As a result, the pulumi code here fails and yet the merge still happened.
    
    Original commit changeset: c37300567dc9

--- ts/pulumi/pleaseintroducemetoyour.dog/index.ts
+++ ts/pulumi/pleaseintroducemetoyour.dog/index.ts
@@ -11,11 +11,6 @@ export interface Args {
 	 * The domain to deploy to.
 	 */
 	domain: string;
-
-	/**
-	 * Prevent indexing the content.
-	 */
-	noIndex: boolean;
 }
 
 /**
@@ -36,7 +31,6 @@ export class Component extends Pulumi.ComponentResource {
 				directory: 'ts/pulumi/pleaseintroducemetoyour.dog/out',
 				zoneId: args.zoneId,
 				domain: args.domain,
-				noIndex: args.noIndex,
 			},
 			{ parent: this }
 		);
