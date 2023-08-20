1649b8c09d56dc87c3997f42f5d7e5929d1d3c16
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-19 17:23:11 -0700

    https://github.com/pnpm/pnpm/issues/6962

--- ci/presubmit.ts
+++ ci/presubmit.ts
@@ -34,26 +34,17 @@ const cmd = new Command('presubmit')
 		// version of inkscape, and I'm deferring solving that to some later day.
 		if (!o.skipBazelTests) {
 			const cwd = process.env['BUILD_WORKING_DIRECTORY'] ?? process.cwd();
-			// perform all the normal tests
-			const p = child_process.spawn('bazel', ['test', '//...'], {
-				cwd,
-				stdio: 'inherit',
-			});
-
-			await new Promise<void>((ok, error) =>
-				p.on('close', code => {
-					if (code != 0) return error(new Error(`Exit code ${code}`));
-					return ok();
-				})
-			);
 
 			// validate the pnpm lockfile.
 			await new Promise<void>((ok, err) =>
 				child_process
 					.spawn(
-						'npx',
+						'npm',
 						[
-							'pnpm',
+							'exec',
+							'--yes',
+							// https://github.com/pnpm/pnpm/issues/6962
+							'pnpm@8.6.12',
 							'i',
 							'--frozen-lockfile',
 							'--lockfile-only',
@@ -73,6 +64,20 @@ const cmd = new Command('presubmit')
 						code != 0 ? err(`exit ${code}`) : ok()
 					)
 			);
+
+			// perform all the normal tests
+			const p = child_process.spawn('bazel', ['test', '//...'], {
+				cwd,
+				stdio: 'inherit',
+			});
+
+			await new Promise<void>((ok, error) =>
+				p.on('close', code => {
+					if (code != 0) return error(new Error(`Exit code ${code}`));
+					return ok();
+				})
+			);
+
 		}
 
 		// attempt a deploy of pulumi to staging, and tear it down.
