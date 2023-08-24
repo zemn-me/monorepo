600c23c1709ee67cb57ea8f7612b1bc7113ce4f9
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-23 17:29:12 -0700

    Introduce ts/pulumi/lib/cloudfront.ts, which makes a CloudFront instance with the certs all set up. Abstracted out from ts/pulumi/lib/website.ts.

---
+++ ts/pulumi/aliases/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview This file defines functionality for the purpose
+ * of making Pulumi aware of aliases (resource identifiers that
+ * must be maintained through a refactoring)
+ */
+
+export * from 'ts/pulumi/aliases/aliases';
