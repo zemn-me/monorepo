8cbbeec4fe3aa493e2dbbe86248d1bf23d8a249d
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-26 17:41:18 -0700

    Progress on zemn.me.
    1. Fix the width of the 2col layout
    2. Move the logo and name into a "letterhead" thing which I've placed on the LHS.

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -6,6 +6,30 @@ import Timeline from 'project/zemn.me/next/components/timeline';
 import style from 'project/zemn.me/next/pages/index.module.css';
 import * as lang from 'ts/react/lang';
 
+interface ProseProps {
+	readonly children?: React.ReactElement[];
+}
+
+/**
+ * Sets up appropriate padding for showing a bunch of paragraphs.
+ */
+function Prose(props: ProseProps) {
+	return <div className={style.prose}>{props.children}</div>;
+}
+
+/**
+ * LetterHead is the inner part of the heading with the name and logo.
+ */
+function LetterHead() {
+	return (
+		<div className={style.letterHead}>
+			<div className={style.handle}>{lang.text(bio.Bio.who.handle)}</div>
+			<TimeEye className={style.logo} />
+			<div className={style.fullName}>Thomas NJ Shadwell</div>
+		</div>
+	);
+}
+
 export default function Main() {
 	return (
 		<main className={style.main}>
@@ -30,44 +54,47 @@ export default function Main() {
 				<kenwood.VideoSources />
 			</video>
 			<header className={style.banner}>
-				<h1 lang={lang.get(bio.Bio.who.handle)}>
-					{lang.text(bio.Bio.who.handle)}
-				</h1>
+				<LetterHead />
 			</header>
 			<section className={style.content}>
 				<header>
-					<h2 lang={lang.get(bio.Bio.who.fullName)}>
-						{lang.text(bio.Bio.who.fullName)}
-					</h2>
-					<TimeEye />
-					<p>
-						I am an internationally recognised international expert
-						on computer security, with specialisms in web security,
-						security program construction, and automated security
-						analysis.
-					</p>
-					<p>
-						I am interested in consulting on legal cases. For
-						business, email me at{' '}
-						<a href="mailto:thomas@shadwell.im">
-							thomas@shadwell.im
-						</a>
-						.
-					</p>
-				</header>
-				{bio.Bio.links !== undefined ? (
-					<nav>
-						{bio.Bio.links.map(([text, url]) => (
-							<a
-								href={url.toString()}
-								key={url.toString()}
-								lang={lang.get(text)}
-							>
-								{lang.text(text)}
+					<Prose>
+						<p>
+							I am an internationally recognised international
+							expert on computer security, with specialisms in web
+							security, security program construction, and
+							automated security analysis.
+						</p>
+						<p>
+							I am interested in consulting on legal cases. For
+							business, email me at{' '}
+							<a href="mailto:thomas@shadwell.im">
+								thomas@shadwell.im
 							</a>
-						))}
-					</nav>
-				) : null}
+							.
+						</p>
+						<p>
+							A selection of my work over the years can be found
+							below.
+						</p>
+
+						<>
+							{bio.Bio.links !== undefined ? (
+								<nav>
+									{bio.Bio.links.map(([text, url]) => (
+										<a
+											href={url.toString()}
+											key={url.toString()}
+											lang={lang.get(text)}
+										>
+											{lang.text(text)}
+										</a>
+									))}
+								</nav>
+							) : null}
+						</>
+					</Prose>
+				</header>
 				<section>
 					<Timeline />
 				</section>
