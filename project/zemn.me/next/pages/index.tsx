68dbf56cd54b82536896c8fbdccfcc9308c3be9f
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-08-30 00:02:58 -0700

    zemn.me: put some information at the bottom of the page.

--- project/zemn.me/next/pages/index.tsx
+++ project/zemn.me/next/pages/index.tsx
@@ -7,15 +7,28 @@ import Timeline from 'project/zemn.me/next/components/timeline';
 import style from 'project/zemn.me/next/pages/index.module.css';
 import * as lang from 'ts/react/lang';
 
-interface ProseProps {
+function ZemnmezLogoInline() {
+	return <ZemnmezLogo className={style.logoInline} />;
+}
+
+function TimeEyeInline() {
+	return <TimeEye className={style.logoInline} />;
+}
+
+type DivAttributes = JSX.IntrinsicElements['div'];
+interface ProseProps extends DivAttributes {
 	readonly children?: React.ReactElement[];
 }
 
 /**
  * Sets up appropriate padding for showing a bunch of paragraphs.
  */
-function Prose(props: ProseProps) {
-	return <div className={style.prose}>{props.children}</div>;
+function Prose({ children, ...props }: ProseProps) {
+	return (
+		<div {...props} className={style.prose}>
+			{children}
+		</div>
+	);
 }
 
 /**
@@ -98,6 +111,128 @@ export default function Main() {
 				</header>
 				<section>
 					<Timeline />
+				</section>
+				<section>
+					<Prose lang="en-GB">
+						<h3 id="website_design">The design of this website.</h3>
+						<p>
+							This website is a direct descendent of one I made in
+							2019. The core ideas come from very early on when I
+							was using the internet, and I didn't want to tell
+							people with my chosen username what kind of person I
+							was. I picked the username ‘zemnmez’ to be something
+							meaningless that people could fill with their own
+							ideas of who I was.
+						</p>
+						<p>
+							When I made the website, I didn't want to tell
+							people directly about myself, so instead I made this
+							timeline to keep track of what I had done every
+							year. The number in roman numerals is my age that
+							year. It fulfilled two roles as I was collecting my
+							work to apply for my US O1 visa, which requires
+							proving that you've done a lot of interesting
+							things!
+						</p>
+						<p>
+							The background video (“hero video”) is of a hidden
+							area in the gardens of{' '}
+							<a href="https://en.wikipedia.org/wiki/Kenwood_House">
+								Kenwood House
+							</a>
+							, a beautiful stately home sandwiched between
+							Highgate and Hampstead in London where I grew up.
+							It's located at about{' '}
+							<a
+								href="https://goo.gl/maps/JEAzn2kZgu6pyaNA6"
+								rel="nofollow"
+								title="Location of the Kenwood video"
+							>
+								51.57139601074658°N, -0.16924392259112794°E
+							</a>
+							.
+						</p>
+						<p>
+							It used to be that there was a bench hidden under
+							overgrown bushes and a tree near the hydrangeas past
+							the orangery. I took a video from there one summer –
+							I was collecting photos and videos to remind me of
+							home because I knew I'd leave it behind someday to
+							move to the US.
+						</p>
+						<p>
+							The type and style itself was inspired by older,
+							pre-computer era typsetting such as the{' '}
+							<a href="https://assets.lloyds.com/assets/pdf-lloyds-acts-mar07lloydsact1871/1/pdf-lloyds-acts-Mar07LloydsAct1871.pdf">
+								Lloyd's Act 1871
+							</a>
+							. Particular effort was put into trying to have
+							content fill horizontal space automatically, as is
+							seen in older documents that try to make the most of
+							the paper they're printed on.
+						</p>
+						<h3 id="logo_disambiguation">
+							What's the difference between <ZemnmezLogoInline />{' '}
+							and <TimeEyeInline />?
+						</h3>
+						<p>
+							When I was first starting to use the internet,
+							people were told very sternly to maintain different
+							identities so that someone online wouldn't find you
+							in real life.
+						</p>
+						<p>
+							The diamond logo (<ZemnmezLogoInline />) came out of
+							several years of wanting a way to express myself in
+							art. For a few years following, I changed logo every
+							year based on feelings I'd felt the year prior,
+							making logos with geometry and construction lines.
+						</p>
+						<p>
+							When I eventually made the diamond logo, it ended up
+							looking a like an eye logo I'd made very early on in
+							2012. I liked it so much, it stuck, and came to
+							represent ‘zemnmez’, the online persona I had since
+							2009. The logo itself is from much later, probably
+							around 2015.
+						</p>
+						<p>
+							The time eye logo (<TimeEyeInline />) was the later
+							(2019) creation, coming out of a specific need to
+							disambiguate between the published work I had as
+							‘Thomas Shadwell’, my real name, versus ‘zemnmez’,
+							the persona / name I had used since 2009.
+						</p>
+						<p>
+							The time eye logo became necessary after I made the
+							Forbes Under 30 list for my tax system hack in 2018.
+							Before this point I'd worked hard to try to keep the
+							two identities separate, but Forbes lists aren't
+							really for online personas.
+						</p>
+						<p>
+							The eye logo was specifically made as a reflection
+							on the well-known{' '}
+							<a href="https://en.wikipedia.org/wiki/Eye_of_Providence">
+								‘eye of providence’
+							</a>
+							, a symbol that represents human achievement as
+							being incomplete without God. I wanted it to reflect
+							the idea that, in a universe that might not have a
+							God, we as people had a responsibility to care for
+							each other.
+						</p>
+						<p>
+							In having to make this distinction, for a short time
+							the work published as ‘zemnmez’ continued to
+							represent the things I was most proud of – an
+							idealised kind of self. But at Google, I started to
+							publish security research I was really proud of as
+							both ‘zemnmez’ and ‘Thomas Shadwell’. The abstract
+							ideas are still there, but now I'm more ‘Thomas’
+							than I ever was. ☺
+						</p>
+					</Prose>
 				</section>
 				<section className={style.footer}>
 					<ZemnmezLogo className={style.future} />
