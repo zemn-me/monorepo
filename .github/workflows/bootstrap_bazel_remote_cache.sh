8f7d74f397a766369530f02ef6b8354b83ec7a2f
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-09-18 02:03:40 +0000

    Bazel remote caching

---
+++ .github/workflows/bootstrap_bazel_remote_cache.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+if [[ -z "${BAZEL_REMOTE_CACHE_URL}" ]]; then
+	echo "::warning file=.github/workflows/bootstrap_bazel_remote_cache.sh,line=3,endLine=3,title=Running without bazel cache!:: BAZEL_REMOTE_CACHE_URL was not specified, so tests will be doing all the work from scratch.
+	exit 0
+else
+	echo "Using bazel remote cache."
+	echo "build --remote_cache=${BAZEL_REMOTE_CACHE_URL}" > .bazelrc
+	echo "test --remote_cache=${BAZEL_REMOTE_CACHE_URL}" > .bazelrc
+fi
+
+exit 0
