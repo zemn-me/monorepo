fdf07f1aee2a92daf1606b82f727da6ff590e394
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-11-04 21:07:17 +0000

    Sketch of adding menu

--- project/zemn.me/next/pages/_app.tsx
+++ project/zemn.me/next/pages/_app.tsx
@@ -2,12 +2,36 @@ import 'project/zemn.me/next/pages/base.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Link from 'next/link';
+import style from 'project/zemn.me/next/pages/index.module.css';
+import React from 'react';
 import { HeaderTags } from 'ts/next.js';
+
+interface NavAccordionProps {
+	readonly title?: React.ReactNode;
+	readonly children?: React.ReactNode;
+}
+
+function NavAccordion(props: NavAccordionProps) {
+	return (
+		<details className={style.navDetails}>
+			{props.title ? <summary>{props.title}</summary> : null}
+			<div className={style.navAccordionBody}>{props.children}</div>
+		</details>
+	);
+}
 
 export function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<HeaderTags />
+			<nav className={style.nav}>
+				<Link href="/">Home (replace with logo)</Link>
+				<Link href="/about">About</Link>
+				<NavAccordion title="Experiments">
+					<Link href="/experiments/rays">rays</Link>
+				</NavAccordion>
+			</nav>
 			<Component {...pageProps} />
 			<Head>
 				<link href="icon.svg" rel="icon" type="image/svg+xml" />
