598ce632ae9d0b34e7506698d3a1709c63f9e08e
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-11-27 22:16:51 -0800

    Modify run_affected_tests to only run tests that are affected by the change in files.
    Also rename the script, the name was stupid.

--- bzl/run_affected_tests.sh
+++ bzl/run_affected_tests.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-#
-# Copyright 2015 The Bazel Authors. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 # This script looks at the files changed in git against origin/master
 # (actually a common ancestor of origin/master and the current commit) and
@@ -30,55 +15,17 @@
 # "Every single change submitted to Google's Perforce depot is built and
 # tested, and only those targets affected by a particular change are
 # built and tested"
-#
-# When this script is triggered by Gerrit's patchset-updated hook (for example)
-# you can replace origin/master in the COMMIT_RANGE variable initialization
-# with the branch passed as argument to the hook. When using Jenkins with the
-# Gerrit Trigger Plugin, use $GERRIT_BRANCH instead. This would make it
-# possible to have the Verified label on Gerrit patchsets populated as fast
-# as possible.
-# For a ref-updated event, use "${GERRIT_OLDREV}..${GERRIT_NEWREV}" as the
-# value for COMMIT_RANGE.
-# When running in Travis-CI, you can directly use the $TRAVIS_COMMIT_RANGE
-# environment variable.
 
-alias bazel=yarn bazel
-
-echo $INIT_CWD
-cd $INIT_CWD
-
-COMMIT_RANGE=${COMMIT_RANGE:-$(git merge-base origin/main HEAD)".."}
-
-# Go to the root of the repo
-cd "$(git rev-parse --show-toplevel)"
-
-# Get a list of the current files in package form by querying Bazel.
-files=()
-for file in $(git diff --name-only ${COMMIT_RANGE} ); do
-  files+=($(bazel query $file))
-  echo $(bazel query $file)
-done
-
-# Query for the associated buildables
-buildables=$(bazel query \
-    --keep_going \
-    --noshow_progress \
-    "kind(.*_binary, rdeps(//..., set(${files[*]})))")
-# Run the tests if there were results
-if [[ ! -z $buildables ]]; then
-  echo "Building binaries"
-  bazel build $buildables
+if test "$BUILD_WORKSPACE_DIRECTORY" = ""; then
+	echo "Please run from bazel."
+	exit 1
 fi
 
-tests=$(bazel query \
-    --keep_going \
-    --noshow_progress \
-    "kind(test, rdeps(//..., set(${files[*]}))) except attr('tags', 'manual', //...)")
-# Run the tests if there were results
-if [[ ! -z $tests ]]; then
-  echo "Running tests"
-  bazel test $tests
-fi
+cd $BUILD_WORKSPACE_DIRECTORY
 
-echo "Ensuring all BUILD files are valid..."
-bazel query //...
+files="$(sl status --change . --no-status)"
+
+bazel query \
+	--keep_going \
+	"rdeps(//..., set($files))" | xargs -t bazel test
+
