5667bf696a1163720eecadb6676a9a8599e879c6
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-02 23:59:08 +0000

    Back out "Back out "See if I can make google analytics work (due to the nature of modifying csp, I'm testing this live).""
    Original commit changeset: bd141194878692b5d6b3d3617ca2e8c72140e120

--- ts/next.js/index.tsx
+++ ts/next.js/index.tsx
@@ -1,39 +1,114 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 export * as config from 'ts/next.js/next.config';
 
-const BASE_CSP_RULES = [
-	"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-	"img-src 'self' data:",
-	"font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com",
+const isDevMode = process?.env?.NODE_ENV === 'development';
+
+class CSPDirectiveList extends Set {
+	toString(): string {
+		return [...super.values()].join(' ');
+	}
+}
+
+class CSPDirective {
+	constructor(
+		private readonly directive: string,
+		private readonly directiveList: CSPDirectiveList
+	) {}
+	toString(): string {
+		return [this.directive, this.directiveList].join(' ');
+	}
+}
+
+const script_src_directives = new CSPDirectiveList([
+	"'self'",
+	'https://*.google-analytics.com',
+]);
+
+const csp_rules: CSPDirective[] = [
+	new CSPDirective(
+		'style-src',
+		new CSPDirectiveList([
+			"'self'",
+			"'unsafe-inline'",
+			'https://fonts.googleapis.com',
+		])
+	),
+	new CSPDirective(
+		'img-src',
+		new CSPDirectiveList([
+			"'self'",
+			"'unsafe-inline'",
+			'data:',
+			'https://*.google-analytics.com',
+			'https://*.g.doubleclick.net',
+		])
+	),
+	new CSPDirective(
+		'font-src',
+		new CSPDirectiveList([
+			"'self'",
+			'https://fonts.gstatic.com',
+			'https://fonts.googleapis.com',
+		])
+	),
+	new CSPDirective(
+		'connect-src',
+		new CSPDirectiveList(["'self'", 'https://*.google-analytics.com'])
+	),
+	new CSPDirective('script-src', script_src_directives),
 ];
 
+if (isDevMode) {
+	["'unsafe-inline'", "'unsafe-eval'"].forEach(v =>
+		script_src_directives.add(v)
+	);
+
+	csp_rules.push(
+		new CSPDirective(
+			'default-src',
+			new CSPDirectiveList(["'self'", "'unsafe-eval'"])
+		)
+	);
+}
+
+declare const ga: (...params: unknown[]) => void;
 export function HeaderTags() {
-	const csp_rules = [
-		...BASE_CSP_RULES,
-		process?.env?.NODE_ENV === 'development'
-			? "default-src 'self' 'unsafe-inline' 'unsafe-eval'"
-			: "default-src 'self'",
-	];
 	return (
-		<Head>
-			<meta
-				content={csp_rules.join('; ')}
-				httpEquiv="Content-Security-Policy"
+		<>
+			<Script
+				async
+				id="ga"
+				onError={() =>
+					console.error('unable to load google analytics :(')
+				}
+				onLoad={() => {
+					ga('create', 'UA-134479219-1', 'auto');
+					ga('send', 'pageview');
+				}}
+				src="https://ssl.google-analytics.com/analytics.js"
+				strategy="lazyOnload"
 			/>
+			<Head>
+				<meta
+					content={csp_rules.join('; ')}
+					httpEquiv="Content-Security-Policy"
+				/>
 
-			<meta
-				content="same-origin"
-				httpEquiv="Cross-Origin-Resource-Policy"
-			/>
+				<meta
+					content="same-origin"
+					httpEquiv="Cross-Origin-Resource-Policy"
+				/>
 
-			<meta
-				content="same-origin"
-				httpEquiv="Cross-Origin-Opener-Policy"
-			/>
-			<meta content="nosniff" httpEquiv="X-Content-Type-Options" />
+				<meta
+					content="same-origin"
+					httpEquiv="Cross-Origin-Opener-Policy"
+				/>
+				<meta content="nosniff" httpEquiv="X-Content-Type-Options" />
 
-			<meta content="no-referrer" name="referrer" />
-		</Head>
+				<meta content="no-referrer" name="referrer" />
+			</Head>
+		</>
 	);
 }
