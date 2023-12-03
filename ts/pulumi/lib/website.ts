2c18c21f9ba6a4d8b9874f66dbbee4440e03c120
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-03 12:49:03 -0800

    Pulumi: control TXT records via Website component, and set up SPF rules for all websites. Fixes #3964

--- ts/pulumi/lib/website.ts
+++ ts/pulumi/lib/website.ts
@@ -387,10 +387,25 @@ export class Website extends pulumi.ComponentResource {
 			{ parent: this, deleteBeforeReplace: true }
 		);
 
+		const txt_record = new aws.route53.Record(
+			`${name}_txt_record`,
+			{
+				zoneId: args.zoneId,
+				name: args.domain,
+				type: 'TXT',
+				records: [
+					'google-site-verification=plPeQFN6n0_8HZ8hr3HMXbYHrU_Yh5wPP9OUwH0ErGY',
+					'v=spf1 include:_spf.google.com ~all',
+				],
+			},
+			{ protect: true }
+		);
+
 		this.registerOutputs({
 			distribution,
 			record,
 			bucketPolicy,
+			txt_record,
 		});
 	}
 }
