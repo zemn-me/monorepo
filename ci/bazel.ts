998cff497071f28e56e36b8e129055c0956adc04
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-09 12:55:28 -0800

    Cute little emoji thing

--- ci/bazel.ts
+++ ci/bazel.ts
@@ -136,14 +136,14 @@ async function* AnnotateBuildCompletion(lines: AsyncGenerator<string>) {
 				break;
 			case 'FAILED': {
 				failures.push(tag);
-				const nextLine = await take();
+				const nextLine = (await take())?.trim();
 				yield Command('error')({
 					title: `${tag} failed in ${time}`,
 					file: buildFile,
 				})(
 					line +
-						(nextLine !== undefined
-							? `\n${await fs.readFile(nextLine.trim())}`
+						(nextLine !== undefined && nextLine
+							? `\n${await fs.readFile(nextLine)}`
 							: '')
 				);
 				break;
@@ -224,6 +224,7 @@ export async function Bazel(cwd: string, ...args: string[]) {
 
 	const finish = new Promise<void>(ok => process.on('close', () => ok()));
 
+	console.log('Beginning tests.');
 	for await (const line of interleave(
 		AnnotateBazelLines(byLine(process.stdout)),
 		AnnotateBazelLines(byLine(process.stderr))
@@ -231,10 +232,22 @@ export async function Bazel(cwd: string, ...args: string[]) {
 		console.log(line);
 	}
 
-	await new Promise(ok => process.addListener('exit', ok));
+	console.log('tests likely concluded. awaiting process exit');
 
-	if (process.exitCode !== 0)
+	try {
+		await Promise.all([
+			finish,
+			new Promise(ok => process.addListener('exit', ok)),
+		]);
+	} catch (e) {
+		console.error('Mysterious error', e);
+	}
+
+	console.log('subprocess exited');
+
+	if (process.exitCode !== 0) {
+		console.info('Failure.');
 		throw new Error(`Bazel failed with exit code: ${process.exitCode}`);
-
-	await finish;
+	}
+	console.info('Success.');
 }
---
+++ project/zemn.me/next/pages/experiments/emoji/flag.tsx
@@ -0,0 +1,205 @@
+import Link from 'project/zemn.me/next/components/Link';
+import { Prose } from 'project/zemn.me/next/components/Prose/prose';
+import { Q } from 'project/zemn.me/next/components/Q';
+import { Title } from 'project/zemn.me/next/components/Title/title';
+import style from 'project/zemn.me/next/pages/experiments/emoji/flag.module.css';
+import { Fragment, useState } from 'react';
+
+interface CodePointReferenceProps {
+	readonly children: string;
+}
+
+function CodePointReference({ children: codepoint }: CodePointReferenceProps) {
+	return (
+		<Link href={`https://www.compart.com/en/unicode/${codepoint}`}>
+			{codepoint}
+		</Link>
+	);
+}
+
+interface CodepointNameProps {
+	readonly children: string;
+}
+
+/**
+ * @see https://en.wikipedia.org/wiki/Small_caps#Use_in_Unicode_standards
+ */
+function CodepointName({ children: name }: CodepointNameProps) {
+	return <span className={style.codepointName}>{name}</span>;
+}
+
+const REGIONAL_INDICATOR_SYMBOL_LETTER_A = '🇦';
+const REGIONAL_INDICATOR_SYMBOL_LETTER_A_CODEPOINT =
+	REGIONAL_INDICATOR_SYMBOL_LETTER_A.codePointAt(0)!;
+const LATIN_LETTER_A = 'a';
+const LATIN_LETTER_A_CODEPOINT = LATIN_LETTER_A.codePointAt(0)!;
+const LATIN_LETTER_Z = 'z';
+const LATIN_LETTER_Z_CODEPOINT = LATIN_LETTER_Z.codePointAt(0)!;
+
+function codepointDisplayForm(codepoint: number) {
+	return `U+${codepoint.toString(16).padStart(4, '0')}`;
+}
+
+class ErrNonLatin extends Error {
+	get letter() {
+		return String.fromCodePoint(this.codepoint);
+	}
+	render() {
+		return (
+			<>
+				<CodePointReference>
+					{codepointDisplayForm(this.codepoint)}
+				</CodePointReference>{' '}
+				(<Q single>{this.letter}</Q>) is not a latin character.
+			</>
+		);
+	}
+	constructor(public readonly codepoint: number) {
+		super(`${codepointDisplayForm(codepoint)} is not a latin character`);
+	}
+}
+
+class ErrNoCodePoint extends Error {
+	render() {
+		return (
+			<>
+				<Q single>{this.letter}</Q> does not contain a codepoint.
+			</>
+		);
+	}
+	constructor(public readonly letter: string) {
+		super(`Can't get a unicode codepoint from ${letter}`);
+	}
+}
+
+function latinLetterToRegionalIndicatorSymbol(latinLetter: string) {
+	const codePoint = latinLetter.codePointAt(0);
+	if (codePoint === undefined) return new ErrNoCodePoint(latinLetter);
+	console.log(codePoint, LATIN_LETTER_A_CODEPOINT, LATIN_LETTER_Z_CODEPOINT);
+	if (
+		codePoint < LATIN_LETTER_A_CODEPOINT ||
+		codePoint > LATIN_LETTER_Z_CODEPOINT
+	)
+		return new ErrNonLatin(codePoint);
+
+	return String.fromCodePoint(
+		codePoint -
+			LATIN_LETTER_A_CODEPOINT +
+			REGIONAL_INDICATOR_SYMBOL_LETTER_A_CODEPOINT
+	);
+}
+
+function latinTextToRegionalIndicators(text: string) {
+	return [...text].map(v => latinLetterToRegionalIndicatorSymbol(v));
+}
+
+function FlagGenerator() {
+	const [input, setInput] = useState<string>();
+
+	const regionalIndicators = latinTextToRegionalIndicators(input ?? '');
+	const errors = regionalIndicators.filter(
+		<E extends Error>(v: string | E): v is E => v instanceof Error
+	);
+
+	return (
+		<>
+			<form>
+				<label htmlFor="input">
+					Country code (latin letters only):{' '}
+					<input
+						id="input"
+						onChange={e => setInput(e.target.value)}
+						pattern="[a-z]*"
+						type="text"
+					/>
+				</label>
+			</form>
+
+			<figure>
+				<figcaption>Regional indicators:</figcaption>
+				{regionalIndicators.map((v, i) => {
+					if (typeof v === 'string')
+						return (
+							<Fragment key={i}>
+								{v}
+								<wbr />
+							</Fragment>
+						);
+
+					return <s key={i}>{v.letter}</s>;
+				})}
+			</figure>
+			{errors.length == 0 ? (
+				<figure>
+					<figcaption>Your country flag!</figcaption>
+					{(regionalIndicators as string[]).join('')}
+				</figure>
+			) : null}
+			{errors.length ? (
+				<figure>
+					<figcaption>Errors occurred:</figcaption>
+					<ol>
+						{errors.map((e, i) => (
+							<li key={i}>{e.render()}</li>
+						))}
+					</ol>
+				</figure>
+			) : null}
+		</>
+	);
+}
+
+export default function Main() {
+	return (
+		<>
+			<Prose>
+				<Title>Custom Country flag emoji generator!</Title>
+				<p>
+					A cute fact is that the flag emoji (<Q single>🇬🇧</Q>) are
+					actually, for international diplomatic reasons, made of
+					their own special alphabet.
+				</p>
+				<p>
+					The UK flag for example, <Q single>🇬🇧</Q> is composed of{' '}
+					<Q single>🇬</Q> (
+					<Q single>
+						<CodePointReference>U+1F1EC</CodePointReference>{' '}
+						<CodepointName>
+							regional indicator symbol letter g
+						</CodepointName>
+					</Q>
+					) and <Q single>🇧</Q> (
+					<Q single>
+						<CodePointReference>U+1F1E7</CodePointReference>{' '}
+						<CodepointName>
+							regional indicator symbol letter b
+						</CodepointName>
+					</Q>
+					).
+				</p>
+				<p>
+					These <Q single>regional indicator symbols</Q> are made of
+					their own version of the latin alphabet and form country
+					codes that on some devices turn into flags.
+				</p>
+				<p>
+					A big part of this is that not all countries recognise the
+					soverignty or existence of all other countries. If Unicode
+					had to assign codepoints to every country in the world there
+					would be a lot of arguing, and Unicode would need to be put
+					in charge of who gets to be a country.
+				</p>
+				<p>
+					With this tool, you can generate flags for both real and
+					theoretical countries. Have fun!
+
+
+
+
+
+				</p>
+			</Prose>
+			<FlagGenerator />
+		</>
+	);
+}
