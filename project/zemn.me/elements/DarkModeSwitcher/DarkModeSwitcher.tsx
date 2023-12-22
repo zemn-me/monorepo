39e0433428174549b9d1d9a1fce2a1dbb7d546ff
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-22 07:40:15 +0000

    Move //project/zemn.me/next to //project/zemn.me

--- project/zemn.me/elements/DarkModeSwitcher/DarkModeSwitcher.tsx
+++
@@ -1,6 +0,0 @@
-import SliderCheckbox from 'project/zemn.me/elements/SliderCheckbox';
-import React from 'react';
-
-export const DarkModeSwitcher: React.FC = () => <SliderCheckbox />;
-
-export default DarkModeSwitcher;
