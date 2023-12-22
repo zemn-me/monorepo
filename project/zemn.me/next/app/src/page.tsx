287fb79e73a1dc7bf218916fd6e5ffe22f44ff8f
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-22 06:30:40 +0000

    project/zemn.me/next: migrate to app router: migrate /src

--- project/zemn.me/next/app/src/page.tsx
+++ project/zemn.me/next/app/src/page.tsx
@@ -1,4 +1,4 @@
-import Redirect from 'ts/next.js/component/Redirect';
+import Redirect from 'ts/next.js/component/Redirect/app';
 
 export default function Page() {
 	return <Redirect to="https://github.com/zemn-me/monorepo" />;
