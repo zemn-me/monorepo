44fa8798b59a46de95cd95049ec561fa1691f856
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-22 04:46:04 +0000

    project/zemn.me/next: migrate to app router (2) _app.tsx

---
+++ project/zemn.me/next/app/ClientLayout.tsx
@@ -0,0 +1,36 @@
+'use client';
+import 'project/zemn.me/next/app/base.css';
+
+import Head from 'next/head';
+import { ReactNode } from 'react';
+import { HeaderTags } from 'ts/next.js';
+
+export interface Props {
+	readonly children?: ReactNode;
+}
+
+export function ClientLayout({ children }: Props) {
+	return (
+		<>
+			<HeaderTags />
+			{children}
+			<Head>
+				<link href="/icon.svg" rel="icon" type="image/svg+xml" />
+				<link
+					href="/icon.svg"
+					rel="apple-touch-icon"
+					type="image/svg+xml"
+				/>
+				<meta content="@zemnmez" name="twitter:site" />
+				<meta content="@zemnnmez" name="twitter:creator" />
+				<meta content="zemnmez" name="author" />
+				<meta
+					content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+					name="viewport"
+				/>
+			</Head>
+		</>
+	);
+}
+
+export default ClientLayout;
