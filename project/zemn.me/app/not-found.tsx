b591777cf98114865f54c5d2f8922e5b559cdb25
Thomas Neil James Shadwell <thomas@shadwell.im>
2023-12-22 08:10:41 +0000

    Move //project/zemn.me/next to //project/zemn.me

---
+++ project/zemn.me/app/not-found.tsx
@@ -0,0 +1,9 @@
+import style from 'project/zemn.me/app/not-found.module.css';
+
+export default function NotFound() {
+	return (
+		<main className={style.NotFound}>
+			<i lang="en-GB">The requested resource was not found.</i>
+		</main>
+	);
+}
--- project/zemn.me/next/components/timeline/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from 'project/zemn.me/next/components/timeline/timeline';
-export { default } from 'project/zemn.me/next/components/timeline/timeline';
