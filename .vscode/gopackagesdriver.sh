aa491bef1464ce7809ba2d6c7a54214e129dc41c
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-01-07 19:57:26 +0000

    Standardise calling bazel binaries from outside of bazel and rework .vscode integrations to use it.

--- .vscode/gopackagesdriver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# https://github.com/bazelbuild/rules_go/wiki/Editor-setup#2-launcher-script
-exec bazel run --tool_tag=gopackagesdriver -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
