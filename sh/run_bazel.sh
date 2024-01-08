454b5178d2be60f96f7d7f608b8a326aceba36ae
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-01-08 08:17:56 +0000

    Add bazel / bazelisk to sh/bin and use bazelisk for all bazel commands.

--- sh/run_bazel.sh
+++ sh/run_bazel.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # runs a binary built by bazel in this repo
+BAZEL="$(realpath $(dirname ${BASH_SOURCE[0]})/bin/bazel)"
 cd $(dirname ${BASH_SOURCE[0]})
 
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress -- "${@}"
+$BAZEL run --ui_event_filters=-info,-stdout,-stderr --noshow_progress -- "${@}"
 
