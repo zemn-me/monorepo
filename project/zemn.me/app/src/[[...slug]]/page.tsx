e6cc030ac05d33f02e6cad90e51ba75b51824076
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-01-16 01:36:53 +0000

    Parameterise zemn.me/src so that zemn.me/src/issues goes to the github issues page.
    Also adds ts/constants/constants.ts which has some basic constants about the repo itself.
    Fixes https://github.com/zemn-me/monorepo/issues/4443

---
+++ project/zemn.me/app/src/[[...slug]]/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next/types';
+import { githubRepoUrl } from 'ts/constants/constants';
+import Redirect from 'ts/next.js/component/Redirect/app';
+
+
+export default function Page({ params }: { params: {slug: string[]}}) {
+	const u = new URL(githubRepoUrl);
+	u.pathname += params.slug.join("/");
+	u.hash = document.location.hash;
+	return <Redirect to={u} />;
+}
+
+export const metadata: Metadata = {
+	description: 'Redirect to the source code.',
+};
+
