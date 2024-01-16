f11e8215824d9e9d13092c626ee1ab09f30d835d
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-01-16 06:01:24 +0000

    ci/presubmit: run all tests even if some fail.

--- ci/presubmit.ts
+++ ci/presubmit.ts
@@ -182,7 +182,7 @@ const cmd = new Command('presubmit')
 
 		if (!o.skipBazelTests) {
 			await Task('Run all bazel tests.')(
-				Bazel.Bazel(cwd, 'test', '//...')
+				Bazel.Bazel(cwd, 'test', '//...', '--keep_going')
 			);
 			// perform all the normal tests
 		}
