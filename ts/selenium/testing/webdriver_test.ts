e4287366196e50bd92b79dcd3789e98a1c0501c9
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-01-19 05:19:00 +0000

    Selenium

---
+++ ts/selenium/testing/webdriver_test.ts
@@ -0,0 +1,17 @@
+import { Browser, until } from 'selenium-webdriver';
+import { Driver } from 'ts/selenium/webdriver';
+
+describe('selenium webdriver', () => {
+	it('should load a data: uri', async () => {
+		expect.assertions(0);
+		const title = 'hello, world!';
+		const driver = Driver().forBrowser(Browser.CHROME).build();
+
+		try {
+			await driver.get(`data:text/html,<title>${title}</title>`);
+			await driver.wait(until.titleIs(title), 1000);
+		} finally {
+			await driver.quit();
+		}
+	});
+});
