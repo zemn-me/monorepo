0f9393482ec4a0e0a4629487b154da81ea033364
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-02-12 08:42:43 +0000

    Forbid <a> and <NextLink> tags that might result in JavaScript URI injection.

--- ts/react/element/link/link_test.tsx
+++ ts/react/element/link/link_test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/forbid-elements */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
