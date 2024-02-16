94d2539c7a58b404e0f78db32a028781a5aa30e3
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-02-16 06:44:43 +0000

    Abstract out pulumi's bucket name generator.

---
+++ ts/pulumi/lib/bucketName.ts
@@ -0,0 +1,9 @@
+const bucketSuffix = '-bucket';
+const pulumiRandomChars = 7;
+const bucketNameMaximumLength = 56 - pulumiRandomChars - bucketSuffix.length;
+
+// bucket has a maximum length of 63 (56 minus the 7 random chars that Pulumi adds)
+export const deriveBucketName = (base: string) =>
+	[...base.replace(/[^a-z0-9.]/g, '-')]
+		.slice(0, bucketNameMaximumLength - 1)
+		.join('') + bucketSuffix;
