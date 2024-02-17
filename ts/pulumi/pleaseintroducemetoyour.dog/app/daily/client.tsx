e0c0214b8b7141649c66c1755812f5e4af84e628
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-02-17 03:19:35 +0000

    blah

--- ts/pulumi/pleaseintroducemetoyour.dog/app/daily/client.tsx
+++ ts/pulumi/pleaseintroducemetoyour.dog/app/daily/client.tsx
@@ -5,6 +5,8 @@ import { useCallback, useRef } from 'react';
 import { z } from 'zod';
 
 import Link from '#root/project/zemn.me/components/Link/index.js';
+import { isDefined, isNotNull } from '#root/ts/guard.js';
+import { isString } from '#root/ts/guards.js';
 
 export const PostProps = z.object({
 	media: z
@@ -21,6 +23,25 @@ export const PostProps = z.object({
 		})
 		.optional()
 		.nullable(),
+	isGallery: z.boolean().optional().nullable(),
+	url: z.string().optional().nullable(),
+	permalink: z.string().optional().nullable(),
+	title: z.string(),
+	media_metadata: z
+		.record(
+			z.string(),
+			z.object({
+				e: z.literal('Image'),
+				s: z.object({
+					u: z.string().url(),
+					x: z.number(),
+					y: z.number(),
+				}),
+			})
+		)
+		.optional()
+		.nullable(),
+
 	preview: z
 		.object({
 			images: z
@@ -28,7 +49,9 @@ export const PostProps = z.object({
 					z.object({
 						source: z
 							.object({
-								width: z.number().optional().nullable(),
+								height: z.number(),
+								width: z.number(),
+								url: z.string().url(),
 							})
 							.optional()
 							.nullable(),
@@ -39,9 +62,6 @@ export const PostProps = z.object({
 		})
 		.optional()
 		.nullable(),
-	url: z.string().optional().nullable(),
-	permalink: z.string().optional().nullable(),
-	title: z.string(),
 });
 
 const SearchResponseProps = z.object({
@@ -64,26 +84,69 @@ function Post(post: z.TypeOf<typeof PostProps>) {
 	const videoRef = useRef<HTMLVideoElement>(null);
 	const onClick = useCallback(() => void videoRef.current?.play(), []);
 	const video = post.media?.reddit_video;
+	const videoUrls = [
+		video?.hls_url,
+		video?.dash_url,
+		video?.fallback_url,
+	].filter(isString);
+
+	const metadataMedia = [...Object.values(post.media_metadata ?? {})];
 	return (
 		<article
 			className={classNames('post', post.media ? 'media' : undefined)}
 			onClick={onClick}
 		>
-			{post.media ? (
-				<video autoPlay loop muted playsInline ref={videoRef}>
-					{[video?.hls_url, video?.dash_url, video?.fallback_url].map(
-						source =>
-							source ? <source key={source} src={source} /> : null
-					)}
-				</video>
-			) : (
-				<img src={post.url?.replace(/\.gifv$/, 'gif')} />
-			)}
 			{post.permalink ? (
 				<Link href={'https://reddit.com' + post.permalink}>
 					<header>{post.title}</header>
 				</Link>
 			) : null}
+			{metadataMedia.length !== 0 ? (
+				<>
+					{metadataMedia.map(v => (
+						<img
+							key={v.s.u}
+							src={v.s.u}
+							style={{
+								display: 'block',
+								maxWidth: v.s.x,
+								maxHeight: v.s.y,
+								width: '100%',
+								objectFit: 'cover',
+								height: 'auto',
+							}}
+						/>
+					))}
+				</>
+			) : null}
+			{videoUrls.length !== 0 ? (
+				<video autoPlay loop muted playsInline ref={videoRef}>
+					{videoUrls.map(source =>
+						source ? <source key={source} src={source} /> : null
+					)}
+				</video>
+			) : null}
+
+			{post.preview?.images
+				? post.preview.images
+						.map(v => v.source)
+						.filter(isDefined)
+						.filter(isNotNull)
+						.map(v => (
+							<img
+								key={v.url}
+								src={v.url}
+								style={{
+									display: 'block',
+									maxWidth: v.width,
+									maxHeight: v.height,
+									width: '100%',
+									objectFit: 'cover',
+									height: 'auto',
+								}}
+							/>
+						))
+				: null}
 		</article>
 	);
 }
@@ -130,9 +193,12 @@ export function DogsOfTheDay() {
 		<>
 			<h1>Top doggoes of the day!!</h1>
 			{doggs.isLoading ? `we are loading the dogs!! 🐕 🐶` : null}
-			{doggs.isError
-				? `we had an issue loading the doggs... 🐕😭 ... ${doggs.error} ...`
-				: null}
+			{doggs.isError ? (
+				<p>
+					we had an issue loading the doggs... 🐕😭 ...{' '}
+					<pre>{`${doggs.error}`}</pre> ...
+				</p>
+			) : null}
 			{doggs.data?.data?.children?.map((post, i) => (
 				<Post key={i} {...post.data} />
 			)) ?? null}
