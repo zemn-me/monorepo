0339214e5a692840f2deaec3d0d327c0fe7852a1
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-02-21 23:25:00 +0000

    entryway project

---
+++ project/entryway/api/router.ts
@@ -0,0 +1,26 @@
+import { initTRPC, TRPCError } from '@trpc/server';
+import superjson from 'superjson';
+
+import { Context } from '#root/project/entryway/api/authorization.js';
+import { db, UserInput } from '#root/project/entryway/api/model.js';
+
+const t = initTRPC.context<Context>().create({
+	transformer: superjson,
+});
+
+export const router = t.router({
+	upsertUser: t.procedure.input(UserInput).mutation(({ input, ctx }) => {
+		if (!ctx.user || ctx.user.role == 'ADMIN' || ctx.user.id === input.id)
+			throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+		const { id: uid, ...inputWithoutId } = input;
+
+		return db.user.upsert({
+			where: { id: uid },
+			update: inputWithoutId,
+			create: inputWithoutId,
+		});
+	}),
+});
+
+export type API = typeof router;
