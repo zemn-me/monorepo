8168eda6851264dc4e335a6073a56e143500e5da
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-03-02 19:55:29 +0000

    Convert some factorio types to zod.

--- ts/factorio/int.ts
+++ ts/factorio/int.ts
@@ -1,1 +1,4 @@
-export type Int = number;
+import { z } from 'zod';
+
+export const Int = z.number();
+export type Int = z.TypeOf<typeof Int>;
--- ts/factorio/uint.ts
+++ ts/factorio/uint.ts
@@ -1,1 +1,7 @@
-export type Uint = number;
+import { z } from 'zod';
+
+export const Uint = z
+	.number()
+	.refine(z => z > 0, { message: 'uint must be > 0' });
+
+export type Uint = z.TypeOf<typeof Uint>;
