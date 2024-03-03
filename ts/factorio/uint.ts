6bafe8ef19b034930569a549085e8285ac872d83
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-03-03 06:15:29 +0000

    Fix big mistake in making uints need to be > 0

--- ts/factorio/uint.ts
+++ ts/factorio/uint.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const Uint = z
 	.number()
-	.refine(z => z > 0, { message: 'uint must be > 0' });
+	.refine(z => z >= 0, { message: 'uint must be >= 0' });
 
 export type Uint = z.TypeOf<typeof Uint>;
