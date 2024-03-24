357f332133980e349c4349c5da1c9b2683a8a146
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-03-24 15:26:22 +0900

    Bazel rule to package a bazel binary for Pulumi (eventually to package a js binary).

---
+++ ts/pulumi/lib/docker/oci_image.tmpl.ts
@@ -0,0 +1,28 @@
+import { Image, ImageArgs } from "@pulumi/awsx/ecr";
+import { ComponentResource, ComponentResourceOptions } from "@pulumi/pulumi";
+
+
+export interface Args {
+	image: Omit<ImageArgs, 'dockerfile'>
+}
+
+export class __ClassName extends ComponentResource {
+	image: Image
+	constructor(
+		name: string,
+		args: Args,
+		opts?: ComponentResourceOptions
+	) {
+		super('__TYPE', name, args, opts);
+
+		this.image = new Image(`${name}_image`, {
+			dockerfile: '__DOCKERFILE_PATH',
+			...args.image
+		}, { parent: this })
+
+
+		super.registerOutputs({ image: this.image })
+	}
+
+
+}
