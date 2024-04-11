02577090af3703e7b409abf5c93553410a2deb79
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2024-04-11 17:28:58 +0000

    Allow next.js to consume input source maps.

--- ts/next.js/next.config.ts
+++ ts/next.js/next.config.ts
@@ -19,3 +19,18 @@ export const output = 'export';
 export const generateBuildId = async () => { /*REPLACE*/ throw new Error() /*REPLACE*/ };
 
 export const productionBrowserSourceMaps = true;
+
+export const future = { webpack5: true };
+
+type StopGapConfigT = {
+	module: { rules: { test: RegExp; use: string[]; enforce: string }[] };
+};
+
+export const webpack = (config: StopGapConfigT) => {
+	config.module.rules.push({
+		test: /\.js$/,
+		use: ['source-map-loader'],
+		enforce: 'pre',
+	});
+	return config;
+};
