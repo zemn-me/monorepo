616a67da5e7edb47af07fc5332666daa7fc3ac0a
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-04-24 00:24:20 +0100

    Factorio: Commit another big hero blueprint.

--- ts/factorio/uint32.ts
+++ ts/factorio/uint32.ts
@@ -2,10 +2,7 @@ import { z } from 'zod';
 
 import { Uint } from '#root/ts/factorio/uint';
 
-export const Uint32 = Uint.refine(
-	z => z > 0 && z < 0b11111111111111111111111111111111,
-	{
-		message: 'Uint32 must be greater than zero and less than Uint32_max',
-	}
-);
+export const Uint32 = Uint.refine(z => z < 0b11111111111111111111111111111111, {
+	message: 'Uint32 must be less than Uint32_max',
+});
 export type Uint32 = z.TypeOf<typeof Uint32>;
