e3239b6cebd5015f52b9278be053768b731af7bd
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-05-02 03:09:52 +0000

    Introduce image transcoding code, and transcode website banner.

--- go/flag/flagutil/fileflag.go
+++ go/flag/flagutil/fileflag.go
@@ -1,15 +1,27 @@
 package flagutil
 
 import (
+	"errors"
+	"flag"
 	"io"
 	"io/fs"
 	"os"
 	"strings"
 )
 
+type FileLike interface {
+	fs.File
+	io.Writer
+}
+
 type ioCombiner struct {
 	io.ReadCloser
 	io.WriteCloser
+}
+
+// Stat() is unsupported and returns an error.
+func (i ioCombiner) Stat() (fs.FileInfo, error) {
+	return nil, errors.New("Unsupported")
 }
 
 func (i ioCombiner) Close() (err error) {
@@ -21,45 +33,45 @@ func (i ioCombiner) Close() (err error) {
 	return
 }
 
-type nopWriter struct {
-	io.Writer
-}
-
-func (n nopWriter) Close() (err error) { return nil }
-
-type nopReader struct {
-	io.Reader
-}
-
-func (n nopReader) Close() (err error) { return nil }
-
+// FileFlag is a flag.Value which
+// opens a file of the given input string.
+//
+// It recognises the special file name '-', which
+// is considered to be stdin for reads and stdout
+// for writes.
+//
+// If MinusValue is set, it will be used as the value
+// of FileLike when the flag value is '-'.
 type FileFlag struct {
-	io.ReadWriteCloser
+	FileLike
 	Flag  string
 	Flags int
 	Perm  fs.FileMode
+	// if set, we fall back to this when '-' is specified.
+	MinusValue FileLike
+}
+
+var _ flag.Value = &FileFlag{}
+
+func (f *FileFlag) Set(s string) (err error) {
+	if strings.TrimSpace(s) == "-" {
+		if f.MinusValue != nil {
+			f.FileLike = f.MinusValue
+			return
+		}
+	}
+
+	f.FileLike, err = os.OpenFile(s, f.Flags, f.Perm)
+	return
 }
 
 func (f FileFlag) String() string {
 	return f.Flag
 }
 
-func (f *FileFlag) Set(s string) (err error) {
-	if strings.TrimSpace(s) == "-" {
-		f.ReadWriteCloser = ioCombiner{
-			ReadCloser:  nopReader{os.Stdin},
-			WriteCloser: nopWriter{os.Stdout},
-		}
-		return
-	}
-
-	f.ReadWriteCloser, err = os.OpenFile(s, f.Flags, f.Perm)
-	return
-}
-
 func (f *FileFlag) Close() (err error) {
-	if f.ReadWriteCloser != nil {
-		err = f.ReadWriteCloser.Close()
+	if f.FileLike != nil {
+		err = f.FileLike.Close()
 	}
 
 	return
