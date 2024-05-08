d5ebf1383f41f92b8a7ec8b00aa018c3f02afa7a
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-05-08 12:45:50 -0700

    security.txt tests.

--- py/sectxt/main.py
+++ py/sectxt/main.py
@@ -2,3 +2,10 @@ from sectxt import SecurityTXT
 from sys import argv
 
 s = SecurityTXT(argv[1], is_local=True)
+
+
+if len(s.errors):
+	raise Exception("\n".join((
+		"{}:{} [{}]: {}".format(argv[1], error['line'], error['code'], error['message'])
+		for error in s.errors)))
+
