2f1814d61bdf3487e646cf2b690acc68d046ce78
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-02 12:12:21 -0700

    Attempt at an article renderer.
    zemn.me: abstract hero video to own component.

---
+++ ts/react/lang/date.tsx
@@ -0,0 +1,72 @@
+import { Fragment, memo } from "react";
+
+import { useLocale } from "#root/ts/react/lang/useLocale.js";
+
+export interface DateProps {
+	readonly date: Date
+}
+
+interface ReactyDateTimeFormatPart extends Omit<Intl.DateTimeFormatPart, 'value'> {
+	value: string | React.ReactElement
+}
+
+// TODO: should be a datetime (or whatever) html element
+
+export const Date = memo(function(props: DateProps) {
+	const [ language ] = useLocale();
+	const locale = new Intl.Locale(language);
+	// i think the types are wrong
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const dateParts: ReactyDateTimeFormatPart[] = new Intl.DateTimeFormat(locale as any, { dateStyle: 'full' })
+		.formatToParts(props.date);
+
+
+	const ordinalIndex = dateParts.findIndex(v => v.type === "day");
+	if (ordinalIndex != -1) switch (locale.language) {
+		case "en": {
+			const day = dateParts[ordinalIndex]!;
+			const pluralRule = new Intl.PluralRules(language, {
+				type: 'ordinal'
+			}).select(+day.value);
+
+			dateParts[ordinalIndex]!.value = <>
+				the {dateParts[ordinalIndex]!.value}<sup>
+					{
+						{
+							one: "st",
+							two: "nd",
+							few: "rd",
+							other: "th",
+							zero: null,
+							many: null
+						}[pluralRule]
+					}
+				</sup> of
+			</>
+
+			dateParts[dateParts.findIndex(v => v.type === "weekday")]!.value += ",";
+
+			break;
+		}
+		case "it": {
+			if (ordinalIndex == -1) break;
+
+			// locate month name
+			const monthName = dateParts.find(v => v.type === "month");
+			if (monthName === undefined) break;
+			let suffix = "";
+			if (monthName.value.toString().endsWith("o")) suffix = "º";
+			if (monthName.value.toString().endsWith("a")) suffix = "ª";
+
+
+			dateParts[ordinalIndex]!.value += suffix;
+
+			break;
+		}
+
+	}
+
+	const toRender = dateParts.map((v, i) => <Fragment key={i}>{v.value}</Fragment>);
+
+	return <time dateTime={props.date.toString()} lang={locale.toString()}>{toRender}</time>
+})
