4ad50a79d1c48f2cc3607ee723a95b829687ac0a
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-02 00:47:56 -0700

    Attempt at an article renderer.

--- project/zemn.me/app/article/2014/csp/page.tsx
+++ project/zemn.me/app/article/2014/csp/page.tsx
@@ -1,12 +1,18 @@
 'use client';
-import { default as Content } from '#root/mdx/article/2014/csp';
+import { default as Content, frontmatter as metadata } from '#root/mdx/article/2014/csp';
 import { HeroVideo } from '#root/project/zemn.me/components/HeroVideo/hero_video.js';
 import style from '#root/project/zemn.me/app/article/2014/csp/style.module.css';
+import { date }  from '#root/ts/time/index.js';
+import { Date } from '#root/ts/react/lang/date.js';
+
 
 
 export default function Page() {
-	return <div className={style.article}>
-		<HeroVideo/>
-		<Content/>
+	return <div className={style.container}>
+		<HeroVideo className={style.heroVideo} />
+		<article>
+			<Date date={date.parse(metadata.date)}/>
+			<Content/>
+		</article>
 	</div>
 }
--- project/zemn.me/assets/kenwood_snow/kenwood_snow.tsx
+++ project/zemn.me/assets/kenwood_snow/kenwood_snow.tsx
@@ -12,9 +12,9 @@ export function VideoSources() {
 			{/* These assets only work because they're in the public directory */}
 			{/* in the root of this next.js project via a copy_to_bin rule. */}
 			{/* no other magic is happening here! */}
-			<source src="kenwood_snow.mp4" type="video/mp4" />
-			<source src="kenwood_snow.webm" type="video/webm" />
-			<source src="kenwood_snow.ogv" type="video/ogv" />
+			<source src="/kenwood_snow.mp4" type="video/mp4" />
+			<source src="/kenwood_snow.webm" type="video/webm" />
+			<source src="/kenwood_snow.ogv" type="video/ogv" />
 		</>
 	);
 }
