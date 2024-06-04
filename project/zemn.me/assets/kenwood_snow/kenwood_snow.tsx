aabe4c87fa3989594c3b570e1bea41069dfb7cc2
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-04 09:15:24 -0700

    Second attempt at fixing https://github.com/zemn-me/monorepo/pull/5257 by moving files around.

--- project/zemn.me/assets/kenwood_snow/kenwood_snow.tsx
+++ project/zemn.me/assets/kenwood_snow/kenwood_snow.tsx
@@ -1,4 +1,6 @@
-export { default as poster } from 'project/zemn.me/public/kenwood_snow.jpg';
+import { mp4, ogv, poster, webm } from '#root/project/zemn.me/public/kenwood_snow';
+
+export { poster }
 
 /**
  * @returns A React element representing the Kenwood video asset
@@ -8,13 +10,10 @@ export { default as poster } from 'project/zemn.me/public/kenwood_snow.jpg';
 export function VideoSources() {
 	return (
 		<>
-			{/* important note!! */}
-			{/* These assets only work because they're in the public directory */}
-			{/* in the root of this next.js project via a copy_to_bin rule. */}
-			{/* no other magic is happening here! */}
-			<source src="/kenwood_snow.mp4" type="video/mp4" />
-			<source src="/kenwood_snow.webm" type="video/webm" />
-			<source src="/kenwood_snow.ogv" type="video/ogv" />
+			<source src={mp4} type="video/mp4" />
+			<source src={webm} type="video/webm" />
+			<source src={ogv} type="video/ogv" />
 		</>
 	);
 }
+
