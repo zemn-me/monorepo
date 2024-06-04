8ff7ba65084f422e8bb8cf6bb55e5b78460b9237
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-04 09:15:24 -0700

    Second attempt at fixing https://github.com/zemn-me/monorepo/pull/5257 by moving files around.

---
+++ project/zemn.me/public/kenwood.ts
@@ -0,0 +1,5 @@
+export { default as poster } from '#root/project/zemn.me/public/kenwood.jpg';
+export const mp4 = `/kenwood.mp4`;
+export const webm = `/kenwood.webm`;
+export const ogv = `/kenwood.ogv`;
+export const jpg = `/kenwood.jpg`;
