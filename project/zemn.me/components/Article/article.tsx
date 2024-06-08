1609ae79ed8cc654fdb6b04cb1397fc05855f8d8
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-08 14:52:28 -0700

    Use zod parser with article date.

--- project/zemn.me/components/Article/article.tsx
+++ project/zemn.me/components/Article/article.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import style from '#root/project/zemn.me/app/article/2014/csp/style.module.css';
 import { ArticleProps } from '#root/project/zemn.me/components/Article/article_types.js';
+import style from '#root/project/zemn.me/components/Article/style.module.css';
 import { HeroVideo } from '#root/project/zemn.me/components/HeroVideo/hero_video.js';
 import { Date } from '#root/ts/react/lang/date.js';
-import { date } from '#root/ts/time/index.js';
+import { nativeDateFromUnknownSimpleDate } from '#root/ts/time/date.js';
 
 export function Article(props: ArticleProps) {
 	return <div className={style.container}>
 		<HeroVideo className={style.heroVideo} />
 		<article>
-			{/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-			<Date date={date.parse(props.date as any)}/>
+			<Date date={nativeDateFromUnknownSimpleDate.parse(props.date)}/>
 			{props.children}
 		</article>
 	</div>
 }
+
 
