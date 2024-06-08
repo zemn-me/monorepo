14d6d1a4084f8a8a51022c020be1ac8fa73b436b
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-08 13:20:21 -0700

    Abstract out mdx article renderer.

--- project/zemn.me/app/article/2014/csp/page.tsx
+++ project/zemn.me/app/article/2014/csp/page.tsx
@@ -1,19 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import { default as Content, frontmatter as metadata } from '#root/mdx/article/2014/csp';
-import style from '#root/project/zemn.me/app/article/2014/csp/style.module.css';
-import { HeroVideo } from '#root/project/zemn.me/components/HeroVideo/hero_video.js';
-import { Date } from '#root/ts/react/lang/date.js';
-import { date } from '#root/ts/time/index.js';
+import { Metadata } from 'next/types';
+
+import { default as Content, frontmatter } from '#root/mdx/article/2014/csp';
+import { Article } from '#root/project/zemn.me/components/Article/article.js';
+import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
 
 
 
 export default function Page() {
-	return <div className={style.container}>
-		<HeroVideo className={style.heroVideo} />
-		<article>
-			<Date date={date.parse(metadata.date as any)}/>
+	return <Article {...frontmatter}>
 			<Content/>
-		</article>
-	</div>
+	</Article>
 }
+
+export const metadata: Metadata = articleMetadata(frontmatter);
