40125da51b1e58ea0369adc6aedfff00d34a8bf9
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-09 01:14:35 -0700

    Apply a generic template that looks good to all pages.

--- project/zemn.me/components/Article/article.tsx
+++ project/zemn.me/components/Article/article.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import { ArticleProps } from '#root/project/zemn.me/components/Article/article_types.js';
 import style from '#root/project/zemn.me/components/Article/style.module.css';
-import { HeroVideo } from '#root/project/zemn.me/components/HeroVideo/hero_video.js';
 import { Date } from '#root/ts/react/lang/date.js';
 import { date } from '#root/ts/time/index.js';
 
 export function Article(props: ArticleProps) {
 	return <div className={style.container}>
-		<HeroVideo className={style.heroVideo} />
 		<article>
 			{/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
 			<Date date={date.parse(props.date as any)}/>
