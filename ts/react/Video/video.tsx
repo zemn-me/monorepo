d0f12f0ae4e7d8024c34a465d52ea0afe9600d3f
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-15 03:53:35 -0700

    zemn.me: support reduced motion.

---
+++ ts/react/Video/video.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/forbid-elements */
+'use client';
+import { useMediaQuery } from "#root/ts/react/useMediaQuery/useMediaQuery.js"
+
+type BaseVideoProps = JSX.IntrinsicElements["video"]
+
+export interface VideoProps extends BaseVideoProps {
+
+}
+
+export function Video(props: VideoProps) {
+	const prefersReducedMotion = useMediaQuery(
+		"(prefers-reduced-motion: reduce)"
+	);
+
+	const autoPlay = !prefersReducedMotion && props.autoPlay;
+
+
+
+	return <video {...{ ...props, autoPlay: autoPlay }}/>
+}
