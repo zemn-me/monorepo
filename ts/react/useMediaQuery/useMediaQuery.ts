6b32fd9e667b3a747988a91d6c326ada184ee79d
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-15 03:53:35 -0700

    zemn.me: support reduced motion.

---
+++ ts/react/useMediaQuery/useMediaQuery.ts
@@ -0,0 +1,18 @@
+'use client';
+import { useEffect, useState } from "react"
+
+
+export function useMediaQuery(mediaQuery: string | undefined) {
+	const query = mediaQuery && (matchMedia as typeof window.matchMedia | undefined) !== undefined ? matchMedia(mediaQuery) : undefined;
+	const [result, setResult] = useState<boolean | undefined>(
+		query?.matches
+	);
+
+
+	useEffect(() => {
+		query?.addEventListener('change', e => setResult(e.matches));
+	}, [query, setResult]);
+
+
+	return result;
+}
