6111ec08cb12022c65b54864e7b96183613b95b2
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-06-30 11:43:14 +0900

    Redirects to show target.

---
+++ ts/next.js/component/Redirect/blurb.tsx
@@ -0,0 +1,15 @@
+import Link from "#root/project/zemn.me/components/Link/Link.js";
+import { Prose } from "#root/project/zemn.me/components/Prose/prose.js"
+
+
+interface BlurbProps {
+	readonly to: URL | string
+}
+
+export function RedirectBlurb(props: BlurbProps) {
+	const target = new URL(props.to);
+	const text = target.protocol === "https:" ?
+		target.host
+		: target.origin;
+	return <Prose><i>You are being redirected to <Link href={props.to}>{text}</Link>. Please wait.</i></Prose>
+}
