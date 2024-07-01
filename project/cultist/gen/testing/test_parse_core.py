8f28996650d728891cdec364f2941c19215cb336
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-07-01 14:02:02 +0900

    Set up rules_python gazelle

--- project/cultist/gen/testing/test_parse_core.py
+++ project/cultist/gen/testing/test_parse_core.py
@@ -1,11 +1,11 @@
 import unittest
 #from gen import core_pb2 as core
-import project.cultist.gen.core
+import project.cultist.gen.testing.core
 
 
 class TestBasic(unittest.TestCase):
     def parse_core_smoke(self):
-        cultist.gen.core.load()
+		project.cultist.gen.testing.core.load()
 
 if __name__ == '__main__':
     unittest.main()
