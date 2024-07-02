83efe750cdc49a2533f02ae6f95c0b15b1640425
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-07-02 23:45:29 +0900

    Validate the PNPM lockfile only after other tests have passed.

--- ci/presubmit.ts
+++ ci/presubmit.ts
@@ -64,48 +64,6 @@ const cmd = new Command('presubmit')
 			);
 		console.log('Executing in detected directory', cwd);
 
-		// validate the pnpm lockfile.
-		if (!o.dangerouslySkipPnpmLockfileValidation) {
-			await Task('Validate the PNPM lockfile.')(
-				new Promise<void>((ok, err) =>
-					child_process
-						.spawn(
-							// TODO(zemnmez): this should be changed to be the local bazel
-							// version once this issue is fixed:
-							// https://github.com/pnpm/pnpm/issues/6962
-							'npm',
-							[
-								'exec',
-								'--yes',
-								'--package',
-								'pnpm@8.6.12',
-								'--',
-								'pnpm',
-								'i',
-								'--frozen-lockfile',
-								'--lockfile-only',
-								// Due to this issue:
-								// https://github.com/pnpm/pnpm/issues/6962
-								// specifying --dir causes --frozen-lockfile to be ignored.
-								/*							'--dir',
-							// aspect_rules_js uses this as their example;
-							// I think the script might pull in some patches etc
-							// so we have to be a bit smarter than just setting
-							// cwd when we launch the binary.
-							cwd, */
-							],
-							{
-								cwd,
-								stdio: 'inherit',
-							}
-						)
-						.on('close', code =>
-							code != 0 ? err(`exit ${code}`) : ok()
-						)
-				)
-			);
-		}
-
 		// this is placed first since it builds everything in parallel
 		// so serial operations coming after take 0 time to build.
 		if (!o.skipBazelTests) {
@@ -114,7 +72,6 @@ const cmd = new Command('presubmit')
 			);
 			// perform all the normal tests
 		}
-
 
 		await Task('check if we might need to run go.mod')(new Promise<void>((ok, error) =>
 			child_process
@@ -203,7 +160,47 @@ const cmd = new Command('presubmit')
 				)
 		));
 
-
+		// validate the pnpm lockfile.
+		if (!o.dangerouslySkipPnpmLockfileValidation) {
+			await Task('Validate the PNPM lockfile.')(
+				new Promise<void>((ok, err) =>
+					child_process
+						.spawn(
+							// TODO(zemnmez): this should be changed to be the local bazel
+							// version once this issue is fixed:
+							// https://github.com/pnpm/pnpm/issues/6962
+							'npm',
+							[
+								'exec',
+								'--yes',
+								'--package',
+								'pnpm@8.6.12',
+								'--',
+								'pnpm',
+								'i',
+								'--frozen-lockfile',
+								'--lockfile-only',
+								// Due to this issue:
+								// https://github.com/pnpm/pnpm/issues/6962
+								// specifying --dir causes --frozen-lockfile to be ignored.
+								/*							'--dir',
+							// aspect_rules_js uses this as their example;
+							// I think the script might pull in some patches etc
+							// so we have to be a bit smarter than just setting
+							// cwd when we launch the binary.
+							cwd, */
+							],
+							{
+								cwd,
+								stdio: 'inherit',
+							}
+						)
+						.on('close', code =>
+							code != 0 ? err(`exit ${code}`) : ok()
+						)
+				)
+			);
+		}
 
 		// attempt a deploy of pulumi to staging, and tear it down.
 		if (!o.skipPulumiDeploy) {
