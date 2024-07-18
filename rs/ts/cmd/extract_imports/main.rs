7a9dc0b2ece2971ef4ea3c09b6064260481a7a4d
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-07-17 19:52:13 -0700

    Introduce extract_imports, a rust binary which can extract typescript import lists.

---
+++ rs/ts/cmd/extract_imports/main.rs
@@ -0,0 +1,46 @@
+/// Extracts imports from typescript and javascript files.
+use std::{convert, env::args, io};
+
+use extract_imports::{extract_imports, ExtractImportsError};
+
+#[derive(Debug)]
+enum ExtractImportsCmdError {
+    ExtractImportsLibError(ExtractImportsError),
+    Io(io::Error),
+    String(String),
+}
+
+impl convert::From<&str> for ExtractImportsCmdError {
+    fn from(error: &str) -> Self {
+        Self::String(error.into())
+    }
+}
+
+impl convert::From<String> for ExtractImportsCmdError {
+    fn from(error: String) -> Self {
+        Self::String(error)
+    }
+}
+
+impl convert::From<io::Error> for ExtractImportsCmdError {
+    fn from(error: io::Error) -> Self {
+        Self::Io(error)
+    }
+}
+
+impl convert::From<ExtractImportsError> for ExtractImportsCmdError {
+    fn from(error: ExtractImportsError) -> Self {
+        Self::ExtractImportsLibError(error)
+    }
+}
+
+fn act() -> Result<(), ExtractImportsCmdError> {
+    Result::Ok(println!(
+        "{}",
+        extract_imports(args().nth(1).ok_or("Please specify a target!")?)?.join("\n")
+    ))
+}
+
+fn main() {
+    act().unwrap()
+}
