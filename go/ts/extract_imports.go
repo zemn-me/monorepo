f1075112052ea6b3ee8640add7b34d4d202d51eb
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-07-19 19:21:46 -0700

    Typescript for Gazelle

---
+++ go/ts/extract_imports.go
@@ -0,0 +1,33 @@
+package ts
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
+)
+
+// Extract imports from a typescript file with given location.
+//
+// This function calls a rust binary that must be present in runfiles. As such,
+// calling it from outside of Bazel will fail.
+func ExtractImports(fileName string) (imports [][]byte, err error) {
+	runfilesBase, err := bazel.RunfilesPath()
+
+	var output bytes.Buffer
+	cmd := exec.Command(filepath.Join(runfilesBase, "rs/ts/cmd/extract_imports/extract_imports"), fileName)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &output
+
+	if err = cmd.Run(); err != nil {
+		err = fmt.Errorf("ExtractImports: %v\n\n%+q", err, stderr.Bytes())
+		return
+	}
+
+	imports = bytes.Split(output.Bytes(), []byte("\n"))
+
+	return
+}
