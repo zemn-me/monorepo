0f01c59ef0dd9d3696d2d1cb9d5a9fa81f897255
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-07-31 19:49:56 -0700

    Add frame size recommendations to the framing calculator.

---
+++ ts/option2/option2.ts
@@ -0,0 +1,65 @@
+import { NewType } from "#root/ts/NewType.js";
+import * as types from '#root/ts/option2/types.js';
+import { Err, Ok, Result } from "#root/ts/result2/result2.js";
+
+
+class impl<T> extends NewType<T> {
+	is_none<T>(this: Option<T>): this is None { return types.is_none(this.value) }
+	is_some<T>(this: Option<T>): this is Some<T> { return types.is_some(this.value) }
+	unwrap<T>(this: Option<T>): T { return types.unwrap(this.value) }
+	unwrap_or<T>(this: Option<T>, fallback: T): T { return types.unwrap_or(this.value, fallback) }
+	unwrap_or_else<T>(this: Option<T>, fallback: () => T): T {
+		return types.unwrap_or_else(this.value, fallback)
+	}
+	and_then<T, O>(this: Option<T>, f: (v: T) => O): Option<O> {
+		return new impl(types.and_then(this.value, f));
+	}
+	flatten<T>(this: Option<Option<T>>): Option<T> {
+		if (this.is_none()) return None;
+		return this.unwrap();
+	}
+	zip<T, TT>(this: Option<T>, other: Option<TT>): Option<[T, TT]> {
+		return this.and_then(v => other.and_then(vv => [v, vv ] as [T, TT])).flatten()
+	}
+	and<V>(this: Option<unknown>, v: V): Option<V> {
+		if (this.is_some()) return Some(v);
+
+		return None;
+	}
+
+	ok_or<T, E>(this: Option<T>, err: E): Result<T, E> {
+		if (this.is_none()) return Err( err );
+		return Ok(this.unwrap());
+	}
+
+	ok_or_else<T, E>(this: Option<T>, err: () => E): Result<T, E> {
+		if (this.is_none()) return Err( err() );
+		return Ok(this.unwrap());
+	}
+
+	/**
+	 * Not in rust. Needed because I can't have Option and Result
+	 * mutually dependent so I can't implement Result.ok()
+	 *
+	 * Absorbs the inner result, discarding its error, if any.
+	 */
+	absorb_result<T>(this: Option<Result<T, unknown>>): Option<T> {
+		if (this.is_none()) return this;
+
+		const inner = this.unwrap() ;
+
+		if (inner.is_err()) return None;
+
+		return Some(inner.unwrap());
+	}
+
+}
+
+export function Some<T>(v: T): Some<T> {
+	return new impl(types.Some(v))
+}
+
+export type Some<T> = impl<types.Some<T>>
+export type None = impl<types.None>;
+export type Option<T> = impl<types.Option<T>>
+export const None: None = new impl(types.None);
---
+++ ts/option2/types.ts
@@ -0,0 +1,49 @@
+export const _some = Symbol();
+export const _none = Symbol();
+
+export type Some<T> = { [_some]: T, [_none]?: undefined }
+export type None = { [_none]: true, [_some]?: undefined }
+export type Option<T> = Some<T> | None
+
+export const None: None = { [_none]: true };
+
+export function Some<T>(v: T): Some<T> {
+	return { [_some]: v }
+}
+
+export function is_some<T>(v: Option<T>): v is Some<T> {
+	return !is_none(v)
+}
+
+export function is_none(v: Option<unknown>): v is None {
+	return _none in v
+}
+
+export function unwrap<T>(v: Option<T>): T {
+	if (is_some(v)) return v[_some];
+	throw new Error("Cannot unwrap Option; has no value.");
+}
+
+export function unwrap_or<T>(v: Option<T>, fallback: T): T {
+	if (is_none(v)) return fallback;
+
+	return v[_some]
+}
+
+export function unwrap_or_else<T>(v: Option<T>, fallback: () => T): T {
+	if (is_none(v)) return fallback();
+
+	return v[_some]
+}
+
+export function and_then<T, O>(v: Option<T>, f: (v: T) => O): Option<O> {
+	if (is_none(v)) return v;
+
+	return Some(f(unwrap(v)))
+}
+
+export function flatten<T>(v: Option<Option<T>>): Option<T> {
+	if (is_none(v)) return v;
+	return unwrap(v);
+}
+
