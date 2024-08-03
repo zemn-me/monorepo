c72cf6401e33b4925e089e5beb460633e76c6cfd
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-08-02 23:29:30 -0700

    Factorio blueprints to parse more safely.

--- ts/zod/util.ts
+++ ts/zod/util.ts
@@ -1,5 +1,14 @@
-import { z } from 'zod';
+import { SafeParseReturnType, z, ZodError } from 'zod';
+
+import { Err, Ok, Result } from '#root/ts/result.js';
 
 export function zodUnsafe<T>() {
 	return z.custom<T>(v => v);
+}
+
+export function resultFromZod<I, O>(
+	t: SafeParseReturnType<I, O>
+): Result<O, ZodError<I>> {
+	if (t.success) return Ok(t.data);
+	return Err(t.error);
 }
