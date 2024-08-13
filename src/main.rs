9b1fa632ab38e87202b5ee86248921a08f2c15bd
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-08-13 11:24:45 -0700

    Commit basic discord bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 // even though this file has no code in it, if we delete it i think rust stops
 // working. I'm fairly sure this is because cargo needs this to exist to
 // be convinced to run.
+
+fn main() {}
