811427b90315f4697e9fe560b0f7bb3ebf9736be
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-08-14 00:59:16 -0700

    Implement blackball election algorithm for discord

--- rs/cmd/discord_blackball_bot/main.rs
+++ rs/cmd/discord_blackball_bot/main.rs
@@ -1,18 +1,107 @@
+use std::collections::HashSet;
 use std::env;
 
-use serenity::async_trait;
+use serenity::all::{parse_user_mention, EditRole, GuildId, UserId};
+use serenity::{async_trait, FutureExt};
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 
 struct Handler;
 
+#[derive(Debug)]
+enum BotError {
+    MissingInviteTarget,
+    MissingGuildId,
+}
+
+
+impl Handler {
+    async fn attempt_election(&self, ctx: Context, who: UserId) -> Result<()> {
+
+    }
+
+    async fn get_role_by_name(&self, ctx: Context, guild: GuildId, name: String) -> Result<Option<Role>> {
+         Ok(guild.roles(&ctx.http).await?.into_iter()
+            .find_map(|(rid, r)| {
+                if r.name == name {
+                    Ok(Some(r))
+                }
+
+                None
+            }
+        ));
+    }
+
+    async fn get_or_create_applicant_role(&self, ctx: Context, guild: GuildId) -> Result<Role> {
+        let x = self.get_role_by_name(ctx, guild, "applicant".into()).await?
+            .unwrap_or_else(|| guild.create_role(&ctx.http,
+                EditRole::new().name("applicant")
+            ));
+
+    }
+
+
+    async fn setup_roles(&self, ctx: Context, guild: GuildId) -> Result<()> {
+
+    }
+
+    async fn handle_message(&self, ctx: Context, msg: Message) -> Result<()> {
+        if !msg.content.starts_with("!elect") {
+            return;
+        }
+
+        if !msg.author.has_role(&ctx.http, msg.guild_id, )
+
+        let target_user: UserId = msg.content.split_whitespace().nth(1)
+            .and_then(|mention| parse_user_mention(mention))
+            .unwrap_or(BotError::MissingInviteTarget)?;
+
+        self.attempt_election(ctx, target_user)
+    }
+
+    async fn n_members(&self, ctx: Context, guild: GuildId) -> Result<u64> {
+        guild.members(
+            &ctx.http,
+            None,
+            None,
+        ).await?.into_iter().count()
+    }
+
+    async fn n_sponsors(&self, ctx: Context, n_members: u64) -> Result<u64> {
+        match n_members {
+            n if n < 5 => 1,
+            _ => 2
+        }
+    }
+
+    /// Calculate the number of votes needed to elect a new member.
+    async fn quorum(&self, ctx: Context, n_members: u64) -> Result<u64> {
+        // at least 12 members are needed if there are more than
+        // 12 members in the club, otherwise all members must vote.
+        match n_members {
+            n if n < 12 => n,
+            _ => 12
+        }
+    }
+
+    /// how many black balls at minimum are required to reject an election.
+    async fn n_balls_exclude(&self, ctx: Context, ballot_size: u64) -> Result<u64> {
+        if ballot_size > 18 {
+            2
+        }
+
+        1
+    }
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
-            }
+        match self.handle_message(ctx, msg) {
+            Ok => (),
+            Err(e) => msg.channel_id.say(&ctx.http, e).await {
+                println!("Error sending message: {why?:}")
+            },
         }
     }
 }
