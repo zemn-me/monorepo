f509cf13c2350d8cd3ef5b845b64b0df33223a4b
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-08-17 15:13:11 -0700

    Allow fix api to be run via `bazel run fix [target]`.
    Also allow fix api to do 2 fixes at a time.

--- bzl/fix_api/fix_all.sh
+++ bzl/fix_api/fix_all.sh
@@ -18,4 +18,16 @@ while read COMMAND; do
 done
 
 
+bazel query $PATTERN | \
+grep '.fix$' | \
+sed 's/\.fix$//g' | \
+xargs bazel test --keep_going | \
+grep FAILED | \
+awk '{ print $1 }' | \
+sed 's/$/.fix/g' |
+while read COMMAND; do
+    bazel run $COMMAND
+done
+
+
 popd
