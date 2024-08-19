915dbdcadd735969160ed4c0d4010cb828086916
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-08-19 10:51:27 -0700

    Add ibazel

--- sh/tools.go
+++ sh/tools.go
@@ -4,8 +4,9 @@
 package tools
 
 import (
+	_ "github.com/bazelbuild/bazel-watcher/cmd/ibazel"
 	_ "github.com/go-delve/delve/cmd/dlv"
+	_ "github.com/itchyny/gojq"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "honnef.co/go/tools/cmd/staticcheck"
-	_ "github.com/itchyny/gojq"
 )
