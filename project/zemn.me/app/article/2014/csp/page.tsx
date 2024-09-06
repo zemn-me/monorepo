862a90a2706d018756de0db132a9a36dfc6ef6ab
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-09-06 11:30:07 +0100

    Generating an article's table of contents! using portals!

--- project/zemn.me/app/article/2014/csp/page.tsx
+++ project/zemn.me/app/article/2014/csp/page.tsx
@@ -1,15 +1,13 @@
 import { Metadata } from 'next/types';
 
-import { default as Content, frontmatter } from '#root/mdx/article/2014/csp';
-import { Article } from '#root/project/zemn.me/components/Article/article.js';
+import * as Article from '#root/mdx/article/2014/csp';
 import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
+import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article.js';
 
 
 
 export default function Page() {
-	return <Article {...frontmatter}>
-			<Content/>
-	</Article>
+	return <MDXArticle {...Article}/>
 }
 
-export const metadata: Metadata = articleMetadata(frontmatter);
+export const metadata: Metadata = articleMetadata(Article.frontmatter);
