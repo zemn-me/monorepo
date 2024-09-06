d5bd17f20e0e7599d62f20af40a25a7026a10542
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-09-06 11:28:34 +0100

    Generating an article's table of contents! using portals!
    THOMAS: you must remove the billion BUILD files you accidentally generated in this commit

--- project/zemn.me/app/article/2019/cors/page.tsx
+++ project/zemn.me/app/article/2019/cors/page.tsx
@@ -1,16 +1,15 @@
 import { Metadata } from 'next/types';
 
-import { default as Content, frontmatter } from '#root/mdx/article/2019/cors/cors';
-import { Article } from '#root/project/zemn.me/components/Article/article.js';
+import * as Article from '#root/mdx/article/2019/cors/cors';
 import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
+import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article';
+
 
 
 
 export default function Page() {
-	return <Article {...frontmatter}>
-			<Content/>
-	</Article>
+	return <MDXArticle {...Article}/>
 }
 
-export const metadata: Metadata = articleMetadata(frontmatter);
+export const metadata: Metadata = articleMetadata(Article.frontmatter);
 
