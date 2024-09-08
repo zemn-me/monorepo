649d47ffdb74c11bbe9ba3743fc1b4534cc0115f
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-09-08 23:26:41 +0100

    Typescript for Gazelle
    Gazelle for mdx.

--- ts/reddit/reddit_test.ts
+++ ts/reddit/reddit_test.ts
@@ -1,7 +1,7 @@
 import { expect, it } from '@jest/globals';
 
-import { RedditSearchResponse } from '#root/ts/reddit/reddit';
-import { response_example } from '#root/ts/reddit/response_example';
+import { RedditSearchResponse } from '#root/ts/reddit/reddit.js';
+import { response_example } from '#root/ts/reddit/response_example.js';
 
 it('should parse an example response correctly', () => {
 	expect(() => RedditSearchResponse.parse(response_example)).not.toThrow();
