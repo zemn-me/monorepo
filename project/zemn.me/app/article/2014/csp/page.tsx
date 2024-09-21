51265510108931533b6e11e61e340db5fa59bb20
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-09-20 20:42:23 -0700

    Typescript for Gazelle
    Gazelle for mdx.

--- project/zemn.me/app/article/2014/csp/page.tsx
+++ project/zemn.me/app/article/2014/csp/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next/types';
 
-import { frontmatter } from '#root/mdx/article/2014/csp.js';
-import Content from '#root/mdx/article/2014/csp.js'
+import { default as Content, frontmatter } from '#root/mdx/article/2014/csp.js';
 import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
 import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article.js';
 
