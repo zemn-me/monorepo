f06b8870a2a8ec5cec11f3f2a32d59a5d03e7e31
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-09-20 20:05:04 -0700

    Gazelle generated

--- project/zemn.me/app/article/2019/cors/page.tsx
+++ project/zemn.me/app/article/2019/cors/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next/types';
 
 import Content, { frontmatter } from '#root/mdx/article/2019/cors/cors.js';
 import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
-import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article';
+import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article.js';
 
 
 
