e709c07788576800a2ef165c9ce390139dbf4b36
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-10 09:06:36 -0800

    Try to work out whast that one weird file is.

--- sh/postUpgrade.sh
+++ sh/postUpgrade.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p $HOME/.bazelisk
-BAZELISK_HOME=$HOME/.bazelisk CARGO_BAZEL_REPIN=true bazelisk run --tool_tag=postupgrade //ci:postupgrade
+echo "let's find out what that mysterious file is..."
+ls -la $HOME/.cache
+cat $HOME/.cache
+cat $HOME/.cache/bazelisk
+CARGO_BAZEL_REPIN=true bazelisk run --tool_tag=postupgrade //ci:postupgrade
