5af9e1cfbde1c5af646bf08e938a93c45eb77e02
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-11 08:51:38 -0800

    Update postUpgrade.sh

--- sh/postUpgrade.sh
+++ sh/postUpgrade.sh
@@ -3,7 +3,7 @@ echo "let's find out what that mysterious file is..."
 ls -la $HOME/.cache
 cat $HOME/.cache
 cat $HOME/.cache/bazelisk
-CARGO_BAZEL_REPIN=true bazelisk run --tool_tag=postupgrade //ci:postupgrade
+CARGO_BAZEL_REPIN=true npx --yes @bazel/bazelisk run -- --tool_tag=postupgrade //ci:postupgrade
 ls -la $HOME/.cache
 cat $HOME/.cache
 cat $HOME/.cache/bazelisk
