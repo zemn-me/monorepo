b85b77682b496c7926db001923b6ae24c5832dda
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2024-11-11 11:03:03 -0800

    Update postUpgrade.sh

--- sh/postUpgrade.sh
+++ sh/postUpgrade.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-echo "let's find out what that mysterious file is..."
-ls -la $HOME/.cache
-cat $HOME/.cache
-cat $HOME/.cache/bazelisk
 CARGO_BAZEL_REPIN=true npx --yes @bazel/bazelisk run -- //ci:postupgrade
-ls -la $HOME/.cache
-cat $HOME/.cache
-cat $HOME/.cache/bazelisk
