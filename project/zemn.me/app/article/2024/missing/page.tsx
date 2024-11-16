cecb01e3d95765afc75e2452aae4f169d82af4bc
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-16 13:50:15 -0800

    Missing

---
+++ project/zemn.me/app/article/2024/missing/page.tsx
@@ -0,0 +1,17 @@
+
+import { Metadata } from 'next/types';
+
+import Content from '#root/mdx/article/2024/missing.js'
+import { frontmatter } from '#root/mdx/article/2024/missing.js';
+import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
+import { MDXArticle } from '#root/project/zemn.me/components/Article/mdx_article.js';
+
+
+
+export default function Page() {
+	return <MDXArticle {...{frontmatter}}>
+		<Content/>
+	</MDXArticle>
+}
+
+export const metadata: Metadata = articleMetadata(frontmatter);
