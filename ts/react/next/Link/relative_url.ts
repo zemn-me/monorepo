55185a5fa4c861bc268e916f1146b12c4ce1b538
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-17 09:50:58 -0800

    Link article "missing" in the timeline

---
+++ ts/react/next/Link/relative_url.ts
@@ -0,0 +1,10 @@
+import { NewType } from "#root/ts/NewType.js";
+
+/**
+ * A pathname that we pinky promise is relative
+ * to the current app.
+ */
+export class RelativeURL extends NewType<string> {
+
+}
+
