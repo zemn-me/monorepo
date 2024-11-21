c73bd289ddae6f9d3822b26b48e42b57eb5ade9b
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-21 10:30:56 -0800

    Add bluesky redirect route.

---
+++ project/zemn.me/app/bluesky/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next/types';
+
+import { links } from '#root/project/zemn.me/bio/index.js';
+import { Redirect } from '#root/project/zemn.me/components/Redirect/Redirect.js';
+
+export default function Page() {
+	return <Redirect to={links.get('bluesky')!.href} />;
+}
+
+export const metadata: Metadata = {
+	description: 'Redirect to my bluesky account',
+};
+
