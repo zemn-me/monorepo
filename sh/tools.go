e6ae896066f5be2fe23f9ecc9a5100711a1b2e78
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-24 17:21:59 -0800

    Add tfgen

--- sh/tools.go
+++ sh/tools.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-delve/delve/cmd/dlv"
 	_ "github.com/itchyny/gojq"
 	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
