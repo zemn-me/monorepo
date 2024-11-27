0780c92b3b86ed368c5655fc5a7c9acdc00a9f85
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-27 11:52:55 -0800

    Add structured author data to articles.

--- project/zemn.me/components/Article/article.tsx
+++ project/zemn.me/components/Article/article.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from 'react';
+import type { Article, WithContext } from 'schema-dts';
 
+import { schema } from '#root/project/zemn.me/bio/schema.js';
 import style from '#root/project/zemn.me/components/Article/style.module.css';
 import { tocSegment } from '#root/project/zemn.me/components/Article/toc_context.js'
 import { ArticleProps } from '#root/project/zemn.me/components/Article/types/article_types.js';
@@ -18,6 +20,18 @@ export function Article(props: ArticleProps) {
 			<tocSegment.Provider value={toc}>
 			{props.children}
 			</tocSegment.Provider>
+			<script
+				dangerouslySetInnerHTML={{__html: JSON.stringify({
+						'@context': 'https://schema.org',
+						'@type': 'Article',
+						'author': [
+							schema
+						],
+						headline: props.title,
+						datePublished: props.date? nativeDateFromUnknownSimpleDate.parse(props.date).toISOString(): undefined
+				} satisfies WithContext<Article>) }}
+				type="application/ld+json"
+			/>
 		</article>
 	</div>
 }
