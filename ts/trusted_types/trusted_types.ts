633eb3e7d53424e8ffba63aae265fdfdf483f5e3
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-11-29 13:18:23 -0800

    Add structured author data to articles.

---
+++ ts/trusted_types/trusted_types.ts
@@ -0,0 +1,33 @@
+"use client";
+import memoize from 'memoizee';
+import { useEffect } from 'react';
+import { trustedTypes } from 'trusted-types';
+
+const trustedTypesPolicy = memoize(() =>
+	trustedTypes.createPolicy("default", {
+		createHTML: v => v
+	})
+);
+
+/**
+ * Declare an HTML fragment as safe against XSS. If no policy
+ * is set, then this function is an identity operation.
+ */
+export function dangerouslyDeclareSafeHTML(html: string) {
+	// below is needed because window can be
+	// undefined in next prerender.
+	return (trustedTypesPolicy().createHTML)(html)
+}
+
+/**
+ * React component that declares a trusted types policy when mounted.
+ *
+ * Unmounting it has no effect.
+ */
+export function DeclareTrustedTypesPolicy() {
+	useEffect(() => {
+		trustedTypesPolicy();
+	}, []);
+	return null;
+}
+
