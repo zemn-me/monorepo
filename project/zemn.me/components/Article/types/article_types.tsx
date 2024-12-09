aac815799fadbf473f3e92ca0acb09047d7d4846
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-09 14:12:57 +0900

    Begin on code to automatically generate article routes.

--- project/zemn.me/components/Article/types/article_types.tsx
+++ project/zemn.me/components/Article/types/article_types.tsx
@@ -1,4 +1,19 @@
 import { ReactElement } from "react"
+import { z } from "zod"
+
+import { nativeDateFromUnknownSimpleDate } from "#root/ts/time/date.js"
+
+export const articleFrontmatter = z.object({
+	title: z.string().optional(),
+	subtitle: z.string().optional(),
+	date: nativeDateFromUnknownSimpleDate.optional(),
+	tags: z.string().array().optional(),
+	url_safe_name: z.string().optional()
+});
+
+export interface ArticleProps {
+	frontmatter: z.TypeOf<typeof articleFrontmatter>
+}
 
 export interface ArticleProps {
 	readonly title?: string
