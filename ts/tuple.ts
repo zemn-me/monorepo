402f912a1690114c0e598047407b5cc7d9c75b69
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-17 10:32:14 +0000

    Introduce type of indexing into a tuple.

--- ts/tuple.ts
+++ ts/tuple.ts
@@ -6,3 +6,16 @@ export type Tuple<T, N extends number> = N extends N
 type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N
 	? R
 	: _TupleOf<T, N, [T, ...R]>;
+
+ type StringToNumber<S extends string> =
+  S extends `${infer N extends number}` ? N : never;
+
+
+type TupleIndexStrings<N extends number>
+	= keyof Omit<
+		Tuple<unknown, N>,
+		keyof []
+	>
+
+export type TupleIndex<N extends number> = TupleIndexStrings<N> extends string ?
+	StringToNumber<TupleIndexStrings<N>> : never;
