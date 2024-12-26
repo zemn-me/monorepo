470e8fa2a1f52a7eb2c2724ab8939834c789c34f
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-26 18:53:06 +0000

    Devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export NONINTERACTIVE=1
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+
+# install sapling
+brew install --build-from-source sapling
+
