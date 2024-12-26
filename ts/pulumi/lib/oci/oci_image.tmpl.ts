a70cc0de8e92c7c636bbfde78280e4a2f34f29e9
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-26 22:14:35 +0000

    Example of deploying a Go project to fargate.

--- ts/pulumi/lib/oci/oci_image.tmpl.ts
+++ ts/pulumi/lib/oci/oci_image.tmpl.ts
@@ -1,5 +1,5 @@
 import { local } from '@pulumi/command';
-import { ComponentResource, ComponentResourceOptions, Input, output } from "@pulumi/pulumi";
+import { ComponentResource, ComponentResourceOptions, Input, Output, output } from "@pulumi/pulumi";
 
 
 export interface Args {
@@ -7,7 +7,7 @@ export interface Args {
 }
 
 export class __ClassName extends ComponentResource {
-	url: string = "TODO" // will fill once i've deployed for the first time
+	url: Output<string>
 	constructor(
 		name: string,
 		args: Args,
@@ -23,6 +23,8 @@ export class __ClassName extends ComponentResource {
 				repository,
 			].join(" "))
 		}, { parent: this })
+
+		this.url = upload.stdout
 
 
 		super.registerOutputs({ upload })
