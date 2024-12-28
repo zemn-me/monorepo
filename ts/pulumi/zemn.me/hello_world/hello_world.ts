ad24cce0f884355267a1f47cddec41442554e924
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-28 20:19:01 +0000

    Example of deploying a Go project to fargate.

---
+++ ts/pulumi/zemn.me/hello_world/hello_world.ts
@@ -0,0 +1,70 @@
+import * as aws from "@pulumi/aws";
+import { ComponentResource, ComponentResourceOptions } from "@pulumi/pulumi";
+
+import { mergeTags, TagSet, tagTrue } from "#root/ts/pulumi/lib/tags.js";
+import { HelloWorldImage } from "#root/ts/pulumi/zemn.me/hello_world/HelloWorldImage.js";
+
+export interface Args {
+    tags?: TagSet;
+}
+
+export class LambdaHelloWorld extends ComponentResource {
+    constructor(
+        name: string,
+        args: Args,
+        opts?: ComponentResourceOptions
+    ) {
+        super("ts:pulumi:Component", name, args, opts);
+
+        const tag = name;
+        const tags = mergeTags(args.tags, tagTrue(tag));
+
+        // Create the ECR repository to store our container image
+        const repo = new aws.ecr.Repository("repo", {
+            forceDelete: true,
+            tags: tags,
+        });
+
+		const auth = aws.ecr.getAuthorizationToken();
+
+
+
+        // Build and push the container image
+        const img = new HelloWorldImage(`${name}_img`, {
+            repository: repo.repositoryUrl,
+			token: auth.then(auth => auth.authorizationToken)
+        });
+
+		// eslint-disable-next-line no-console
+		img.url.apply(u => console.log(
+			`STDOUT: ${u}`
+		));
+
+        // Create the Lambda function using the container image
+        const lambda = new aws.lambda.Function(`${name}_lambda`, {
+            packageType: "Image",
+			// e.g. --
+			// https://658613637108.dkr.ecr.us-east-1.amazonaws.com/repo-fe6824f@sha256:c61c4b619733eb59d7c68a34d7b56e562894c9d777f26e982bc453bbcf57ab5b
+            imageUri: img.url,
+            role: new aws.iam.Role("lambdaRole", {
+                assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+                    Service: "lambda.amazonaws.com",
+                }),
+            }).arn,
+            timeout: 30,
+            memorySize: 512,
+            tags: tags,
+        });
+
+        // Add permissions for Lambda to log to CloudWatch
+        new aws.iam.RolePolicyAttachment(`${name}_lambda_logging`, {
+            role: lambda.role!,
+            policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+        });
+
+        // Export the Lambda function name
+        this.registerOutputs({
+            lambdaFunctionName: lambda.name,
+        });
+    }
+}
