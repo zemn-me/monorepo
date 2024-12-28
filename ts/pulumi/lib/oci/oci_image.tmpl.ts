c1f985d9711329775754195b6e1a630bb7986a96
Thomas Neil James Shadwell <thomas@shadwell.im>
2024-12-28 20:11:04 +0000

    Example of deploying a Go project to fargate.

--- ts/pulumi/lib/oci/oci_image.tmpl.ts
+++ ts/pulumi/lib/oci/oci_image.tmpl.ts
@@ -1,13 +1,58 @@
+import { createHash } from 'node:crypto';
+import { mkdir, writeFile } from 'node:fs/promises';
+import { tmpdir } from 'node:os';
+import path, { resolve } from 'node:path';
+
 import { local } from '@pulumi/command';
-import { ComponentResource, ComponentResourceOptions, Input, output } from "@pulumi/pulumi";
+import { all, ComponentResource, ComponentResourceOptions, Input, Output, output } from "@pulumi/pulumi";
 
 
 export interface Args {
 	repository: Input<string>
+	token?: Input<string | undefined>
+}
+
+const imgAuthConfigFilename = "config.json";
+
+export async function createPodmanAuthFile(
+	token: string,
+	registry: string
+): Promise<string> {
+	// Encode credentials
+
+	// Build the auth.json structure
+	const authData = {
+		auths: {
+			[`https://${registry}`]: {
+				auth: token
+			},
+			// i dont know which is right so im just trying both
+			[`${registry}`]: {
+				auth: token
+			},
+		},
+	};
+
+	const fileContent = JSON.stringify(authData)
+
+	const hash = createHash('sha256').update(fileContent).digest('hex');
+
+	// Generate a temporary file path
+	const tempDir = tmpdir();
+	const contentBasedDirName = hash;
+	const configDir = path.join(tempDir, contentBasedDirName);
+	await mkdir(configDir)
+	const filePath = path.join(configDir, imgAuthConfigFilename);
+
+	// Write the JSON data to the file
+	await writeFile(filePath, JSON.stringify(authData));
+
+	// Return the path to the newly created temporary file
+	return resolve(configDir)
 }
 
 export class __ClassName extends ComponentResource {
-	url: string = "TODO" // will fill once i've deployed for the first time
+	url: Output<string>
 	constructor(
 		name: string,
 		args: Args,
@@ -15,14 +60,34 @@ export class __ClassName extends ComponentResource {
 	) {
 		super('__TYPE', name, args, opts);
 
+		const arg = all([args.repository]).apply(([repo]) => [
+			"--repository", `${repo}`
+		])
+
+		const authFile = all([args.token, args.repository]).apply(([token, registry]) => {
+			if (!token || !registry) return;
+			return createPodmanAuthFile(token, registry)
+		});
+
 		const upload = new local.Command(`${name}_push`, {
-			create:
-				output(args.repository).apply(repository => [
-				"__PUSH_BIN",
-				"--repository",
-				repository,
-			].join(" "))
+			environment: output(authFile).apply(f => ({
+				DOCKER_CONFIG: f,
+				// not supported yet by crane --
+				// supported from v0.20.2
+				// https://github.com/google/go-containerregistry/releases/tag/v0.20.2
+				//
+				// rules_oci uses v0.18.x
+				// https://github.com/bazel-contrib/rules_oci/blob/843eb01b152b884fe731a3fb4431b738ad00ea60/oci/private/versions.bzl#L3
+				//REGISTRY_AUTH_FILE: f
+			}) as { [v: string]: string }),
+			interpreter:
+				arg.apply(arg => [
+					"__PUSH_BIN",
+					...arg
+				])
 		}, { parent: this })
+
+		this.url = upload.stdout
 
 
 		super.registerOutputs({ upload })
---
+++ ts/pulumi/zemn.me/hello_world/hello_world.ts
@@ -0,0 +1,61 @@
+import * as aws from "@pulumi/aws";
+import { ComponentResource, ComponentResourceOptions } from "@pulumi/pulumi";
+
+import { mergeTags, TagSet, tagTrue } from "#root/ts/pulumi/lib/tags.js";
+import { HelloWorldImage } from "#root/ts/pulumi/zemn.me/hello_world/HelloWorldImage.js";
+
+export interface Args {
+    tags?: TagSet;
+}
+
+export class LambdaHelloWorld extends ComponentResource {
+    constructor(
+        name: string,
+        args: Args,
+        opts?: ComponentResourceOptions
+    ) {
+        super("ts:pulumi:Component", name, args, opts);
+
+        const tag = name;
+        const tags = mergeTags(args.tags, tagTrue(tag));
+
+        // Create the ECR repository to store our container image
+        const repo = new aws.ecr.Repository("repo", {
+            forceDelete: true,
+            tags: tags,
+        });
+
+		const auth = aws.ecr.getAuthorizationToken();
+
+        // Build and push the container image
+        const img = new HelloWorldImage(`${name}_img`, {
+            repository: repo.repositoryUrl,
+			token: auth.then(auth => auth.authorizationToken)
+        });
+
+        // Create the Lambda function using the container image
+        const lambda = new aws.lambda.Function(`${name}_lambda`, {
+            packageType: "Image",
+            imageUri: img.url,
+            role: new aws.iam.Role("lambdaRole", {
+                assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+                    Service: "lambda.amazonaws.com",
+                }),
+            }).arn,
+            timeout: 30,
+            memorySize: 512,
+            tags: tags,
+        });
+
+        // Add permissions for Lambda to log to CloudWatch
+        new aws.iam.RolePolicyAttachment(`${name}_lambda_logging`, {
+            role: lambda.role!,
+            policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+        });
+
+        // Export the Lambda function name
+        this.registerOutputs({
+            lambdaFunctionName: lambda.name,
+        });
+    }
+}
