5bbed51ba9bf33cbc22f8950e2ee78438214fc42
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-01-02 01:16:51 +0000

    Oci pulumi: upload image when its digest changes.
    We were getting errors at deploy time when the image did not change:
    
    
    ```
    
        command:local:Command (monorepo_zemn.me_fargate_img_oci_image_push):
          error: 2025/01/01 23:57:07 existing manifest: sha256:a44bf22c85a4ebb85f1c4a2883976090d2a3a3a0f6ce1d26c089533f7c6eba93
    
    ```

--- ts/pulumi/lib/oci/oci_image.tmpl.ts
+++ ts/pulumi/lib/oci/oci_image.tmpl.ts
@@ -1,4 +1,6 @@
-import { ComponentResource, ComponentResourceOptions, Input, Output } from "@pulumi/pulumi";
+import { readFile } from "node:fs/promises";
+
+import { ComponentResource, ComponentResourceOptions, Input, Output, output } from "@pulumi/pulumi";
 
 import { OCIImage } from "#root/ts/pulumi/lib/oci/image.js";
 
@@ -10,6 +12,7 @@ export interface Args {
 
 export class __ClassName extends ComponentResource {
 	url: Output<string>
+	static digestPath: string = "__DIGEST"
 	constructor(
 		name: string,
 		args: Args,
@@ -21,6 +24,7 @@ export class __ClassName extends ComponentResource {
 			`${name}_oci_image`,
 			{
 				push: "__PUSH_BIN",
+				digest: output(readFile(__ClassName.digestPath).then(f => f.toString())),
 				...args
 			},
 			{ parent: this }
