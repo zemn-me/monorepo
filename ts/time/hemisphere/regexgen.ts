e5d6fcb50b5362b548ff2fb3eeb4071893d29c3c
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-01-18 00:07:49 +0000

    Generate a short regex to determine the hemisphere a user is in instead of using a massive json file.

---
+++ ts/time/hemisphere/regexgen.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+
+import { readFile } from 'node:fs/promises';
+
+import regexgen from 'regexgen';
+
+
+const fc = readFile('py/time/hemisphere/tz_to_hemisphere.json')
+	.then(ct => JSON.parse(ct.toString()));
+
+const northernHemiLocs = new Set(
+	Object.entries((await fc)).filter(
+		([, v]) => v
+	).map(([k]) => k));
+
+
+const southernHemiLocs = new Set(
+	Object.entries((await fc)).filter(
+		([, v]) => !v
+	).map(([k]) => k));
+
+const southernHemiRe = regexgen([...southernHemiLocs]);
+const northernHemiRe = regexgen([...northernHemiLocs]);
+
+const southernHemiRes = southernHemiRe.toString();
+const northernHemiRes = northernHemiRe.toString();
+
+const body =
+		northernHemiRes.length < southernHemiRes.length
+		? `\
+export const isNorthernHemisphereTz = (s: string) => ${northernHemiRes}.test(s);
+export const isSouthernHemisphereTz = (s: string) => !isNortherHemisphereTz(s);`
+		: `\
+export const isSouthernHemisphereTz = (s: string) => ${southernHemiRes}.test(s)
+export const isNorthernHemisphereTz = (s: string) => !isSouthernHemisphereTz(s)`
+
+console.log(body);
