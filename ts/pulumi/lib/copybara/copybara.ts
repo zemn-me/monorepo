972e029d4a73ea23491ca6672189e922ba442199
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-14 11:27:18 +0000

    Experimental copybara support.
    I think that in the long-term, we should be generating the `copy.bara.sky` from Pulumi such that we can use Pulumi's GitHub plugin t create the repos -- this way no click-ops is requried.

---
+++ ts/pulumi/lib/copybara/copybara.ts
@@ -0,0 +1,52 @@
+
+import { local } from '@pulumi/command';
+import { ComponentResource, ComponentResourceOptions, Input, Output, output } from "@pulumi/pulumi";
+import { FileAsset } from '@pulumi/pulumi/asset';
+
+import { copybaraBin } from '#root/ts/cmd/copybara/copybara.js';
+import { isDefined, must } from '#root/ts/guard.js';
+
+
+export interface CopybaraArgs {
+	args?: Input<string[]>
+	config: Input<FileAsset>
+	staging: Input<boolean>
+}
+
+/**
+ * Represents an OCI [Docker-like] image uploaded to an arbitrary remote OCI
+ * repository such as AWS ECR.
+ */
+export class Copybara extends ComponentResource {
+	/**
+	 * URI uniquely identifying the image as landed in the repo.
+	 */
+	uri: Output<string>
+	constructor(
+		name: string,
+		args: CopybaraArgs,
+		opts?: ComponentResourceOptions
+	) {
+		super("ts:pulumi:lib:copybara:copybara",
+			name, args, opts,
+		);
+
+		// not best practice, but...
+		if (args.staging) return;
+
+		const upload = new local.Command(`${name}_run`, {
+			environment: {
+				GITHUB_TOKEN: must(isDefined)(process.env["GITHUB_TOKEN"])
+			},
+			interpreter: output(args.args??[]).apply(a => [
+				copybaraBin,
+				output(args.config).path,
+				...a
+			]),
+		}, { parent: this });
+
+		this.uri = upload.stdout;
+
+		this.registerOutputs({ upload });
+	}
+}
