d7773655c313c79f76c5c8cc1ed505cc977c95fd
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-17 13:32:48 +0000

    Experimental copybara support.
    I think that in the long-term, we should be generating the `copy.bara.sky` from Pulumi such that we can use Pulumi's GitHub plugin t create the repos -- this way no click-ops is requried.

---
+++ ts/pulumi/github.com/zemn-me/do-sync/do_sync.ts
@@ -0,0 +1,31 @@
+import { runfiles } from '@bazel/runfiles';
+import * as Pulumi from '@pulumi/pulumi';
+import { FileAsset } from '@pulumi/pulumi/asset/asset.js';
+
+import { Copybara } from '#root/ts/pulumi/lib/copybara/copybara.js';
+
+export interface Args {
+	staging: boolean;
+	tags?: Pulumi.Input<Record<string, Pulumi.Input<string>>>;
+}
+
+export class DoSync extends Pulumi.ComponentResource {
+	constructor(
+		name: string,
+		args: Args,
+		opts?: Pulumi.ComponentResourceOptions
+	) {
+		super('ts:pulumi:github.com:zemn-me:do-sync', name, args, opts);
+
+		new Copybara(
+			`${name}_copybara`,
+			{
+				config: new FileAsset(
+					runfiles.resolve("monorepo/ts/do-sync/copy.bara.sky")
+				),
+				staging: args.staging,
+			},
+			{parent: this}
+		)
+	}
+}
