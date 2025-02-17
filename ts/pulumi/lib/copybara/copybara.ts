92609a4420d6ac1abe9bcff1816ac4a3923fab2e
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-17 22:12:30 +0000

    Prevent copybara from properly failing so we can log it...

--- ts/pulumi/lib/copybara/copybara.ts
+++ ts/pulumi/lib/copybara/copybara.ts
@@ -1,9 +1,44 @@
 
-import { local } from '@pulumi/command';
+import { local } from "@pulumi/command";
 import { all, ComponentResource, ComponentResourceOptions, Input, output } from "@pulumi/pulumi";
 
 import { copybaraBin } from '#root/ts/cmd/copybara/copybara.js';
 
+export interface SafeCommandArgs {
+    interpreter: Input<string[]>;
+    environment?: Input<{ [key: string]: Input<string> }>;
+}
+
+export class SafeCommand extends ComponentResource {
+    stdout: Input<string>;
+    stderr: Input<string>;
+
+    constructor(name: string, args: SafeCommandArgs, opts?: ComponentResourceOptions) {
+        super("ts:pulumi:lib:safecommand:SafeCommand", name, args, opts);
+
+        // Wrap command execution to always return exit code 0
+        const wrappedInterpreter = all([args.interpreter]).apply(([interpreter]) => [
+			"sh", "-c", "\"$@\" || exit 0", "--", ...interpreter ?? []
+        ]);
+
+        // Execute the command
+        const run = new local.Command(`${name}_run`, {
+            interpreter: wrappedInterpreter,
+            environment: args.environment ?? {},
+            triggers: [Math.random()] // Ensures the command runs fresh each time
+        }, { parent: this });
+
+        // Capture logs
+        this.stdout = run.stdout;
+        this.stderr = run.stderr;
+
+        // Print logs to console
+        all([this.stdout, this.stderr]).apply(([stdout, stderr]) =>
+            // eslint-disable-next-line no-console
+            console.log(stdout, stderr)
+        );
+    }
+}
 
 export interface CopybaraArgs {
 	args?: Input<string[]>
@@ -41,14 +76,13 @@ export class Copybara extends ComponentResource {
 			]
 		)
 
-		const run = new local.Command(`${name}_run`, {
+		const run = new SafeCommand(`${name}_run`, {
 			environment: output(args.githubToken).apply(
 				GITHUB_TOKEN => GITHUB_TOKEN ? { GITHUB_TOKEN } as {
 					[key: string]: Input<string>;
 				} : {}
 			),
 			interpreter: interpreterParam,
-			triggers: [ Math.random() ]
 		}, { parent: this });
 
 
