deeea02dd7f396b0d9f29a9dbc652e90132b2db7
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-17 23:20:31 +0000

    Provide a proper copybara executable
    And test it!

--- ts/cmd/copybara/smoke_test.ts
+++ ts/cmd/copybara/smoke_test.ts
@@ -1,7 +1,40 @@
-import { expect, test } from "@jest/globals";
+import { execFile } from 'node:child_process';
+import { mkdtempSync, rmSync } from 'node:fs';
+import { tmpdir } from 'node:os';
+import * as path from 'node:path'
+import { promisify } from "node:util";
+
+import { afterEach, beforeEach, expect, test } from '@jest/globals';
 
 import { copybaraBin } from "#root/ts/cmd/copybara/copybara.js";
 
 test('smoke', () => {
 	expect(copybaraBin).toBeDefined();
+})
+
+let tempDir: string;
+
+beforeEach(() => {
+  tempDir = mkdtempSync(path.join(tmpdir(), 'jest-'));
+});
+
+afterEach(() => {
+  rmSync(tempDir, { recursive: true, force: true });
+});
+
+test('execsmoke', async () => {
+	expect.assertions(2);
+
+	// should be set by bazel
+	expect(tempDir).toBeDefined();
+
+	await expect(promisify(execFile)(
+		copybaraBin,
+		["version"],
+		{
+			env: {
+				HOME: tempDir
+			}
+		}
+	)).resolves.toBeDefined();
 })
