8dda65b3f44f7d4ffb1383985995eede130bb2fa
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-21 19:32:54 +0000

    Set up pulumi GCP and register an auth client for auth.zemn.me

---
+++ ts/pulumi/zemn.me/auth/auth.ts
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview auth.zemn.me
+ */
+
+import * as gcp from '@pulumi/gcp';
+import * as Pulumi from '@pulumi/pulumi';
+
+export interface Args {
+	zoneId: Pulumi.Input<string>;
+	domain: string;
+}
+
+export class AuthZemnMe extends Pulumi.ComponentResource {
+	constructor(
+		name: string,
+		args: Args,
+		opts?: Pulumi.ComponentResourceOptions
+	) {
+		super('ts:pulumi:zemn.me:auth', name, args, opts);
+
+		const project = new gcp.organizations.Project(`${name}_project`, {
+		}, { parent: this })
+
+		// should be a singleton out there somewhere some day.
+		const service = new gcp.projects.Service(`${name}_enable_iap`, {
+			service: 'identitytoolkit.googleapis.com',
+			project: project.id
+		}, { parent: this });
+
+
+		const brand = new gcp.iap.Brand(`${name}_brand`, {
+			supportEmail: 'thomas@shadwell.im',
+			applicationTitle: args.domain,
+			project: service.project
+		})
+
+		const client = new gcp.iap.Client(`${name}_client`, {
+			displayName: args.domain,
+			brand: brand.name
+		}, { parent: this });
+
+		this.registerOutputs({ client })
+	}
+}
