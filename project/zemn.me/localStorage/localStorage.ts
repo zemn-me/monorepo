e8f6f56a41cb17277f6ea4c5a1ee489d1e491e7b
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-02-26 21:53:29 +0000

    Add auth service https://zemn.me/auth

---
+++ project/zemn.me/localStorage/localStorage.ts
@@ -0,0 +1,64 @@
+/**
+ * @fileoverview keys for localstorage.
+ */
+
+import b64 from 'base64-js';
+
+
+type Serde<From, To> = [
+	serialize: (f: From) => To,
+	deserialize: (t: To) => From
+]
+
+type Lens<S, A> = [
+	get: (s: S) => A,
+	set: (v: A, s: S) => S
+]
+
+const item =
+(key: string) =>
+	<T>(r: Serde<T, string>): Lens<Storage, T | null> => [
+		storage => {
+			const item = storage.getItem(key);
+			if (item == null) return item;
+
+			return r[1](item)
+		},
+		(value, storage) => {
+			if (value === null) {
+				return (storage.removeItem(key), storage);
+			}
+
+			storage.setItem(key, r[0](value))
+
+			return storage
+		}
+	];
+
+const serdeBytes: Serde<Uint8Array, string> = [
+	u => b64.fromByteArray(u),
+	u => b64.toByteArray(u)
+];
+
+const serdeString: Serde<string, string> = [
+	u => u, u => u
+]
+
+export const ClientSecret = item("0")(serdeBytes);
+export const IdToken = item("1")(serdeString);
+
+export const LSet = <S, A>(
+	lens: Lens<S, A>,
+	storage: S,
+	value: A
+) => lens[1](value, storage);
+
+
+export const LGet = <S, A>(
+	lens: Lens<S, A>,
+	storage: S,
+) => lens[0](storage);
+
+
+
+
