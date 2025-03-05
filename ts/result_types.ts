0889e8ed4c5b76266123ca2bfc6aec2822cd908e
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-05 00:55:48 +0000

    Add auth service https://zemn.me/auth

--- ts/result_types.ts
+++ ts/result_types.ts
@@ -22,17 +22,21 @@ export function is_err<E>(v: Result<unknown, E>): v is Err<E> {
 }
 
 export function unwrap<T, E>(v: Result<T, E>): T {
-	if (is_ok(v)) return v[_ok];
+	if (is_ok(v)) return unwrap_unsafe(v);
 	throw v[_err];
 }
 
-export function unwrap_or<T>(v: Result<T, unknown>, fallback: T): T {
+export function unwrap_unsafe<T>(v: Ok<T>): T {
+	return v[_ok]
+}
+
+export function unwrap_or<T, TT>(v: Result<T, unknown>, fallback: TT): T | TT {
 	if (is_err(v)) return fallback;
 
 	return v[_ok]
 }
 
-export function unwrap_or_else<T1, T2, E>(v: Result<T1, unknown>, fallback: (e: E) => T2): T1 | T2 {
+export function unwrap_or_else<T1, T2, E>(v: Result<T1, E>, fallback: (e: E) => T2): T1 | T2 {
 	if (is_err(v)) return fallback(v[_err] as E);
 
 	return v[_ok]
@@ -49,3 +53,18 @@ export function flatten<T, E1, E2>(v: Result<Result<T, E2>, E1>): Result<T, E1 |
 	return unwrap(v);
 }
 
+
+export function zip<T, TT, E>(self: Result<T, E>, other: Result<TT, E>): Result<[T, TT], E> {
+	if (is_err(self)) return self;
+	if (is_err(other)) return other;
+
+	return Ok([unwrap_unsafe(self), unwrap_unsafe(other)]);
+}
+
+
+export async function result_promise_transpose<T, E>(
+	r: Result<Promise<T>, E>
+): Promise<Result<T, E>> {
+	if (is_err(r)) return r;
+	return Ok(await unwrap_unsafe(r))
+}
