a563be106752cae94fc121601a597d19db451f0b
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-06 01:29:09 +0000

    Export phone number from twilio just by requesting it lol

--- ts/pulumi/lib/twilio/phone_number.ts
+++ ts/pulumi/lib/twilio/phone_number.ts
@@ -2,6 +2,7 @@ import { CustomResourceOptions, dynamic, Output } from "@pulumi/pulumi";
 import twilio from "twilio";
 import { IncomingPhoneNumberInstance, IncomingPhoneNumberListInstanceCreateOptions } from "twilio/lib/rest/api/v2010/account/incomingPhoneNumber.js";
 
+
 export interface TwilioPhoneNumberArgs {
 	countryCode: string;
 	options: Omit<IncomingPhoneNumberListInstanceCreateOptions, 'phoneNumber'>
@@ -11,10 +12,46 @@ const accountSID = () => process.env["TWILIO_ACCOUNT_SID"];
 const authToken = () => process.env["TWILIO_AUTH_TOKEN"];
 const apiKeySid = () => process.env["TWILIO_API_KEY_SID"];
 
-const Client = () => twilio(apiKeySid(), authToken(), { accountSid: accountSID() });
+const maybeClient = () => {
+	const [apikeysid, authtoken, accountsid] = [
+		apiKeySid(),
+		authToken(),
+		accountSID(),
+	];
+
+	if (apikeysid === undefined) return new Error("missing TWILIO_ API_KEY_SID");
+	if (authtoken === undefined) return new Error("missing TWILIO_AUTH TOKEN");
+	if (accountsid === undefined) return new Error("missing TWILIO_ACCOUNT_SID");
+
+	return twilio(
+			apikeysid, authtoken, {accountSid: accountsid}
+		)
+}
+
+const Client = () => {
+	const m = maybeClient();
+	if (m instanceof Error) throw m;
+
+	return m;
+}
 
 type SerializedIncomingPhoneNumberInstance =
-	ReturnType<IncomingPhoneNumberInstance["toJSON"]>
+	ReturnType<IncomingPhoneNumberInstance["toJSON"]>;
+
+/**
+ * get info on the twilio phone number.
+ *
+ * unlike other operations, during testing this just returns
+ * a dummy instead of crashing because the client is not configured.
+ */
+export function getTwilioPhoneNumber(id: string) {
+	const m = maybeClient();
+	if (m instanceof Error) return Promise.resolve({
+		phoneNumber: "dummy"
+	});
+
+	return m.incomingPhoneNumbers(id).fetch()
+}
 
 class TwilioPhoneNumberProvider implements dynamic.ResourceProvider<TwilioPhoneNumberArgs, SerializedIncomingPhoneNumberInstance> {
     public async create(args: TwilioPhoneNumberArgs) {
