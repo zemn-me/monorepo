50c4017c2a4f69c7e1588c0563af8aa3dbae02e0
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-07 00:49:57 +0000

    Filter out expired id_tokens in the admin panel.

--- project/zemn.me/app/admin/client.tsx
+++ project/zemn.me/app/admin/client.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { z } from "zod";
 
 import { requestOIDC, useOIDC } from "#root/project/zemn.me/app/hook/useOIDC.js";
-import { and_then as option_and_then, is_none, None, Option, Some, unwrap_or as option_unwrap_or, unwrap_or_else as option_unwrap_or_else, unwrap_unchecked as option_unwrap_unchecked } from "#root/ts/option/types.js";
+import { ID_Token } from "#root/ts/oidc/oidc.js";
+import { and_then as option_and_then, flatten, is_none, None, Option, option_result_transpose, Some, unwrap_or as option_unwrap_or, unwrap_or_else as option_unwrap_or_else, unwrap_unchecked as option_unwrap_unchecked } from "#root/ts/option/types.js";
 import { and_then as result_and_then, is_err, unwrap_err_unchecked, unwrap_or as result_unwrap_or, unwrap_or_else as result_unwrap_or_else, unwrap_unchecked as result_unwrap_unchecked, unwrap_unchecked as unwrap_result_unchecked } from "#root/ts/result_types.js";
 import { resultFromZod } from "#root/ts/zod/util.js";
 
@@ -13,13 +14,25 @@ const phoneNumberResponseSchema = z.strictObject({
 })
 
 export default function Admin() {
-	const googleAuth = useOIDC("https://accounts.google.com");
+	const googleAuth = useOIDC((v): v is ID_Token => v.iss == "https://accounts.google.com");
+	const authToken = option_and_then(
+		googleAuth,
+		q => result_and_then(
+			q,
+			v => v[0] === undefined ? None : Some(v[0])
+		)
+	);
+
+	const at = result_and_then(option_result_transpose(authToken),
+		o => flatten(o)
+	);
+
 	const [openWindowHnd, setOpenWindowHnd] = useState<Option<WindowProxy>>(None);
 
 	// when googleAuth is something, make sure to close any open window handles
 	useEffect(
 		() => void result_and_then(
-			googleAuth,
+			at,
 			r => option_and_then(
 				r,
 				() => option_and_then(
@@ -28,24 +41,24 @@ export default function Admin() {
 				)
 			)
 		)
-	, [googleAuth])
+	, [at])
 
 	const authTokenCacheKey = result_unwrap_or(result_and_then(
-		googleAuth,
+		at,
 		o => option_unwrap_or(option_and_then(
 			o,
-			o => o.id_token
+			o => o
 		), undefined)
 	), undefined);
 
 	const phoneNumber = useQuery({
 		queryKey: ['callbox', 'phone number', authTokenCacheKey],
 		queryFn: async () => {
-			if (is_err(googleAuth)) return <>
-				⚠ {unwrap_err_unchecked(googleAuth)}
+			if (is_err(at)) return <>
+				⚠ {unwrap_err_unchecked(at)}
 			</>;
 
-			const auth = result_unwrap_unchecked(googleAuth);
+			const auth = result_unwrap_unchecked(at);
 
 			if (is_none(auth)) return <>
 				You need to log in to see this.
@@ -53,7 +66,7 @@ export default function Admin() {
 
 			const pnRespJson = await fetch("https://api.zemn.me/phone/number", {
 				headers: {
-					Authorization: option_unwrap_unchecked(auth).id_token,
+					Authorization: option_unwrap_unchecked(auth)
 				}
 			}) .then(v => v.json());
 
@@ -72,7 +85,7 @@ export default function Admin() {
 
 	const login_button = result_unwrap_or_else(
 		result_and_then(
-			googleAuth,
+			at,
 			r => option_unwrap_or_else(
 				option_and_then(
 					r,
