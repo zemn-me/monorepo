53f3081891273c5c6d69b180651875fbab46372c
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-09 20:02:33 -0700

    Update main.go

--- ts/pulumi/zemn.me/api/cmd/api/main.go
+++ ts/pulumi/zemn.me/api/cmd/api/main.go
@@ -19,10 +19,11 @@ func init() {
 	}))
 
 	r.Get("/phone/init", TwilioErrorHandler(TwilioCallboxEntryPoint))
- r.Post("/phone/init", TwilioErrorHandler(TwilioCallboxEntryPoint))
+	r.Post("/phone/init", TwilioErrorHandler(TwilioCallboxEntryPoint))
 	r.Get("/phone/handleEntry", TwilioErrorHandler(TwilioCallboxProcessPhoneEntry))
+	r.Post("/phone/handleEntry", TwilioErrorHandler(TwilioCallboxProcessPhoneEntry))
 	r.Get("/phone/number", CallboxNumberHandler)
- r.Post("/phone/number", CallboxNumberHandler)
+ 	r.Post("/phone/number", CallboxNumberHandler)
 }
 
 func main() {
