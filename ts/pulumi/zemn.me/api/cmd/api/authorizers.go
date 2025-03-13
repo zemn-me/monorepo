51b616d3a9ff1eff0f9bff897de72fc8ab18fd6e
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-13 06:00:26 +0000

    Add a fake staging for presubmit

--- ts/pulumi/zemn.me/api/cmd/api/authorizers.go
+++ ts/pulumi/zemn.me/api/cmd/api/authorizers.go
@@ -1,9 +1,9 @@
+package main
 
+import "net/http"
 
 func (Server) GetCallboxAuthorizers(w http.ResponseWriter, rq *http.Request) {
-
 }
 
 func (Server) PatchCallboxAuthorizers(w http.ResponseWriter, rq *http.Request) {
-
 }
