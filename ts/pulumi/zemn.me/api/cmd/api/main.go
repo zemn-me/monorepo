42c9a6ec4c8164e20988b7b4e03f875b7e5d3057
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-13 21:17:53 +0000

    zemn.me/api: refactor out go http server to make testing easier.

--- ts/pulumi/zemn.me/api/cmd/api/main.go
+++ ts/pulumi/zemn.me/api/cmd/api/main.go
@@ -2,33 +2,10 @@
 package main
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type"},
-		MaxAge:         300, // Cache preflight response for 5 minutes
-	}))
-
-	r.Get("/phone/init", TwilioErrorHandler(TwilioCallboxEntryPoint))
-	r.Post("/phone/init", TwilioErrorHandler(TwilioCallboxEntryPoint))
-	r.Get("/phone/handleEntry", TwilioErrorHandler(TwilioCallboxProcessPhoneEntry))
-
-	server := NewServer(
-		context.Background(),
-	)
-
-	h := HandlerFromMux(server, r)
-
 	lambda.Start(httpadapter.New(h).ProxyWithContext)
 }
--- ts/pulumi/zemn.me/api/server/authorizers.go
+++ ts/pulumi/zemn.me/api/server/authorizers.go
@@ -1,4 +1,4 @@
-package main
+package apiserver
 
 import (
 	"context"
