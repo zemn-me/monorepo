52419e64224b226a3aece2e5407159d85d4cd08e
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-15 21:42:46 +0000

    add function "queryResult", which turns react-query responses into Result values.

---
+++ ts/result/react-query/queryResult.ts
@@ -0,0 +1,21 @@
+import { UseQueryResult } from "@tanstack/react-query";
+
+import { None, Option, Some } from "#root/ts/option/types.js";
+import { Err, Ok, Result } from "#root/ts/result/result.js";
+
+/**
+ * Returns {@link None} if the query is still loading. Returns
+ * {@link Some}({@Link Result}) if done loading.
+ */
+export function queryResult<T, E>(
+	r: UseQueryResult<T, E>
+): Option<Result<T, E>> {
+	switch (r.status) {
+	case "error":
+		return Some(Err(r.error))
+	case "pending":
+		return None
+	case "success":
+		return Some(Ok(r.data))
+	}
+}
