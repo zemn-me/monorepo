3a23fc63dd941e212d35e9e1386b2800c0f4a798
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-20 23:17:34 +0000

    Play nook phone "yes" sound when the entry code is correct.

---
+++ ts/pulumi/zemn.me/api/server/constants.go
@@ -0,0 +1,8 @@
+package apiserver
+
+// nook phone "complete" pip sound
+// some part of me thinks I should be injecting this from
+// pulumi by some environment variable.
+//
+// some part of me says I don't care.
+const nook_phone_yes = "https://static.zemn.me/nook_phone_yes.mp3"
