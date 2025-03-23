4672eff3d7e76ba0b2ef36aa1a54f00f79ed660e
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-23 07:00:27 +0000

    Twilio request signing (why cant i get a test for this...)

--- ts/pulumi/zemn.me/api/server/phone.go
+++ ts/pulumi/zemn.me/api/server/phone.go
@@ -53,6 +53,31 @@ type AllowedNumber struct {
 	Local string
 }
 
+func (s *Server) AssertTwilioRequest_GET(rw http.ResponseWriter, rq *http.Request) (err error) {
+	if err = rq.ParseForm(); err != nil {
+		return
+	}
+
+	// for some reason twilio's api doesn't take the standard format...
+	var fields map[string]string
+	// this will clearly break if there are dupe fields.
+	for k, v := range map[string][]string(rq.Form) {
+		fields[k] = v[0] // i think must be at least one...
+	}
+
+	ok := s.twilioValidator.Validate(
+		rq.URL.String(),
+		fields,
+		rq.Header.Get("X-Twilio-Signature"),
+	)
+
+	if !ok {
+		return fmt.Errorf("Invalid Twilio Signature")
+	}
+
+	return nil
+}
+
 // getAllowedNumbers returns international format and local format numbers
 // in pairs so end-users can enter their numbers without faffing
 // with exit codes or +.
@@ -100,6 +125,10 @@ func (Server) HandleErrorForTwilio(rw http.ResponseWriter, rq *http.Request, err
 // resident phone numbers). The user is still moved onto the next step if
 // they enter nothing.
 func (s *Server) getPhoneInit(w http.ResponseWriter, r *http.Request) (err error) {
+	if err = s.AssertTwilioRequest_GET(w, r); err != nil {
+		return
+	}
+
 	salutation, err := Salutation()
 	if err != nil {
 		return
@@ -235,6 +264,10 @@ func (s *Server) handleEntryViaAuthorizer(w http.ResponseWriter, rq *http.Reques
 // Takes a param of a phone number to forward the call to (the owner of that
 // phone may then press 9 to open the door).
 func (s *Server) getPhoneHandleEntry(w http.ResponseWriter, r *http.Request, params GetPhoneHandleEntryParams) (err error) {
+	if err = s.AssertTwilioRequest_GET(w, r); err != nil {
+		return
+	}
+
 	ok, err := s.handleEntryViaCode(w, r, params)
 	if ok || err != nil {
 		return
