315db4e4764cc3870c2f8c509d57eec757e189d4
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-03-24 00:46:13 +0000

    Back out "Back out "Back out "Back out "Back out "Back out "Twilio request signing (why cant i get a test for this...)""""""
    Original commit changeset: 0955e3ac58ed

---
+++ ts/pulumi/zemn.me/api/server/twilio_request_validator.go
@@ -0,0 +1,37 @@
+package apiserver
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/twilio/twilio-go/client"
+)
+
+type TwilioRequestValidator struct {
+	client.RequestValidator
+}
+
+func (t TwilioRequestValidator) Validate(rq *http.Request) bool {
+	u := (&url.URL{
+		Scheme: rq.URL.Scheme,
+		Host:   rq.URL.Host,
+		Path:   rq.URL.Path,
+	}).String()
+
+	if err := rq.ParseForm(); err != nil {
+		return false // probably should return the error at some point...
+	}
+
+	// for some reason twilio's api doesn't take the standard format...
+	fields := make(map[string]string)
+	// this will clearly break if there are dupe fields.
+	for k, v := range map[string][]string(rq.PostForm) {
+		fields[k] = v[0] // i think must be at least one...
+	}
+
+	return t.RequestValidator.Validate(
+		u,
+		fields,
+		rq.Header.Get("X-Twilio-Signature"),
+	)
+}
