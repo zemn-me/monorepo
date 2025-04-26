b732677f0cb35106a4aefcb59c9296143369f671
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-04-26 01:21:13 +0000

    Back out "Delete the cloud workstations."
    Original commit changeset: 2ce7ed26b9f2

--- ts/pulumi/index.ts
+++ ts/pulumi/index.ts
@@ -132,7 +132,7 @@ export class Component extends Pulumi.ComponentResource {
 		this.zemnMe = new ZemnMe.Component(
 			`${name}_zemn.me`,
 			{
-				cloudWorkstations: false, // it was not cheaper than GitHub
+				cloudWorkstations: !args.staging,
 				zoneId: Pulumi.output(zone.me.zemn.then(z => z.id)),
 				domain: stage('zemn.me'),
 				noIndex: args.staging,
