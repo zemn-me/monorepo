f1af5136f355751bb43aac822123e30337ba8d1d
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-05-13 00:16:28 +0000

    Eggsfordogs.com

---
+++ ts/pulumi/eggsfordogs.com/app/page.tsx
@@ -0,0 +1,7 @@
+import '#root/ts/pulumi/eggsfordogs.com/app/base.css';
+
+import { EggDogYardClient } from "#root/ts/pulumi/eggsfordogs.com/app/client.js";
+
+export default function Page() {
+	return <EggDogYardClient/>
+}
