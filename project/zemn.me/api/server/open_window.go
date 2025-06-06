de3454409dd248a3f5a771b856bab1f69a18bf0d
Thomas Neil James Shadwell <thomas@shadwell.im>
2025-06-05 22:18:44 -0700

    fix open window endpoint

--- project/zemn.me/api/server/open_window.go
+++ project/zemn.me/api/server/open_window.go
@@ -1,60 +1,70 @@
 package apiserver
 
 import (
-    "context"
-    "time"
+	"context"
+	"time"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-    "github.com/aws/aws-sdk-go-v2/service/dynamodb"
-    "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 const openWindowPartitionKey = "OPEN_WINDOW"
 
+// authCtxKey is used to store the Authorization header in the request context.
+type authCtxKey struct{}
+
 type OpenWindowRecord struct {
-    Id   string `dynamodbav:"id"`
-    Who  string `dynamodbav:"who"`
-    Until Time   `dynamodbav:"until"`
+	Id    string `dynamodbav:"id"`
+	Who   string `dynamodbav:"who"`
+	Until Time   `dynamodbav:"until"`
 }
 
 func (s Server) createOpenWindow(ctx context.Context, rec OpenWindowRecord) error {
-    item, err := attributevalue.MarshalMap(rec)
-    if err != nil {
-        return err
-    }
-    _, err = s.ddb.PutItem(ctx, &dynamodb.PutItemInput{
-        TableName: aws.String(s.openWindowTableName),
-        Item:      item,
-    })
-    return err
+	item, err := attributevalue.MarshalMap(rec)
+	if err != nil {
+		return err
+	}
+	_, err = s.ddb.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(s.openWindowTableName),
+		Item:      item,
+	})
+	return err
 }
 
 func (s Server) isDoorOpen(ctx context.Context) (bool, error) {
-    now := Now()
-    input := &dynamodb.QueryInput{
-        TableName:              aws.String(s.openWindowTableName),
-        KeyConditionExpression: aws.String("id = :id AND until >= :now"),
-        ExpressionAttributeValues: map[string]types.AttributeValue{
-            ":id":  &types.AttributeValueMemberS{Value: openWindowPartitionKey},
-            ":now": &types.AttributeValueMemberS{Value: now.Time.Format(time.RFC3339Nano)},
-        },
-        ScanIndexForward: aws.Bool(false),
-        Limit:            aws.Int32(10),
-    }
-    result, err := s.ddb.Query(ctx, input)
-    if err != nil {
-        return false, err
-    }
-    return len(result.Items) > 0, nil
+	now := Now()
+	input := &dynamodb.QueryInput{
+		TableName:              aws.String(s.openWindowTableName),
+		KeyConditionExpression: aws.String("id = :id AND until >= :now"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":id":  &types.AttributeValueMemberS{Value: openWindowPartitionKey},
+			":now": &types.AttributeValueMemberS{Value: now.Time.Format(time.RFC3339Nano)},
+		},
+		ScanIndexForward: aws.Bool(false),
+		Limit:            aws.Int32(10),
+	}
+	result, err := s.ddb.Query(ctx, input)
+	if err != nil {
+		return false, err
+	}
+	return len(result.Items) > 0, nil
+}
+
+func getAuthToken(ctx context.Context) string {
+	if v, ok := ctx.Value(authCtxKey{}).(string); ok {
+		return v
+	}
+	return ""
 }
 
 func (s Server) PostCallboxOpen(ctx context.Context, rq PostCallboxOpenRequestObject) (PostCallboxOpenResponseObject, error) {
-    who := rq.Headers.Authorization
-    until := Time{Time: time.Now().Add(5 * time.Minute)}
-    err := s.createOpenWindow(ctx, OpenWindowRecord{Id: openWindowPartitionKey, Who: who, Until: until})
-    if err != nil {
-        return nil, err
-    }
-    return PostCallboxOpen200JSONResponse(OpenWindowResponse{OpenUntil: until.Time, Who: who}), nil
+	who := getAuthToken(ctx)
+	until := Time{Time: time.Now().Add(5 * time.Minute)}
+	err := s.createOpenWindow(ctx, OpenWindowRecord{Id: openWindowPartitionKey, Who: who, Until: until})
+	if err != nil {
+		return nil, err
+	}
+	return PostCallboxOpen200JSONResponse(OpenWindowResponse{OpenUntil: until.Time, Who: who}), nil
 }
--- project/zemn.me/api/server/server.go
+++ project/zemn.me/api/server/server.go
@@ -22,10 +22,10 @@ import (
 
 // Server holds the DynamoDB client and table name.
 type Server struct {
-        ddb               *dynamodb.Client
-        settingsTableName string
-       openWindowTableName string
-	rt                *chi.Mux
+	ddb                 *dynamodb.Client
+	settingsTableName   string
+	openWindowTableName string
+	rt                  *chi.Mux
 	http.Handler
 	log                *log.Logger
 	twilioSharedSecret string
@@ -70,9 +70,9 @@ func NewServer(ctx context.Context) (*Server, error) {
 		return nil, err
 	}
 
-       // Allow the table name to be set via environment variables.
-       settingsTableName := os.Getenv("DYNAMODB_TABLE_NAME")
-       openWindowTableName := os.Getenv("OPEN_WINDOW_TABLE_NAME")
+	// Allow the table name to be set via environment variables.
+	settingsTableName := os.Getenv("DYNAMODB_TABLE_NAME")
+	openWindowTableName := os.Getenv("OPEN_WINDOW_TABLE_NAME")
 
 	r := chi.NewRouter()
 
@@ -92,16 +92,23 @@ func NewServer(ctx context.Context) (*Server, error) {
 
 	r.Use(mw)
 
+	authCtxMiddleware := func(next StrictHandlerFunc, _ string) StrictHandlerFunc {
+		return func(ctx context.Context, w http.ResponseWriter, r *http.Request, req interface{}) (interface{}, error) {
+			ctx = context.WithValue(ctx, authCtxKey{}, r.Header.Get("Authorization"))
+			return next(ctx, w, r, req)
+		}
+	}
+
 	s := &Server{
 		log: log.New(
 			os.Stderr,
 			"Server",
 			log.Ldate|log.Ltime|log.Llongfile|log.LUTC,
 		),
-               ddb:                 dynamodb.NewFromConfig(cfg),
-               settingsTableName:   settingsTableName,
-               openWindowTableName: openWindowTableName,
-		twilioSharedSecret: os.Getenv("TWILIO_SHARED_SECRET"),
+		ddb:                 dynamodb.NewFromConfig(cfg),
+		settingsTableName:   settingsTableName,
+		openWindowTableName: openWindowTableName,
+		twilioSharedSecret:  os.Getenv("TWILIO_SHARED_SECRET"),
 		twilioClient: twilio.NewRestClientWithParams(twilio.ClientParams{
 			Username: os.Getenv("TWILIO_API_KEY_SID"), // idk
 			Password: os.Getenv("TWILIO_AUTH_TOKEN"),
@@ -110,7 +117,7 @@ func NewServer(ctx context.Context) (*Server, error) {
 
 	s.Handler = HandlerFromMux(NewStrictHandler(
 		s,
-		nil,
+		[]StrictMiddlewareFunc{authCtxMiddleware},
 	), r)
 
 	return s, nil
