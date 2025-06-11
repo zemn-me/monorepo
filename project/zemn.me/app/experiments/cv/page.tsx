68dddc5e61081f3d3170fda98561d7803a4576c1
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-06-11 17:21:33 +0000

    experiments/cv: use proper time eye logo

--- project/zemn.me/app/experiments/cv/page.tsx
+++ project/zemn.me/app/experiments/cv/page.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import style from '#root/project/zemn.me/app/experiments/cv/page.module.css';
 import { Bio, work } from '#root/project/zemn.me/bio/bio.js';
 import priorities from '#root/project/zemn.me/bio/priority.json';
+import TimeEye from '#root/project/zemn.me/components/TimeEye/TimeEye.js';
 import { isDefined, must } from '#root/ts/guard.js';
 
 const priorityMap = new Map<string, number>(
@@ -71,15 +72,7 @@ export default function CV() {
                                 <a className={style.website} href={Bio.officialWebsite.toString()}>zemn.me</a>
                                 {<div className={style.email}>{Bio.email[0]}</div>}
                                 <div className={style.phone}>+1 901 910 1110</div>
-                                <svg className={`${style.sadIcon} ${style.headerIcon}`} viewBox="0 0 17.78 7.81" xmlns="http://www.w3.org/2000/svg">
-                                        <g style={{ stroke: 'var(--fgc)' }} transform="translate(-13.03 -62.53)">
-                                                <path d="M16.73 62.66l-3.47 6.02h17.32l-3.47-6.02z" fill="none" strokeWidth=".26" />
-                                                <circle cx="21.92" cy="65.47" fill="none" r="1.61" strokeWidth=".16" />
-                                                <ellipse cx="21.92" cy="65.47" fill="none" rx="3.23" ry="1.58" strokeWidth=".23" />
-                                                <path d="M23.53 68.65a1.61 1.61 0 0 1-3.22 0c0-.9.72-1.2 1.61-1.62.9.42 1.61.73 1.61 1.62z" strokeWidth=".16" style={{ fill: 'var(--bgc)' }} />
-                                                <circle cx="21.92" cy="65.47" r=".54" strokeWidth=".08" style={{ fill: 'var(--fgc)' }} />
-                                        </g>
-                                </svg>
+								<TimeEye className={style.headerIcon} />
                                 <div className={style.date}><time dateTime={String(Date.now())}>{new Date().toLocaleDateString('en-GB', { month: 'short', year: 'numeric' })}</time></div>
                                 <div className={`${style.profileName} ${style.name}`}>{Bio.who.name.text}</div>
                         </div>
