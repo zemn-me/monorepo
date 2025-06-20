02bbc33a1262c87d52e91071ea8f194b56da07ff
Thomas Neil James Shadwell <thomas@shadwell.im>
2025-06-20 18:38:22 +0100

    fix expire_on_delete lifecycle object size type

--- ts/pulumi/lib/expire_on_delete/expire_on_delete.ts
+++ ts/pulumi/lib/expire_on_delete/expire_on_delete.ts
@@ -142,7 +142,7 @@ export class S3ExpireOnDeletePolicy extends ComponentResource {
                             key: "soft_deleted",
                             value: "true",
                         },
-						objectSizeGreaterThan: '0',
+                        objectSizeGreaterThan: 0,
                     },
                     status: "Enabled",
                     expiration: {
