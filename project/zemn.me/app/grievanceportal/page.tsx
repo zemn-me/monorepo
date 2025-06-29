09aaf54e33bf20ef1c38ab96379987d90db40315
Thomas Neil James Shadwell <thomas@shadwell.im>
2025-06-28 19:49:56 -0700

    Add metadata to grievance portal

--- project/zemn.me/app/grievanceportal/page.tsx
+++ project/zemn.me/app/grievanceportal/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next/types';
+
 import GrievancePortal from "#root/project/zemn.me/app/grievanceportal/client.js";
 
 export default function Page() {
         return <GrievancePortal />;
 }
+
+export const metadata: Metadata = {
+        title: 'Grievance Portal',
+        description: 'Submit and track grievances.',
+};
