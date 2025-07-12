bba55884c27f4ac0a96ed1d9bfb42d54b7d69888
Thomas Neil James Shadwell <thomas@shadwell.im>
2025-07-11 17:39:46 -0700

    Link ts gazelle extension

--- go/gazelle/ts/ts.go
+++ go/gazelle/ts/ts.go
@@ -1,16 +1,16 @@
 package ts
 
 import (
-    "flag"
-    "path/filepath"
-    "strings"
+	"flag"
+	"path/filepath"
+	"strings"
 
-    "github.com/bazelbuild/bazel-gazelle/config"
-    "github.com/bazelbuild/bazel-gazelle/label"
-    "github.com/bazelbuild/bazel-gazelle/language"
-    "github.com/bazelbuild/bazel-gazelle/repo"
-    "github.com/bazelbuild/bazel-gazelle/resolve"
-    "github.com/bazelbuild/bazel-gazelle/rule"
+	"github.com/bazelbuild/bazel-gazelle/config"
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/bazelbuild/bazel-gazelle/language"
+	"github.com/bazelbuild/bazel-gazelle/repo"
+	"github.com/bazelbuild/bazel-gazelle/resolve"
+	"github.com/bazelbuild/bazel-gazelle/rule"
 )
 
 const allowPath = "go/gazelle/ts"
@@ -22,66 +22,69 @@ type Language struct{}
 func (Language) Name() string { return "typescript" }
 
 func (Language) RegisterFlags(fs *flag.FlagSet, cmd string, c *config.Config) {}
-func (Language) CheckFlags(fs *flag.FlagSet, c *config.Config) error { return nil }
-func (Language) KnownDirectives() []string { return []string{} }
+func (Language) CheckFlags(fs *flag.FlagSet, c *config.Config) error          { return nil }
+func (Language) KnownDirectives() []string                                    { return []string{} }
 func (Language) Configure(c *config.Config, rel string, f *rule.File)         {}
 func (Language) Embeds(r *rule.Rule, from label.Label) []label.Label          { return nil }
-func (Language) Fix(c *config.Config, f *rule.File)                            {}
+func (Language) Fix(c *config.Config, f *rule.File)                           {}
 
 func isAllowed(rel string) bool {
-    return strings.HasPrefix(rel, allowPath)
+	return strings.HasPrefix(rel, allowPath)
 }
 
 func (Language) Kinds() map[string]rule.KindInfo {
-    return map[string]rule.KindInfo{
-        "ts_project": {
-            MatchAny:   true,
-            MergeableAttrs: map[string]bool{
-                "srcs": true,
-                "deps": true,
-            },
-            ResolveAttrs: map[string]bool{
-                "deps": true,
-            },
-            NonEmptyAttrs: map[string]bool{
-                "srcs": true,
-            },
-        },
-    }
+	return map[string]rule.KindInfo{
+		"ts_project": {
+			MatchAny: true,
+			MergeableAttrs: map[string]bool{
+				"srcs": true,
+				"deps": true,
+			},
+			ResolveAttrs: map[string]bool{
+				"deps": true,
+			},
+			NonEmptyAttrs: map[string]bool{
+				"srcs": true,
+			},
+		},
+	}
 }
 
 func (Language) Loads() []rule.LoadInfo {
-    return []rule.LoadInfo{{Name: "//ts:rules.bzl", Symbols: []string{"ts_project"}}}
+	return []rule.LoadInfo{{Name: "//ts:rules.bzl", Symbols: []string{"ts_project"}}}
 }
 
 func (Language) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.ImportSpec {
-    return nil
+	return nil
 }
 
-func (Language) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, r *rule.Rule, imports interface{}, from label.Label) {}
+func (Language) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, r *rule.Rule, imports interface{}, from label.Label) {
+}
 
 func (Language) GenerateRules(args language.GenerateArgs) language.GenerateResult {
-    if !isAllowed(args.Rel) {
-        return language.GenerateResult{}
-    }
+	if !isAllowed(args.Rel) {
+		return language.GenerateResult{}
+	}
 
-    var srcs []string
-    for _, f := range args.RegularFiles {
-        if strings.HasSuffix(f, ".ts") || strings.HasSuffix(f, ".tsx") {
-            srcs = append(srcs, f)
-        }
-    }
-    if len(srcs) == 0 {
-        return language.GenerateResult{}
-    }
+	var srcs []string
+	for _, f := range args.RegularFiles {
+		if strings.HasSuffix(f, ".ts") || strings.HasSuffix(f, ".tsx") {
+			srcs = append(srcs, f)
+		}
+	}
+	if len(srcs) == 0 {
+		return language.GenerateResult{}
+	}
 
-    name := filepath.Base(args.Dir)
-    r := rule.NewRule("ts_project", name)
-    r.SetAttr("srcs", srcs)
-    r.SetAttr("deps", []string{})
+	name := filepath.Base(args.Dir)
+	r := rule.NewRule("ts_project", name)
+	r.SetAttr("srcs", srcs)
+	r.SetAttr("deps", []string{})
 
-    return language.GenerateResult{Gen: []*rule.Rule{r}}
+	return language.GenerateResult{
+		Gen:     []*rule.Rule{r},
+		Imports: []interface{}{nil},
+	}
 }
 
 func NewLanguage() language.Language { return &Language{} }
-
