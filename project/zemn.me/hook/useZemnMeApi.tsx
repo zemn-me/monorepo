7ef6c58b9bfe3a1fe8c1cef077b6dfee91540697
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-21 23:51:43 +0000

    zemn.me: accept NEXT_PUBLIC_ZEMN_ME_API_BASE as api base

--- project/zemn.me/hook/useZemnMeApi.tsx
+++ project/zemn.me/hook/useZemnMeApi.tsx
@@ -3,10 +3,11 @@ import createClient from "openapi-react-query";
 import { useMemo } from 'react';
 
 import type { paths } from "#root/project/zemn.me/api/api_client.gen";
+import { ZEMN_ME_API_BASE } from "#root/project/zemn.me/constants/constants.js";
 
 export function useFetchClient() {
 	return useMemo(() => createFetchClient<paths>({
-		baseUrl: "https://api.zemn.me",
+		baseUrl: ZEMN_ME_API_BASE,
 	})
 	, []);
 }
