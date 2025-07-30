f908a537dd4747c806571e3fdc9bf7b04b07c67a
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-30 18:25:37 +0000

    Integration tests for zemn.me

--- project/zemn.me/testing/browser_test.ts
+++ project/zemn.me/testing/browser_test.ts
@@ -1,12 +1,9 @@
-import { ChildProcess, spawn } from 'node:child_process';
-import http from 'node:http';
 import Path from 'node:path';
 
 import { runfiles } from '@bazel/runfiles';
-import { afterAll, beforeAll, beforeEach, describe, expect, it } from '@jest/globals';
+import { beforeAll, describe, expect, it } from '@jest/globals';
 import glob from 'fast-glob';
 import { Browser, By, ThenableWebDriver } from 'selenium-webdriver';
-import handler from 'serve-handler';
 
 import { Driver } from '#root/ts/selenium/webdriver.js';
 
@@ -17,57 +14,23 @@ const pathsThatMayError = new Set(['healthcheck/bad', 'poc/c/']);
 
 describe('zemn.me website', () => {
 	describe('Endpoint Tests', () => {
-                let server: http.Server;
                 let origin: string;
-                let apiProc: ChildProcess;
-                let apiOrigin: string;
                 let driver: ThenableWebDriver;
 		const paths = glob.sync(Path.join(base, '/**/*.html')).map(path =>
 			Path.relative(base, path).replace(/index.html|.html$/g, '')
 		);
 		paths.sort();
-
                 beforeAll(async () => {
-                        server = http.createServer((rq, rw) => {
-                                void handler(rq, rw, { public: base });
-                        }).listen();
+					driver = Driver().forBrowser(Browser.CHROME).build();
 
-                        const addressInfo = server.address();
+					const ports = JSON.parse(process.env['ASSIGNED_PORTS']!);
 
-                        if (addressInfo == null || typeof addressInfo === 'string') {
-                                throw new Error('Not AddressInfo');
-                        }
+					console.log(ports);
 
-                        origin = `http://localhost:${addressInfo.port}`;
-
-                        const apiBin = runfiles.resolveWorkspaceRelative(
-                                'project/zemn.me/api/cmd/localserver/localserver_/localserver'
-                        );
-                        apiProc = spawn(apiBin, {
-                                stdio: ['ignore', 'pipe', 'inherit'],
-                        });
-                        apiOrigin = await new Promise<string>((resolve, reject) => {
-                                apiProc.stdout!.on('data', chunk => {
-                                        const m = /PORT=(\d+)/.exec(chunk.toString());
-                                        if (m) {
-                                                resolve(`http://localhost:${m[1]}`);
-                                        }
-                                });
-                                apiProc.once('error', reject);
-                                setTimeout(() => reject(new Error('api server did not start')), 10000);
-                        });
+					origin = ports.something;
                 });
 
-		beforeEach(async () => {
-			driver = Driver().forBrowser(Browser.CHROME).build();
-		});
 
-                afterAll(async () => {
-                        apiProc.kill();
-                        await new Promise<void>((resolve, reject) => {
-                                server.close(err => (err ? reject(err) : resolve()));
-                        });
-                });
 
 		const testEndpoint = async (endpoint: string) => {
 			try {
@@ -85,6 +48,7 @@ describe('zemn.me website', () => {
 		};
 
                 it.each(paths)('/%s should have no errors', async path => {
+					expect.assertions(1);
                         const logs = await testEndpoint(path);
                         if (pathsThatMayError.has(path)) return;
                         expect(logs).toHaveLength(
@@ -94,7 +58,7 @@ describe('zemn.me website', () => {
 
                 it('api server /healthz returns OK', async () => {
                         try {
-                                await driver.get(`${apiOrigin}/healthz`);
+								expect.assertions(1);
                                 const body = await driver.findElement(By.css('body')).getText();
                                 expect(body).toBe('"OK"');
                         } finally {
