3d0c952c99c423a6c4d35e58517096b778ffb414
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-30 18:19:22 +0000

    Integration tests for zemn.me

--- project/zemn.me/app/experiments/article/page.tsx
+++ project/zemn.me/app/experiments/article/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next/types';
 
-import { default as Content, frontmatter } from '#root/project/zemn.me/app/experiments/article/test.js';
+import { default as Content, frontmatter } from '#root/project/zemn.me/app/experiments/article/testarticle.js';
 import { Article } from '#root/project/zemn.me/components/Article/article.js';
 import { articleMetadata } from '#root/project/zemn.me/components/Article/article_metadata.js';
 
