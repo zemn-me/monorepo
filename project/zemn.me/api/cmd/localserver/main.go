84fbe1438d745f1a676f94ddc64380d201ab76b6
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-30 09:03:07 +0000

    Connect dynamodb to localserver

--- project/zemn.me/api/cmd/localserver/main.go
+++ project/zemn.me/api/cmd/localserver/main.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	apiserver "github.com/zemn-me/monorepo/project/zemn.me/api/server"
 )
 
-var address string
+var (
+	address    string
+	ddbAddress string
+)
 
 func init() {
 	flag.StringVar(&address, "address", ":0", "Address to listen on")
+	flag.StringVar(&ddbAddress, "ddb-address", "", "Address of the DynamoDB server.")
+}
+
+type AssignedPorts struct {
+	APIPort string `json:"@@//java/software/amazon/dynamodb:dynamodb"`
 }
 
 func main() {
 	flag.Parse()
+
+	assignedPorts := os.Getenv("ASSIGNED_PORTS")
+	if assignedPorts != "" {
+		var ports AssignedPorts
+		if err := json.Unmarshal([]byte(assignedPorts), &ports); err != nil {
+			log.Fatalf("failed to parse ASSIGNED_PORTS: %v (%s)", err)
+		}
+
+		ddbAddress = ports.APIPort
+	}
+
+	fmt.Println(ddbAddress)
+
+	err := os.Setenv("DYNAMODB_ENDPOINT", ddbAddress)
+	if err != nil {
+		log.Fatalf("failed to set DYNAMODB_ENDPOINT: %v", err)
+	}
+
 	srv, err := apiserver.NewServer(context.Background())
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
