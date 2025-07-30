d8e77fbeada989c6645a53adfa274610aa66fddb
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-30 06:24:42 +0000

    Add rules_itest and an api server itest service.

--- project/zemn.me/api/cmd/localserver/main.go
+++ project/zemn.me/api/cmd/localserver/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net"
-    "net/http"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 
-    apiserver "github.com/zemn-me/monorepo/project/zemn.me/api/server"
+	apiserver "github.com/zemn-me/monorepo/project/zemn.me/api/server"
 )
 
-func main() {
-    srv, err := apiserver.NewServer(context.Background())
-    if err != nil {
-        log.Fatalf("failed to create server: %v", err)
-    }
+var address string
 
-    ln, err := net.Listen("tcp", ":0")
-    if err != nil {
-        log.Fatalf("listen: %v", err)
-    }
-    addr := ln.Addr().(*net.TCPAddr)
-    fmt.Printf("PORT=%d\n", addr.Port)
-    if err := http.Serve(ln, srv); err != nil {
-        log.Fatalf("serve: %v", err)
-    }
+func init() {
+	flag.StringVar(&address, "address", ":0", "Address to listen on")
+}
+
+func main() {
+	flag.Parse()
+	srv, err := apiserver.NewServer(context.Background())
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
+
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("listen: %v", err)
+	}
+	addr := ln.Addr().(*net.TCPAddr)
+	fmt.Printf("PORT=%d\n", addr.Port)
+	if err := http.Serve(ln, srv); err != nil {
+		log.Fatalf("serve: %v", err)
+	}
 }
--- project/zemn.me/constants/constants.ts
+++ project/zemn.me/constants/constants.ts
@@ -1,1 +1,3 @@
 export const ZEMN_ME_API_BASE = process.env["NEXT_PUBLIC_ZEMN_ME_API_BASE"] ?? "https://api.zemn.me"
+
+console.log(ZEMN_ME_API_BASE);
