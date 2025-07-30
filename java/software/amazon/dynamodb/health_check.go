d6e0dd4de6348b660f16b23caba3b93d0e1945ae
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2025-07-30 17:02:46 +0000

    add dynamodblocal via maven

---
+++ java/software/amazon/dynamodb/health_check.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/exec"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	cmd := exec.Command(os.Getenv("GET_ASSIGNED_PORT_BIN"), "@@//java/software/amazon/dynamodb:dynamodb")
+	port, err := cmd.CombinedOutput()
+	must(err)
+
+	client := dynamodb.New(dynamodb.Options{
+		EndpointResolver: dynamodb.EndpointResolverFromURL("http://127.0.0.1:" + string(port)),
+		Retryer:          aws.NopRetryer{},
+		Credentials: aws.CredentialsProviderFunc(func(ctx context.Context) (aws.Credentials, error) {
+			return aws.Credentials{
+				AccessKeyID:     "LOCALSTACK",
+				SecretAccessKey: "LOCALSTACK",
+			}, nil
+		}),
+	})
+
+	_, err = client.ListTables(context.Background(), &dynamodb.ListTablesInput{})
+	must(err)
+}
