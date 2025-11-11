37797903e4ac5e6f950838b7dd2a46ab437b7220
Thomas Neil James Shadwell <thomas@shadwell.im>
2025-11-11 12:12:24 -0800

    Rely less on polling in the integration test for the admin panel.

--- project/zemn.me/api/server/admin_test.go
+++ project/zemn.me/api/server/admin_test.go
@@ -1,21 +1,24 @@
 package apiserver
 
 import (
-        "context"
-        "testing"
+	"context"
+	"testing"
 
-        "github.com/zemn-me/monorepo/project/zemn.me/api/server/auth"
+	"github.com/zemn-me/monorepo/project/zemn.me/api/server/auth"
 )
 
 func TestGetAdminUid(t *testing.T) {
-        s := newTestServer()
-        ctx := context.WithValue(context.Background(), auth.SubjectKey, "12345")
-        resp, err := s.GetAdminUid(ctx, GetAdminUidRequestObject{})
-        if err != nil {
-                t.Fatalf("unexpected error: %v", err)
-        }
-        if string(resp.(GetAdminUid200TextResponse)) != "12345" {
-                t.Fatalf("unexpected response: %v", resp)
-        }
+	s := newTestServer()
+	ctx := context.WithValue(context.Background(), auth.SubjectKey, "12345")
+	resp, err := s.GetAdminUid(ctx, GetAdminUidRequestObject{})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	jsonResp, ok := resp.(GetAdminUid200JSONResponse)
+	if !ok {
+		t.Fatalf("unexpected response type: %T", resp)
+	}
+	if jsonResp.Uid != "12345" {
+		t.Fatalf("unexpected response: %v", jsonResp)
+	}
 }
-
