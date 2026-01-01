2028bf88813f20652b48cf8b5197204caef996fa
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-01-01 13:24:39 -0800

    useOIDC: allow configuration of the request params by caller.

--- project/zemn.me/hook/useGoogleAuth.tsx
+++ project/zemn.me/hook/useGoogleAuth.tsx
@@ -1,7 +1,12 @@
 import { GOOGLE_ISSUER_DOMAIN } from '#root/project/zemn.me/constants/constants.js';
 import { useOIDC } from '#root/project/zemn.me/hook/useOIDC.js';
+import { OAuthClientByIssuer } from '#root/project/zemn.me/OAuth/clients.js';
 
 
 export function useGoogleAuth(scopes: string[]) {
-	return useOIDC(GOOGLE_ISSUER_DOMAIN, scopes);
+	const oauthClient = OAuthClientByIssuer(GOOGLE_ISSUER_DOMAIN);
+	return useOIDC(GOOGLE_ISSUER_DOMAIN, {
+		client_id: oauthClient.clientId,
+		scope: scopes.join(' '),
+	});
 }
