30cf462aa2f67f29f50aa459008f4fd6700f295a
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-01-01 14:45:08 -0800

    Localise client registry information and delete the client registry.

--- project/zemn.me/hook/useGoogleAuth.tsx
+++ project/zemn.me/hook/useGoogleAuth.tsx
@@ -1,12 +1,9 @@
-import { GOOGLE_ISSUER_DOMAIN } from '#root/project/zemn.me/constants/constants.js';
+import { GOOGLE_CLIENT_ID, GOOGLE_ISSUER_DOMAIN } from '#root/project/zemn.me/constants/constants.js';
 import { useOIDC } from '#root/project/zemn.me/hook/useOIDC.js';
-import { OAuthClientByIssuer } from '#root/project/zemn.me/OAuth/clients.js';
-
 
 export function useGoogleAuth(scopes: string[]) {
-	const oauthClient = OAuthClientByIssuer(GOOGLE_ISSUER_DOMAIN);
 	return useOIDC(GOOGLE_ISSUER_DOMAIN, {
-		client_id: oauthClient.clientId,
+		client_id: GOOGLE_CLIENT_ID,
 		scope: scopes.join(' '),
 	});
 }
