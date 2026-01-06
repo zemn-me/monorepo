a162cff643a2137dea2c427f5da54008adb4e91e
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-01-06 09:54:25 -0800

    Improvements to PhoneNumberDisplay.

--- project/zemn.me/constants/constants.ts
+++ project/zemn.me/constants/constants.ts
@@ -1,4 +1,5 @@
-export const ZEMN_ME_API_BASE = process.env["NEXT_PUBLIC_ZEMN_ME_API_BASE"] ?? "https://api.zemn.me"
+export const ZEMN_ME_API_BASE = //process.env["NEXT_PUBLIC_ZEMN_ME_API_BASE"] ?? "https://api.zemn.me";
+	"https://api.zemn.me";
 
 export const GOOGLE_ISSUER_DOMAIN = process.env["NEXT_PUBLIC_ZEMN_TEST_OIDC_ISSUER"] ?? "https://accounts.google.com";
 
