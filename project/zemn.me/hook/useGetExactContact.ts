2cd0471d0f16a3e94e9ee69a0c33d882d08886f2
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-01-07 21:15:56 -0800

    Fix useExactContact

--- project/zemn.me/hook/useGetExactContact.ts
+++ project/zemn.me/hook/useGetExactContact.ts
@@ -1,4 +1,5 @@
 import { skipToken, useQuery } from "@tanstack/react-query";
+import parsePhoneNumber from 'libphonenumber-js'
 import createFetchClient from "openapi-fetch";
 
 import { useGoogleAuth } from "#root/project/zemn.me/hook/useGoogleAuth.js";
@@ -6,15 +7,36 @@ import type { paths } from "#root/third_party/com/googleapis/people/api_client.g
 import * as option from "#root/ts/option/types.js";
 
 
+function phoneNumberEqual(
+	a: string,
+	b: string
+): boolean {
+	const [c, d] = [a, b].map(v => v.replaceAll(/-/g, " "))
+	return c === d;
+}
+
 
 export function useGetExactContact(
 	matchOn: "emailAddresses" | "phoneNumbers",
 	content: string | undefined
 ) {
+	// Search uses a lazy cache that is updated after a request. Clients should
+	// first send a warmup search request with an empty query to make sure the
+	// cache has the latest data.
+	const c =
+
+		content
+			? parsePhoneNumber(content)?.formatInternational() ?? ""
+			: ""
+
+	// normalise the phone number via libphonenumber because the google people
+	// api somehow does not index the canonical number only the formatted
+	// number
+
 	const [, access_token ] = useGoogleAuth([]);
 	return useQuery({
-		queryKey: ['contacts-search', content],
-		queryFn: option.is_some(access_token) && content !== undefined
+		queryKey: ['contacts-search', c],
+		queryFn: option.is_some(access_token)
 			? async () => createFetchClient<paths>({
 				baseUrl: "https://people.googleapis.com",
 				headers: {
@@ -23,14 +45,16 @@ export function useGetExactContact(
 			}).GET("/v1/people:searchContacts", {
 				params: {
 					query: {
-						query: content,
+						query: c,
 						readMask: "emailAddresses,phoneNumbers,nicknames,names"
 					}
 				}
 			}).then(
 				v => v.data?.results?.filter(
 					item => item.person?.[matchOn]?.some(
-						ident => ident.value === content
+						ident => matchOn === "phoneNumbers"
+							? phoneNumberEqual(ident.value??"", c)
+							: ident.value === c
 					)
 				)
 			)
