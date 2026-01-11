945661f691f376e3bf20d203a39535d22f60fdee
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2026-01-11 21:30:32 +0000

    Create my dream Future type.

---
+++ ts/future/react-query/useQuery.ts
@@ -0,0 +1,18 @@
+import { UseQueryResult } from "@tanstack/react-query";
+
+import { error, Future, loading, resolve } from "#root/ts/future/future.js";
+
+
+export function useQueryFuture<Then, Error>(
+	result: UseQueryResult<Then, Error>,
+): Future<Then, void, Error> {
+	switch (result.status) {
+	case 'success':
+		return resolve(result.data);
+	case 'pending':
+		return loading(undefined)
+	case 'error':
+		return error(result.error)
+	}
+}
+
