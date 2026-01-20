091191d128f9b95f6634edcbb287730c60427725
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-01-20 10:50:31 -0800

    Only run pulumi refresh 1 in 10 times.

--- ci/submit.ts
+++ ci/submit.ts
@@ -5,15 +5,22 @@ import * as Stack from '#root/ts/pulumi/stack.js';
 async function main() {
 	const stack = await Stack.production();
 
-	console.log(Command('group')({})('Pulumi stack refresh'));
-	const result1 = await stack.refresh({
-		logToStdErr: true,
-		onOutput: o => console.info(o),
-	});
+	const shouldRefresh = Math.random() < 0.1;
+	let refreshSummary = 'Pulumi refresh skipped (90% sample).';
+	if (shouldRefresh) {
+		console.log(Command('group')({})('Pulumi stack refresh'));
+		const result1 = await stack.refresh({
+			logToStdErr: true,
+			onOutput: o => console.info(o),
+		});
 
-	if (result1.stderr !== '')
-		console.log(Command('warning')({})(result1.stderr));
-	console.log(Command('endgroup')({})());
+		if (result1.stderr !== '')
+			console.log(Command('warning')({})(result1.stderr));
+		console.log(Command('endgroup')({})());
+		refreshSummary = result1.summary.message;
+	} else {
+		console.log(Command('notice')({})('Pulumi refresh skipped (90% sample).'));
+	}
 
 	const result2 = await stack.up({
 		logToStdErr: true,
@@ -25,9 +32,7 @@ async function main() {
 	if (result2.stderr !== '')
 		console.log(Command('warning')({})(result2.stderr));
 
-	await Summarize(
-		[result1.summary.message, result2.summary.message].join('\n')
-	);
+	await Summarize([refreshSummary, result2.summary.message].join('\n'));
 }
 
 main().catch(e => {
