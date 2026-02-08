904e67d0477ef135e8a89637ef05b31a3b1f6b90
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-02-08 14:27:12 -0800

    Add react-query debugger.

--- go/gazelle/ts/implied_deps.go
+++ go/gazelle/ts/implied_deps.go
@@ -3,6 +3,7 @@ package ts
 // these naughty packages did not declare
 // their deps correctly...
 var impliedDeps = map[string][]string{
-	"@tanstack/react-query": {"react"},
-	"next":                  {"react-dom"},
+	"@tanstack/react-query":          {"react"},
+	"@tanstack/react-query-devtools": {"@tanstack/query-core"},
+	"next":                           {"react-dom"},
 }
