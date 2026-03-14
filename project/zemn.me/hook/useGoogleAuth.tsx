a7ee699f19d9a705806be36cd9532a8a208789c4
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-03-14 13:03:43 -0700

    fixes and amendments for mesh rendering and quaternions

--- project/zemn.me/hook/useGoogleAuth.tsx
+++ project/zemn.me/hook/useGoogleAuth.tsx
@@ -1,6 +1,10 @@
 import { GOOGLE_CLIENT_ID, GOOGLE_ISSUER_DOMAIN } from '#root/project/zemn.me/constants/constants.js';
 import { useOIDC } from '#root/project/zemn.me/hook/useOIDC.js';
 
+export function invalidateGoogleAuth() {
+
+}
+
 export function useGoogleAuth(scopes: string[]) {
 	return useOIDC(GOOGLE_ISSUER_DOMAIN, {
 		client_id: GOOGLE_CLIENT_ID,
