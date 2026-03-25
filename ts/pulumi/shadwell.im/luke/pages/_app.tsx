8fb20fe108ff03810f4942796e1ceca713fff856
luke shadwell <steadyonroblox@Gmail.com>
2026-03-25 17:34:57 +0000

    Add /wikitree for wikidata family trees to luke.shadwell.im

--- ts/pulumi/shadwell.im/luke/pages/_app.tsx
+++ ts/pulumi/shadwell.im/luke/pages/_app.tsx
@@ -1,11 +1,28 @@
 import type { AppProps } from 'next/app';
 
-import { HeaderTagsAppRouter } from '#root/ts/next.js/index.js';
+import { DefaultContentSecurityPolicy, HeaderTagsPagesRouter } from '#root/ts/next.js/index.js';
 
 export function App({ Component, pageProps }: AppProps) {
+	const pageCsp = {
+		...DefaultContentSecurityPolicy,
+		'script-src': new Set([
+			...(DefaultContentSecurityPolicy['script-src'] ?? []),
+			'https://cdn.jsdelivr.net',
+		]),
+		'connect-src': new Set([
+			...(DefaultContentSecurityPolicy['connect-src'] ?? []),
+			'https://www.wikidata.org',
+		] as const),
+		'img-src': new Set([
+			...(DefaultContentSecurityPolicy['img-src'] ?? []),
+			'https://upload.wikimedia.org',
+			'https://commons.wikimedia.org',
+		] as const),
+	};
+
 	return (
 		<>
-			<HeaderTagsAppRouter domain="luke.shadwell.im" />
+			<HeaderTagsPagesRouter cspPolicy={pageCsp} domain="luke.shadwell.im" />
 			<Component {...pageProps} />
 		</>
 	);
