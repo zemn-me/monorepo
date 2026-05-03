e64b0693e5068613c438f97f4f5bdb3bea683b25
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-05-03 15:16:16 -0700

    Quaternions not to throw, but to return errors.

--- ts/math/wireframe_test.ts
+++ ts/math/wireframe_test.ts
@@ -3,6 +3,7 @@ import { describe, expect, test } from '@jest/globals';
 import { point } from '#root/ts/math/cartesian.js';
 import * as Quaternion from '#root/ts/math/quaternion.js';
 import { box, pyramid, rigidTransform } from '#root/ts/math/wireframe.js';
+import { unwrap } from '#root/ts/result/result.js';
 
 describe('wireframe', () => {
 	test('box returns the expected twelve edges', () => {
@@ -35,16 +36,17 @@ describe('wireframe', () => {
 	test('rigidTransform rotates and translates each endpoint', () => {
 		const transformed = rigidTransform(
 			[[point<3>(1, 0, 0), point<3>(0, 1, 0)]],
-			Quaternion.fromAxisAngle(point<3>(0, 0, 1), Math.PI / 2),
+			unwrap(Quaternion.fromAxisAngle(point<3>(0, 0, 1), Math.PI / 2)),
 			point<3>(1, 2, 3)
 		);
+		const value = unwrap(transformed);
 
-		expect(transformed).toHaveLength(1);
-		expect(transformed[0]![0][0]![0]!).toBeCloseTo(1);
-		expect(transformed[0]![0][1]![0]!).toBeCloseTo(3);
-		expect(transformed[0]![0][2]![0]!).toBeCloseTo(3);
-		expect(transformed[0]![1][0]![0]!).toBeCloseTo(0);
-		expect(transformed[0]![1][1]![0]!).toBeCloseTo(2);
-		expect(transformed[0]![1][2]![0]!).toBeCloseTo(3);
+		expect(value).toHaveLength(1);
+		expect(value[0]![0][0]![0]!).toBeCloseTo(1);
+		expect(value[0]![0][1]![0]!).toBeCloseTo(3);
+		expect(value[0]![0][2]![0]!).toBeCloseTo(3);
+		expect(value[0]![1][0]![0]!).toBeCloseTo(0);
+		expect(value[0]![1][1]![0]!).toBeCloseTo(2);
+		expect(value[0]![1][2]![0]!).toBeCloseTo(3);
 	});
 });
