823ddcda3ebed41c7291f1e197818c708efeb242
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-05-03 20:36:02 -0700

    Move baby/lulu/shadwell site code into canonical project namespaces

---
+++ project/computer/lulu/app/page.tsx
@@ -0,0 +1,7 @@
+import '#root/project/computer/lulu/app/base.css';
+
+import { FishbowlClient } from "#root/project/computer/lulu/app/client.js";
+
+export default function Page() {
+	return <FishbowlClient/>
+}
