a2b0d54a1c7f4c0c75d4a0c60a99f75e7c0152d2
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-05-15 11:24:08 -0700

    Endings

---
+++ project/endings/app/page.tsx
@@ -0,0 +1,7 @@
+import '#root/project/endings/app/base.css';
+
+import { EndingsClient } from '#root/project/endings/app/client.js';
+
+export default function Page() {
+	return <EndingsClient />;
+}
