68b3cedfd550b8af31600a26092c218ea759b4eb
Thomas Neil James Shadwell <thomas@shadwell.im>
2026-05-24 12:31:53 -0700

    Use content-addressable store for web assets.

--- ts/pulumi/testing/pulumi_test.ts
+++ ts/pulumi/testing/pulumi_test.ts
@@ -1,16 +1,13 @@
-import '@pulumi/pulumi';
 import 'ts/pulumi/setMocks';
 
 import { describe, test } from '@jest/globals';
+import * as pulumi from '@pulumi/pulumi';
 
 import * as project from '#root/ts/pulumi/index.js';
 
 describe('pulumi', () => {
 	test('smoke', async () => {
 		new project.Component('monorepo', { staging: false });
-		// eventually I think I need to make
-		// the whole setup a custom component to make this
-		// actually work.
-		await new Promise(ok => setTimeout(ok, 5000));
+		await pulumi.runtime.disconnect();
 	});
 });
