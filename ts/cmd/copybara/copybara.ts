de8fa46769111e5eff543713c744c3552cbfe562
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2026-07-24 11:40:28 -0700

    Upgrade to Bazel 9 (#13593)
    Co-authored-by: Thomas Neil James Shadwell <thomas@Thomass-MacBook-Pro.local>

--- ts/cmd/copybara/copybara.ts
+++
@@ -1,7 +0,0 @@
-import { runfiles } from '@bazel/runfiles';
-
-import { isDefined, must } from '#root/ts/guard.js';
-
-export const copybaraBin = runfiles.resolve(
-	must(isDefined)('copybara/java/com/google/copybara/copybara')
-);
