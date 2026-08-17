25fad2a4ebd8d6959b82df52b8893c21a3e3313b
Thomas Neil James Shadwell <thomas@Mac.localdomain>
2026-08-16 21:51:59 -0700

    Update Next.js to 15.5.21

--- project/me/zemn/app/robots.ts
+++ project/me/zemn/app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next/types';
 
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
--- ts/next.js/config_validate_test.ts
+++ ts/next.js/config_validate_test.ts
@@ -1,9 +1,9 @@
 import { expect, it } from '@jest/globals';
 import { NextConfig } from 'next/types';
 
-import * as mod from '#root/ts/next.js/next.config.js';
+import config from '#root/ts/next.js/next.config.js';
 
 it('should be correct type', () => {
-	const x: NextConfig = mod;
+	const x: NextConfig = config;
 	expect(x).not.toBeUndefined();
 });
